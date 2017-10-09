3666d705b5c9cbce15d22ee4f5e2f328903b1197
james58899 <james59988@gmail.com>
2017-10-10 01:55:02 +0800

    remove command spawn,hat

--- src/main/java/one/oktw/sponge/internal/CommandRegister.java
+++ src/main/java/one/oktw/sponge/internal/CommandRegister.java
@@ -14,8 +14,6 @@ public class CommandRegister {
         commandManager.register(plugin, new CommandCreate().getSpec(), "create");
         commandManager.register(plugin, new CommandWorld().getSpec(), "world");
         commandManager.register(plugin, new CommandRebuild().getSpec(), "rebuild");
-        commandManager.register(plugin, new CommandHat().getSpec(), "hat");
-        commandManager.register(plugin, new CommandSpawn().getSpec(), "spawn");
         commandManager.register(plugin, new CommandInvite().getSpec(), "invite");
         commandManager.register(plugin, new CommandRemove().getSpec(), "remove");
         commandManager.register(plugin, new CommandHub().getSpec(), "hub");

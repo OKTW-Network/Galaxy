f9d78157e2e9c598c0556af75316662f8cf01afc
james58899 <james59988@gmail.com>
2017-10-14 22:15:59 +0800

    init save world info

--- src/main/java/one/oktw/sponge/internal/DatabaseManager.java
+++ src/main/java/one/oktw/sponge/internal/DatabaseManager.java
@@ -1,18 +1,35 @@
 package one.oktw.sponge.internal;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import org.slf4j.Logger;
 
 import static one.oktw.sponge.Main.getMain;
 
 public class DatabaseManager {
+    private MongoDatabase database;
+
     public DatabaseManager() {
-        ConfigManager config = getMain().getConfigManager();
-        CommentedConfigurationNode configNode = config.getConfigNode();
-        CommentedConfigurationNode databaseConfig = configNode.getNode("database");
+        ConfigManager configManager = getMain().getConfigManager();
+        CommentedConfigurationNode config = configManager.getConfigNode().getNode("database");
 
         Logger logger = getMain().getLogger();
         logger.info("Loading Database...");
-        // TODO
+
+        if (config.isVirtual()) {
+            config.setComment("Mongodb connect setting");
+            config.getNode("host").setValue("localhost");
+            config.getNode("port").setValue(27017);
+            config.getNode("name").setValue("oktw");
+            config.getNode("name").setComment("Database name");
+            configManager.save();
+        }
+
+        database = new MongoClient(config.getNode("host").getString(), config.getNode("port").getInt()).getDatabase(config.getNode("name").getString());
+    }
+
+    MongoDatabase getDatabase() {
+        return database;
     }
 }
--- src/main/java/one/oktw/sponge/internal/WorldManager.java
+++ src/main/java/one/oktw/sponge/internal/WorldManager.java
@@ -1,73 +1,82 @@
 package one.oktw.sponge.internal;
 
+import com.mongodb.client.MongoCollection;
 import one.oktw.sponge.Main;
+import org.bson.Document;
 import org.slf4j.Logger;
-import org.spongepowered.api.CatalogTypes;
 import org.spongepowered.api.Server;
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.world.World;
-import org.spongepowered.api.world.WorldArchetype;
+import org.spongepowered.api.world.WorldArchetypes;
 import org.spongepowered.api.world.storage.WorldProperties;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
+import static com.mongodb.client.model.Filters.eq;
 import static one.oktw.sponge.Main.getMain;
 
 public class WorldManager {
-    private Main core = getMain();
-    private Logger logger = core.getLogger();
+    private Main main = getMain();
+    private Logger logger = main.getLogger();
+    private MongoCollection<Document> collection = main.getDatabaseManager().getDatabase().getCollection("world");
     private Server server = Sponge.getServer();
 
-    public Optional<World> createWorld(String name) {
-        logger.info("Create World [{}]", name);
-        WorldArchetype worldArchetype;
-        if (!Sponge.getGame().getRegistry().getType(CatalogTypes.WORLD_ARCHETYPE, name).isPresent()) {
-            worldArchetype = WorldArchetype.builder()
-                    .keepsSpawnLoaded(false)
-                    .generateSpawnOnLoad(false)
-                    .loadsOnStartup(false)
-                    .build(name, name);
-        } else {
-            worldArchetype = Sponge.getGame().getRegistry().getType(CatalogTypes.WORLD_ARCHETYPE, name).get();
-        }
+    public Optional<World> createWorld(String name, Player player) {
+        WorldProperties properties;
+        logger.info("Create World [{}], Owner: {}", name, player.getName());
 
         try {
-            WorldProperties properties = server.createWorldProperties(name, worldArchetype);
+            properties = server.createWorldProperties(name, WorldArchetypes.OVERWORLD);
             properties.setKeepSpawnLoaded(false);
             properties.setGenerateSpawnOnLoad(false);
             properties.setLoadOnStartup(false);
+            server.saveWorldProperties(properties);
         } catch (IOException e) {
             logger.error("Create world failed!", e);
+            return Optional.empty();
         }
 
+        Document worldInfo = new Document("uuid", properties.getUniqueId().toString())
+                .append("name", name)
+                .append("owner", player.getUniqueId());
+
+        collection.insertOne(worldInfo);
         return server.loadWorld(name);
     }
 
-    public void removeWorld(String name) {
-        if (!server.getWorldProperties(name).isPresent()) {
-            logger.error("Delete World [{}] failed: world not found", name);
+    public void removeWorld(UUID uuid) {
+        WorldProperties properties;
+        if (server.getWorldProperties(uuid).isPresent()) {
+            properties = server.getWorldProperties(uuid).get();
+        } else {
+            logger.error("Delete World [{}] failed: world not found", uuid.toString());
+            return;
         }
 
-        logger.info("Deleting World [{}]", name);
-        if (server.getWorld(name).isPresent()) {
-            World world = server.getWorld(name).get();
+        logger.info("Deleting World [{}]", properties.getWorldName());
+        if (server.getWorld(uuid).isPresent()) {
+            World world = server.getWorld(uuid).get();
             world.getPlayers().forEach(player -> player.setLocationSafely(server.getWorld(server.getDefaultWorldName()).get().getSpawnLocation()));
             server.unloadWorld(world);
         }
 
         try {
-            server.deleteWorld(server.getWorldProperties(name).get()).get();
+            server.deleteWorld(properties).get();
         } catch (InterruptedException | ExecutionException e) {
             logger.error("Delete world failed!", e);
+            return;
         }
+
+        collection.deleteOne(eq("uuid", uuid.toString()));
     }
 
-    public Optional<World> loadWorld(String name) {
-        Optional<WorldProperties> propertiesOptional = server.getWorldProperties(name);
-        if (propertiesOptional.isPresent()) {
-            WorldProperties worldProperties = propertiesOptional.get();
+    public Optional<World> loadWorld(UUID uuid) {
+        if (server.getWorldProperties(uuid).isPresent()) {
+            WorldProperties worldProperties = server.getWorldProperties(uuid).get();
             worldProperties.setGenerateSpawnOnLoad(false);
             return server.loadWorld(worldProperties);
         } else {

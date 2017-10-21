fb9455b798719615ff51a9ec5045f2327a943e59
james58899 <james59988@gmail.com>
2017-10-21 22:14:29 +0800

    Implement PlayerData

--- src/main/java/one/oktw/sponge/internal/EventRegister.java
+++ src/main/java/one/oktw/sponge/internal/EventRegister.java
@@ -2,6 +2,7 @@ package one.oktw.sponge.internal;
 
 import one.oktw.sponge.Main;
 import one.oktw.sponge.event.DisablePortal;
+import one.oktw.sponge.event.Disconnect;
 import one.oktw.sponge.event.ForceGamemode;
 import org.slf4j.Logger;
 import org.spongepowered.api.Sponge;
@@ -17,9 +18,9 @@ public class EventRegister {
         logger.info("Registering Event...");
         eventManager.registerListeners(main, new DisablePortal());
         eventManager.registerListeners(main, new ForceGamemode());
+        eventManager.registerListeners(main, new Disconnect());
 
 //        eventManager.registerListeners(getCore(), new PlayerJoin());
-//        eventManager.registerListeners(getCore(), new Event());
 //        eventManager.registerListeners(getCore(), new EventExplosion());
 //        eventManager.registerListeners(getCore(), new ChangeBlockFilter());
     }

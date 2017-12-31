fc6495f376d1cfb2f132c05c3131541627a528fd
jimchen5209 <jimchen5209@gmail.com>
2017-12-31 19:59:27 +0800

    Registered /galaxy command(but no function)

---
+++ src/main/kotlin/one/oktw/galaxy/command/CommandGalaxy.kt
@@ -0,0 +1,23 @@
+package one.oktw.galaxy.command
+
+import org.spongepowered.api.command.CommandResult
+import org.spongepowered.api.command.CommandSource
+import org.spongepowered.api.command.args.CommandContext
+import org.spongepowered.api.command.spec.CommandSpec
+import org.spongepowered.api.entity.living.player.Player
+import org.spongepowered.api.text.Text
+
+class CommandGalaxy : CommandBase {
+    override val spec: CommandSpec
+        get() = CommandSpec.builder()
+                .executor(this)
+                .permission("oktw.command.galaxy")
+                .description(Text.of("銀河世界管理"))
+                .build()
+    override fun execute(src: CommandSource?, args: CommandContext?): CommandResult {
+        if (src is Player) {
+            return CommandResult.success()
+        }
+        return CommandResult.empty()
+    }
+}
--- src/main/kotlin/one/oktw/galaxy/internal/CommandRegister.kt
+++ src/main/kotlin/one/oktw/galaxy/internal/CommandRegister.kt
@@ -1,11 +1,20 @@
 package one.oktw.galaxy.internal
 
+import one.oktw.galaxy.Main.Companion.main
+import one.oktw.galaxy.command.CommandGalaxy
 import org.spongepowered.api.Sponge
 
 class CommandRegister {
     init {
         Sponge.getCommandManager().apply {
-            //            register(main, CommandTest().spec, "test")
+//                        register(main, CommandTest().spec, "test")
+            register(main, CommandGalaxy().spec, "galaxy")
         }
     }
 }
+
+
+
+
+
+

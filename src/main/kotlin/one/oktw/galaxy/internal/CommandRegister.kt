b340eada971924ab5fabf6f1a5416cb4ffa03459
james58899 <james59988@gmail.com>
2018-01-19 19:15:09 +0800

    test command

--- src/main/kotlin/one/oktw/galaxy/internal/CommandRegister.kt
+++ src/main/kotlin/one/oktw/galaxy/internal/CommandRegister.kt
@@ -1,11 +1,13 @@
 package one.oktw.galaxy.internal
 
+import one.oktw.galaxy.Main.Companion.main
+import one.oktw.galaxy.command.CommandTest
 import org.spongepowered.api.Sponge
 
 class CommandRegister {
     init {
         Sponge.getCommandManager().apply {
-            //            register(main, CommandTest().spec, "test")
+            register(main, CommandTest().spec, "test")
         }
     }
 }

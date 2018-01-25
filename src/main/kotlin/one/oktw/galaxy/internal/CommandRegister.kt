4f2c439db8a87dd0a40fb448315b6c772ff8c5e3
jimchen5209 <jimchen5209@gmail.com>
2018-01-25 23:26:49 +0800

    Removed tpa tpahere for testing

--- src/main/kotlin/one/oktw/galaxy/internal/CommandRegister.kt
+++ src/main/kotlin/one/oktw/galaxy/internal/CommandRegister.kt
@@ -1,8 +1,6 @@
 package one.oktw.galaxy.internal
 
 import one.oktw.galaxy.Main.Companion.main
-import one.oktw.galaxy.command.CommandTpa
-import one.oktw.galaxy.command.CommandTpaHere
 import one.oktw.galaxy.command.SaveMySelf
 import one.oktw.galaxy.command.CommandTest
 import org.spongepowered.api.Sponge
@@ -13,8 +11,6 @@ class CommandRegister {
     init {
         logger.info("Register command...")
         Sponge.getCommandManager().apply {
-            register(main, CommandTpa().spec, "tpa")
-            register(main, CommandTpaHere().spec, "tpahere")
             register(main, SaveMySelf().spec, "savemyself")
             register(main, CommandTest().spec, "test")
         }

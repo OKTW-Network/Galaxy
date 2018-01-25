94e481ca561dbcfcff42afe094780988b9a94e6f
jimchen5209 <jimchen5209@gmail.com>
2018-01-25 22:28:35 +0800

    Cleaned the conflicts

--- src/main/kotlin/one/oktw/galaxy/internal/CommandRegister.kt
+++ src/main/kotlin/one/oktw/galaxy/internal/CommandRegister.kt
@@ -1,14 +1,11 @@
 package one.oktw.galaxy.internal
 
 import one.oktw.galaxy.Main.Companion.main
-<<<<<<< HEAD
 import one.oktw.galaxy.command.CommandGalaxy
 import one.oktw.galaxy.command.CommandTpa
 import one.oktw.galaxy.command.CommandTpaHere
 import one.oktw.galaxy.command.SaveMySelf
-=======
 import one.oktw.galaxy.command.CommandTest
->>>>>>> ce965b261ef8493d333a588c37349dd2be55a778
 import org.spongepowered.api.Sponge
 
 class CommandRegister {

f18fff85ae78101f012319e5685f8d4504b17c74
jimchen5209 <jimchen5209@gmail.com>
2018-01-25 23:19:36 +0800

    Remove unused Galaxy command

--- src/main/kotlin/one/oktw/galaxy/internal/CommandRegister.kt
+++ src/main/kotlin/one/oktw/galaxy/internal/CommandRegister.kt
@@ -1,7 +1,6 @@
 package one.oktw.galaxy.internal
 
 import one.oktw.galaxy.Main.Companion.main
-import one.oktw.galaxy.command.CommandGalaxy
 import one.oktw.galaxy.command.CommandTpa
 import one.oktw.galaxy.command.CommandTpaHere
 import one.oktw.galaxy.command.SaveMySelf
@@ -14,8 +13,6 @@ class CommandRegister {
     init {
         logger.info("Register command...")
         Sponge.getCommandManager().apply {
-//                        register(main, CommandTest().spec, "test")
-            register(main, CommandGalaxy().spec, "galaxy")
             register(main, CommandTpa().spec, "tpa")
             register(main, CommandTpaHere().spec, "tpahere")
             register(main, SaveMySelf().spec, "savemyself")

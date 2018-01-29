1f4b53d1e056b95d19f257c648db7ffe77c1f41d
jimchen5209 <jimchen5209@gmail.com>
2018-01-29 22:26:38 +0800

    Removed click one time check due to memory leak

--- src/main/kotlin/one/oktw/galaxy/command/UnStuck.kt
+++ src/main/kotlin/one/oktw/galaxy/command/UnStuck.kt
@@ -22,21 +22,17 @@ class UnStuck : CommandBase {
 
     override fun execute(src: CommandSource, args: CommandContext?): CommandResult {
         if (src is Player) {
-            val random = UUID.randomUUID()
-            Btnlist.add(random)
             val retrybutton = Text.builder("高度提高一點試試")
                     .color(TextColors.AQUA)
                     .style(TextStyles.UNDERLINE)
                     .onClick(TextActions.executeCallback {
-                        if (random in Btnlist) {
-                            Btnlist.remove(random)
                             if (src.setLocationSafely(src.location.add(0.0, 2.0, 0.0))) {
                                 src.sendMessage(Text.of(TextColors.GREEN, "已嘗試自救"))
                             } else {
                                 src.sendMessage(Text.of(TextColors.RED, "自救失敗，找不到安全位置"))
                             }
                         }
-                    })
+                    )
                     .build()
             if (src.setLocationSafely(src.location)) {
                 src.sendMessage(Text.of(TextColors.GREEN, "已嘗試自救\n",TextColors.GOLD,"覺得沒被救到嗎?",retrybutton))

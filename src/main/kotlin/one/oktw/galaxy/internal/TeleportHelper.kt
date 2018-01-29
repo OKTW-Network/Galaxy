2d27721d7da7635f6b3b6b0b1759a3507d8028f3
james58899 <james59988@gmail.com>
2018-01-29 14:17:05 +0800

    TeleportHelper

---
+++ src/main/kotlin/one/oktw/galaxy/internal/TeleportHelper.kt
@@ -0,0 +1,70 @@
+package one.oktw.galaxy.internal
+
+import one.oktw.galaxy.Main.Companion.galaxyManager
+import one.oktw.galaxy.Main.Companion.travelerManager
+import one.oktw.galaxy.internal.enums.AccessLevel.*
+import org.spongepowered.api.entity.living.player.Player
+import org.spongepowered.api.world.Location
+import org.spongepowered.api.world.World
+
+class TeleportHelper {
+    companion object {
+        fun checkValid(player: Player, location: Location<World>): Boolean {
+            val traveler = travelerManager.getTraveler(player)
+            val planet = galaxyManager.getPlanet(location.extent.uniqueId) ?: return false
+
+            return when (planet.checkPermission(traveler)) {
+                MODIFY, VIEW -> true
+                DENY -> false
+            }
+        }
+
+        fun teleport(player: Player, location: Location<World>): Boolean {
+            if (!checkValid(player, location)) return false
+
+            val traveler = travelerManager.getTraveler(player)
+            val planet = galaxyManager.getPlanet(location.extent.uniqueId) ?: return false
+
+            result@ PlanetHelper.loadPlanet(planet).ifPresent {
+                val result = player.setLocation(location)
+
+                if (result) {
+                    traveler.position.fromPosition(location.position).planet = planet.uuid
+                    if (planet.checkPermission(traveler) == VIEW) {
+                        travelerManager.setViewer(player.uniqueId)
+                    } else {
+                        travelerManager.removeViewer(player.uniqueId)
+                    }
+                }
+
+                return@result result
+            }
+
+            return false
+        }
+
+        fun teleportSafely(player: Player, location: Location<World>): Boolean {
+            if (!checkValid(player, location)) return false
+
+            val traveler = travelerManager.getTraveler(player)
+            val planet = galaxyManager.getPlanet(location.extent.uniqueId) ?: return false
+
+            result@ PlanetHelper.loadPlanet(planet).ifPresent {
+                val result = player.setLocationSafely(location)
+
+                if (result) {
+                    traveler.position.fromPosition(location.position).planet = planet.uuid
+                    if (planet.checkPermission(traveler) == VIEW) {
+                        travelerManager.setViewer(player.uniqueId)
+                    } else {
+                        travelerManager.removeViewer(player.uniqueId)
+                    }
+                }
+
+                return@result result
+            }
+
+            return false
+        }
+    }
+}

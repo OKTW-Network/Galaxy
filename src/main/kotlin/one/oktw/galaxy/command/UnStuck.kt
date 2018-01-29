d990c25577bc851eb2b7b39d4e4d56d073979c5d
jimchen5209 <jimchen5209@gmail.com>
2018-01-29 23:09:31 +0800

    Added auto delete UUID in unstuck

--- src/main/kotlin/one/oktw/galaxy/command/UnStuck.kt
+++ src/main/kotlin/one/oktw/galaxy/command/UnStuck.kt
@@ -1,15 +1,18 @@
 package one.oktw.galaxy.command
 
+import one.oktw.galaxy.Main.Companion.main
 import org.spongepowered.api.command.CommandResult
 import org.spongepowered.api.command.CommandSource
 import org.spongepowered.api.command.args.CommandContext
 import org.spongepowered.api.command.spec.CommandSpec
 import org.spongepowered.api.entity.living.player.Player
+import org.spongepowered.api.scheduler.Task
 import org.spongepowered.api.text.Text
 import org.spongepowered.api.text.action.TextActions
 import org.spongepowered.api.text.format.TextColors
 import org.spongepowered.api.text.format.TextStyles
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 val Btnlist : ArrayList<UUID> = ArrayList()
 class UnStuck : CommandBase {
@@ -22,24 +25,39 @@ class UnStuck : CommandBase {
 
     override fun execute(src: CommandSource, args: CommandContext?): CommandResult {
         if (src is Player) {
+            val random = UUID.randomUUID()
             val retrybutton = Text.builder("高度提高一點試試")
                     .color(TextColors.AQUA)
                     .style(TextStyles.UNDERLINE)
                     .onClick(TextActions.executeCallback {
+                        if (random in Btnlist) {
+                            Btnlist.remove(random)
                             if (src.setLocationSafely(src.location.add(0.0, 2.0, 0.0))) {
                                 src.sendMessage(Text.of(TextColors.GREEN, "已嘗試自救"))
                             } else {
                                 src.sendMessage(Text.of(TextColors.RED, "自救失敗，找不到安全位置"))
                             }
                         }
-                    )
+                    })
                     .build()
             if (src.setLocationSafely(src.location)) {
+                Btnlist.add(random)
                 src.sendMessage(Text.of(TextColors.GREEN, "已嘗試自救\n",TextColors.GOLD,"覺得沒被救到嗎?",retrybutton))
+                Task.builder()
+                        .execute{->
+                            Btnlist.remove(random)
+                        }
+                        .delay(300,TimeUnit.SECONDS).submit(main)
                 return CommandResult.affectedEntities(1)
             } else {
                 if (src.setLocationSafely(src.location.add(0.0, 2.0, 0.0))){
+                    Btnlist.add(random)
                     src.sendMessage(Text.of(TextColors.GREEN, "已嘗試自救\n",TextColors.GOLD,"覺得沒被救到嗎?",retrybutton))
+                    Task.builder()
+                            .execute{->
+                                Btnlist.remove(random)
+                            }
+                            .delay(300,TimeUnit.SECONDS).submit(main)
                     return CommandResult.affectedEntities(1)
                 }else {
                     src.sendMessage(Text.of(TextColors.RED, "自救失敗，找不到安全位置"))

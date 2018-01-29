e887fe1daf7f34e2e1b9a603191ea33765cf38a9
jimchen5209 <jimchen5209@gmail.com>
2018-01-29 16:19:09 +0800

    Added retry button to unstuck

--- src/main/kotlin/one/oktw/galaxy/command/UnStuck.kt
+++ src/main/kotlin/one/oktw/galaxy/command/UnStuck.kt
@@ -6,8 +6,12 @@ import org.spongepowered.api.command.args.CommandContext
 import org.spongepowered.api.command.spec.CommandSpec
 import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.text.Text
+import org.spongepowered.api.text.action.TextActions
 import org.spongepowered.api.text.format.TextColors
+import org.spongepowered.api.text.format.TextStyles
+import java.util.*
 
+val Btnlist : ArrayList<UUID> = ArrayList()
 class UnStuck : CommandBase {
     override val spec: CommandSpec
         get() = CommandSpec.builder()
@@ -18,14 +22,34 @@ class UnStuck : CommandBase {
 
     override fun execute(src: CommandSource, args: CommandContext?): CommandResult {
         if (src is Player) {
+            val random = UUID.randomUUID()
+            Btnlist.add(random)
+            val retrybutton = Text.builder("高度提高一點試試")
+                    .color(TextColors.AQUA)
+                    .style(TextStyles.UNDERLINE)
+                    .onClick(TextActions.executeCallback {
+                        if (random in Btnlist) {
+                            Btnlist.remove(random)
+                            if (src.setLocationSafely(src.location.add(0.0, 2.0, 0.0))) {
+                                src.sendMessage(Text.of(TextColors.GREEN, "已嘗試自救"))
+                            } else {
+                                src.sendMessage(Text.of(TextColors.RED, "自救失敗，找不到安全位置"))
+                            }
+                        }
+                    })
+                    .build()
             if (src.setLocationSafely(src.location)) {
-                src.sendMessage(Text.of(TextColors.GREEN, "已嘗試自救"))
+                src.sendMessage(Text.of(TextColors.GREEN, "已嘗試自救\n",TextColors.GOLD,"覺得沒被救到嗎?",retrybutton))
                 return CommandResult.affectedEntities(1)
             } else {
-                src.sendMessage(Text.of(TextColors.RED, "自救失敗，找不到安全位置"))
-                return CommandResult.success()
-            }
-        }
+                if (src.setLocationSafely(src.location.add(0.0, 2.0, 0.0))){
+                    src.sendMessage(Text.of(TextColors.GREEN, "已嘗試自救\n",TextColors.GOLD,"覺得沒被救到嗎?",retrybutton))
+                    return CommandResult.affectedEntities(1)
+                }else {
+                    src.sendMessage(Text.of(TextColors.RED, "自救失敗，找不到安全位置"))
+                    return CommandResult.success()
+                }
+            }}
         return CommandResult.empty()
     }
 }

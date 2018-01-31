3a7164538492da483d5baf8c047c3b9702c55af3
james58899 <james59988@gmail.com>
2018-01-31 15:05:45 +0800

    format

--- src/main/kotlin/one/oktw/galaxy/command/CommandTeleportHereAsk.kt
+++ src/main/kotlin/one/oktw/galaxy/command/CommandTeleportHereAsk.kt
@@ -18,7 +18,7 @@ import java.util.*
 
 class CommandTeleportHereAsk : CommandBase {
 
-    var teleportHereAskTemp = HashMap<UUID,Player>()
+    var teleportHereAskTemp = HashMap<UUID, Player>()
 
     override val spec: CommandSpec
         get() = CommandSpec.builder()
@@ -32,31 +32,31 @@ class CommandTeleportHereAsk : CommandBase {
 
         if (src is Player) {
 
-            if(args.getOne<Player>("Player").isPresent){
+            if (args.getOne<Player>("Player").isPresent) {
 
                 val uuid = UUID.randomUUID()
                 val target = args.getOne<Player>("Player").get()
-                teleportHereAskTemp.put(uuid,target)
+                teleportHereAskTemp.put(uuid, target)
 
                 DelayHelper.Delay(Runnable {
-                    if(teleportHereAskTemp.containsKey(uuid)){
+                    if (teleportHereAskTemp.containsKey(uuid)) {
                         teleportHereAskTemp.remove(uuid)
                     }
-                },300)
+                }, 300)
 
                 val teleportMsg = Text.builder("玩家 ").color(TextColors.YELLOW).append(Text.builder(src.name).color(TextColors.AQUA).build()).append(Text.builder(" 想要傳送你到他的位置，是否接受?").color(TextColors.YELLOW).build())
-                        .append(Text.NEW_LINE).append(Text.builder("接受").onHover(TextActions.showText(Text.of(TextColors.RED,"請勿輕易接受其他人的邀請"))).style(TextStyles.UNDERLINE).color(TextColors.GREEN).onClick(
+                        .append(Text.NEW_LINE).append(Text.builder("接受").onHover(TextActions.showText(Text.of(TextColors.RED, "請勿輕易接受其他人的邀請"))).style(TextStyles.UNDERLINE).color(TextColors.GREEN).onClick(
                         TextActions.executeCallback {
-                            if(teleportHereAskTemp.containsKey(uuid)){
+                            if (teleportHereAskTemp.containsKey(uuid)) {
                                 src.sendMessage(Text.of(TextColors.GREEN, "對方已接受傳送請求"))
                                 target.sendMessage(Text.of(TextColors.GREEN, "已接受傳送請求"))
-                                Teleport(target,src.location)
+                                Teleport(target, src.location)
                                 teleportHereAskTemp.remove(uuid)
                             }
                         }).build())
-                        .append(Text.builder("拒絕").style(TextStyles.UNDERLINE).onHover(TextActions.showText(Text.of(TextColors.RED,"請勿輕易拒絕其他人的邀請"))).color(TextColors.RED).onClick(
+                        .append(Text.builder("拒絕").style(TextStyles.UNDERLINE).onHover(TextActions.showText(Text.of(TextColors.RED, "請勿輕易拒絕其他人的邀請"))).color(TextColors.RED).onClick(
                                 TextActions.executeCallback {
-                                    if(teleportHereAskTemp.containsKey(uuid)){
+                                    if (teleportHereAskTemp.containsKey(uuid)) {
                                         src.sendMessage(Text.of(TextColors.RED, "對方已拒絕傳送請求"))
                                         target.sendMessage(Text.of(TextColors.RED, "已拒絕傳送請求"))
                                         teleportHereAskTemp.remove(uuid)
@@ -65,7 +65,7 @@ class CommandTeleportHereAsk : CommandBase {
                 src.sendMessage(Text.of(TextColors.GREEN, "已傳送請求"))
                 target.sendMessage(teleportMsg)
                 return CommandResult.success()
-            }else{
+            } else {
                 return CommandResult.empty()
             }
 
@@ -76,24 +76,24 @@ class CommandTeleportHereAsk : CommandBase {
     }
 
 
-    private fun Teleport(player : Player, location : Location<World>){
+    private fun Teleport(player: Player, location: Location<World>) {
 
         val first_location = player.location
 
-        player.sendMessage(Text.of(TextColors.YELLOW,"請站在原地不要移動，將在 3 秒後傳送......").toText())
+        player.sendMessage(Text.of(TextColors.YELLOW, "請站在原地不要移動，將在 3 秒後傳送......").toText())
 
         DelayHelper.Delay(Runnable {
-                if (first_location.equals(player.location)) {
-                    if (TeleportHelper.teleport(player,location,false)){
-                        player.sendMessage(Text.of(TextColors.GREEN, "傳送成功").toText())
-                    }else{
-                        player.sendMessage(Text.of(TextColors.RED, "傳送失敗，或許是對方或是你的權限不足").toText())
-                    }
-
+            if (first_location.equals(player.location)) {
+                if (TeleportHelper.teleport(player, location, false)) {
+                    player.sendMessage(Text.of(TextColors.GREEN, "傳送成功").toText())
                 } else {
-                    player.sendMessage(Text.of(TextColors.RED, "位置移動，傳送取消").toText())
+                    player.sendMessage(Text.of(TextColors.RED, "傳送失敗，或許是對方或是你的權限不足").toText())
                 }
-            },3)
+
+            } else {
+                player.sendMessage(Text.of(TextColors.RED, "位置移動，傳送取消").toText())
+            }
+        }, 3)
     }
 
 }
--- src/main/kotlin/one/oktw/galaxy/internal/DelayHelper.kt
+++ src/main/kotlin/one/oktw/galaxy/internal/DelayHelper.kt
@@ -1,17 +1,15 @@
 package one.oktw.galaxy.internal
 
 import one.oktw.galaxy.Main.Companion.main
-import org.spongepowered.api.scheduler.Task;
+import org.spongepowered.api.scheduler.Task
 import java.util.concurrent.TimeUnit
-
-
 
 class DelayHelper {
     companion object {
-        fun Delay( code : Runnable , delay : Long = 3){
+        fun Delay(code: Runnable, delay: Long = 3) {
             Task.builder()
                     .execute(code)
-                    .delay(delay,TimeUnit.SECONDS).submit(main);
-            }
+                    .delay(delay, TimeUnit.SECONDS).submit(main)
         }
+    }
 }

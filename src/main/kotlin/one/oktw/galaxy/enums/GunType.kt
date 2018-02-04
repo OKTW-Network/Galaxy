4575e7ac743c57aa5419d7fbe66c192bd16f4cc2
james58899 <james59988@gmail.com>
2018-02-05 00:20:45 +0800

    more gun

--- src/main/kotlin/one/oktw/galaxy/enums/GunType.kt
+++ src/main/kotlin/one/oktw/galaxy/enums/GunType.kt
@@ -1,5 +1,10 @@
 package one.oktw.galaxy.enums
 
 enum class GunType(val id: Short) {
-    ORIGIN(58)
+    ORIGIN(58),
+    RAINBOW(57),
+    GRAY_ARROW(56),
+    RAINBOW_ARROW(55),
+    GRAY_SLASH(54),
+    RAINBOW_SLASH(53)
 }

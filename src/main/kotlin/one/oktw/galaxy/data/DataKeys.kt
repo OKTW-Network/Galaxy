e86bbcc94ff1baa7febf6ca766336fd01d5c48ec
james58899 <james59988@gmail.com>
2018-02-04 18:05:31 +0800

    fix error

--- src/main/kotlin/one/oktw/galaxy/data/DataKeys.kt
+++ src/main/kotlin/one/oktw/galaxy/data/DataKeys.kt
@@ -1,5 +1,6 @@
 package one.oktw.galaxy.data
 
+import org.spongepowered.api.data.DataQuery
 import org.spongepowered.api.data.key.Key
 import org.spongepowered.api.data.value.mutable.Value
 import org.spongepowered.api.util.TypeTokens
@@ -9,7 +10,8 @@ class DataKeys {
     companion object {
         val UUID: Key<Value<UUID>> = Key.builder()
                 .type(TypeTokens.UUID_VALUE_TOKEN)
-                .id("galaxy:uuid")
+                .query(DataQuery.of("UUID"))
+                .id("uuid")
                 .name("UUID")
                 .build()
     }

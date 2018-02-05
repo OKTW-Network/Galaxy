a689b67a4ec171daf70b8dc7d1a99c85a5d50135
james58899 <james59988@gmail.com>
2018-02-06 01:55:10 +0800

    update gun type

--- src/main/kotlin/one/oktw/galaxy/enums/GunType.kt
+++ src/main/kotlin/one/oktw/galaxy/enums/GunType.kt
@@ -7,5 +7,7 @@ enum class GunType(val id: Short) {
     GRAY_ARROW(55),
     RAINBOW_ARROW(54),
     GRAY_SLASH(53),
-    RAINBOW_SLASH(52)
+    RAINBOW_SLASH(52),
+    GRAY_DASH(51),
+    RAINBOW_DASH(50)
 }

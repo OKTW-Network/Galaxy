6f81e5d7ed77c4a5fc025246a6644de8394f1309
james58899 <james59988@gmail.com>
2018-02-06 14:03:48 +0800

    remove cool down on disconnect

--- src/main/kotlin/one/oktw/galaxy/event/TravelerWatcher.kt
+++ src/main/kotlin/one/oktw/galaxy/event/TravelerWatcher.kt
@@ -2,6 +2,8 @@ package one.oktw.galaxy.event
 
 import one.oktw.galaxy.Main.Companion.travelerManager
 import one.oktw.galaxy.Main.Companion.viewerManager
+import one.oktw.galaxy.helper.CoolDownHelper.Companion.getCoolDown
+import one.oktw.galaxy.helper.CoolDownHelper.Companion.removeCoolDown
 import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.event.Listener
 import org.spongepowered.api.event.filter.Getter
@@ -11,6 +13,7 @@ class TravelerWatcher {
     @Listener
     @Suppress("UNUSED_PARAMETER")
     fun onDisconnect(event: ClientConnectionEvent.Disconnect, @Getter("getTargetEntity") player: Player) {
+        travelerManager.getTraveler(player).item.gun.forEach { getCoolDown(it.uuid)?.let { it1 -> removeCoolDown(it1) } }
         travelerManager.updateTraveler(player)
         viewerManager.removeViewer(player.uniqueId)
     }

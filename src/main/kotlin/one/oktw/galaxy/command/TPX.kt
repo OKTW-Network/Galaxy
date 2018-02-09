2bd9f8805621936091cf40f49ccb8c5382a05efe
jimchen5209 <jimchen5209@gmail.com>
2018-02-09 21:34:04 +0800

    Add tpx world

--- src/main/kotlin/one/oktw/galaxy/command/TPX.kt
+++ src/main/kotlin/one/oktw/galaxy/command/TPX.kt
@@ -1,5 +1,6 @@
 package one.oktw.galaxy.command
 
+import org.spongepowered.api.Sponge
 import org.spongepowered.api.command.CommandResult
 import org.spongepowered.api.command.CommandSource
 import org.spongepowered.api.command.args.CommandContext
@@ -7,21 +8,58 @@ import org.spongepowered.api.command.args.GenericArguments
 import org.spongepowered.api.command.spec.CommandSpec
 import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.text.Text
+import org.spongepowered.api.text.format.TextColors
 
 class TPX : CommandBase {
     override val spec: CommandSpec
         get() = CommandSpec.builder()
                 .executor(this)
-                .arguments(GenericArguments.onlyOne(GenericArguments.player(Text.of("Player"))))
+                .child(TpxPlayer().spec, "player")
+                .child(TpxDim().spec, "dim")
                 .permission("oktw.command.tpx")
                 .build()
 
     override fun execute(src: CommandSource, args: CommandContext): CommandResult {
-        if (src !is Player) return CommandResult.empty()
+        return CommandResult.empty()
+    }
 
-        args.getOne<Player>("Player").ifPresent {
-            src.location = args.getOne<Player>("Player").get().location
+    class TpxPlayer : CommandBase {
+        override val spec: CommandSpec
+            get() = CommandSpec.builder()
+                    .executor(this)
+                    .arguments(GenericArguments.onlyOne(GenericArguments.player(Text.of("Player"))))
+                    .build()
+
+        override fun execute(src: CommandSource, args: CommandContext): CommandResult {
+            if (src !is Player) return CommandResult.empty()
+
+            args.getOne<Player>("Player").ifPresent {
+                src.location = it.location
+            }
+            return CommandResult.success()
         }
-        return CommandResult.success()
+    }
+    class TpxDim : CommandBase {
+        override val spec: CommandSpec
+            get() = CommandSpec.builder()
+                    .executor(this)
+                    .arguments(GenericArguments.onlyOne(GenericArguments.string((Text.of("world")))))
+                    .build()
+
+        override fun execute(src: CommandSource, args: CommandContext): CommandResult {
+            if (src !is Player) return CommandResult.empty()
+            val worldName = args.getOne<String>("world").get()
+            if (!Sponge.getServer().getWorldProperties(worldName).isPresent) {
+                src.sendMessage(Text.of(TextColors.RED,"世界 $worldName 不存在"))
+                return CommandResult.empty()
+            }
+            val world = Sponge.getServer().getWorld(worldName)
+            if (!world.isPresent) {
+                src.sendMessage(Text.of(TextColors.RED,"世界 $worldName 尚未被載入"))
+                return CommandResult.empty()
+            }
+            src.location = world.get().spawnLocation
+            return CommandResult.success()
+        }
     }
 }

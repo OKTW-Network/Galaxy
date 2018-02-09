8bc6cb2b15e72b28f154f2932e5a482b2fd0acef
james58899 <james59988@gmail.com>
2018-02-09 18:50:34 +0800

    clean up

--- src/main/kotlin/one/oktw/galaxy/command/TPX.kt
+++ src/main/kotlin/one/oktw/galaxy/command/TPX.kt
@@ -9,25 +9,19 @@ import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.text.Text
 
 class TPX : CommandBase {
-
     override val spec: CommandSpec
         get() = CommandSpec.builder()
                 .executor(this)
                 .arguments(GenericArguments.onlyOne(GenericArguments.player(Text.of("Player"))))
-                .permission("oktw.command.teleport.x")
+                .permission("oktw.command.tpx")
                 .build()
 
     override fun execute(src: CommandSource, args: CommandContext): CommandResult {
-        if (src is Player) {
-            if (args.getOne<Player>("Player").isPresent) {
-                src.setLocation(args.getOne<Player>("Player").get().location)
-                return CommandResult.success()
-            } else {
-                return CommandResult.empty()
-            }
+        if (src !is Player) return CommandResult.empty()
+
+        args.getOne<Player>("Player").ifPresent {
+            src.location = args.getOne<Player>("Player").get().location
         }
-
-        return CommandResult.empty()
+        return CommandResult.success()
     }
-
 }

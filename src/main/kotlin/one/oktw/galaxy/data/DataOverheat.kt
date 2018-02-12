fa8309c769df515103e123d3e418b8270a19077f
james58899 <james59988@gmail.com>
2018-02-12 09:02:55 +0800

    clean up

--- src/main/kotlin/one/oktw/galaxy/data/DataOverheat.kt
+++ src/main/kotlin/one/oktw/galaxy/data/DataOverheat.kt
@@ -20,9 +20,13 @@ class DataOverheat(overheat: Boolean) : AbstractBooleanData<DataOverheat, DataOv
                 .type(TypeTokens.BOOLEAN_VALUE_TOKEN)
                 .id("overheat")
                 .name("Overheat")
-                .query(DataQuery.of("item", "overheat"))
+                .query(DataQuery.of("overheat"))
                 .build()
     }
+
+    override fun getContentVersion() = 1
+    override fun asImmutable() = Immutable(value)
+    override fun copy() = DataOverheat(value)
 
     override fun from(container: DataContainer): Optional<DataOverheat> {
         return if (container[key.query].isPresent) {
@@ -33,44 +37,19 @@ class DataOverheat(overheat: Boolean) : AbstractBooleanData<DataOverheat, DataOv
         }
     }
 
-    override fun copy(): DataOverheat {
-        return DataOverheat(value)
-    }
-
-    override fun getContentVersion(): Int {
-        return 1
-    }
-
     override fun fill(dataHolder: DataHolder, overlap: MergeFunction): Optional<DataOverheat> {
         value = overlap.merge(this, dataHolder[DataOverheat::class.java].orElse(null)).value
         return Optional.of(this)
     }
 
-    override fun asImmutable(): Immutable {
-        return Immutable(value)
-    }
-
     class Immutable(overheat: Boolean) : AbstractImmutableBooleanData<Immutable, DataOverheat>(overheat, key, false) {
-        override fun getContentVersion(): Int {
-            return 1
-        }
-
-        override fun asMutable(): DataOverheat {
-            return DataOverheat(value)
-        }
+        override fun getContentVersion() = 1
+        override fun asMutable() = DataOverheat(value)
     }
 
     class Builder : AbstractDataBuilder<DataOverheat>(DataOverheat::class.java, 1), DataManipulatorBuilder<DataOverheat, Immutable> {
-        override fun createFrom(dataHolder: DataHolder): Optional<DataOverheat> {
-            return create().fill(dataHolder)
-        }
-
-        override fun create(): DataOverheat {
-            return DataOverheat(false)
-        }
-
-        override fun buildContent(container: DataView): Optional<DataOverheat> {
-            return create().from(container.copy())
-        }
+        override fun createFrom(dataHolder: DataHolder): Optional<DataOverheat> = create().fill(dataHolder)
+        override fun create() = DataOverheat(false)
+        override fun buildContent(container: DataView) = create().from(container.copy())
     }
 }

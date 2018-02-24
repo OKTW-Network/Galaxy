9e30851b1cc3ba2d59cfbea96436163f37c0573c
james58899 <james59988@gmail.com>
2018-02-24 23:28:38 +0800

    init chunkLoader event

--- src/main/kotlin/one/oktw/galaxy/event/ChunkLoader.kt
+++ src/main/kotlin/one/oktw/galaxy/event/ChunkLoader.kt
@@ -1,5 +1,9 @@
 package one.oktw.galaxy.event
 
+import kotlinx.coroutines.experimental.launch
+import one.oktw.galaxy.Main.Companion.chunkLoaderManager
+import one.oktw.galaxy.data.DataUUID
+import one.oktw.galaxy.gui.ChunkLoader
 import org.spongepowered.api.entity.EnderCrystal
 import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.event.Listener
@@ -7,20 +11,29 @@ import org.spongepowered.api.event.entity.InteractEntityEvent
 import org.spongepowered.api.event.entity.SpawnEntityEvent
 import org.spongepowered.api.event.filter.Getter
 import org.spongepowered.api.event.filter.cause.First
-import org.spongepowered.api.text.Text
 
 @Suppress("unused")
 class ChunkLoader {
     @Listener
-    fun onSpawnEntity(event: SpawnEntityEvent, @First player: Player) {
+    fun onSpawnEntity(
+            event: SpawnEntityEvent,
+            @First player: Player
+    ) {
         val enderCrystal = event.entities.firstOrNull { it is EnderCrystal } as? EnderCrystal ?: return
-        // TODO
+
+        launch {
+            val uuid = chunkLoaderManager.addChunkLoader(enderCrystal.location, 0)
+            enderCrystal.offer(DataUUID(uuid))
+        }
     }
 
     @Listener
     @Suppress("UNUSED_PARAMETER")
-    fun onInteractEntity(event: InteractEntityEvent.Secondary.MainHand, @First player: Player, @Getter("getTargetEntity") enderCrystal: EnderCrystal) {
-        player.sendMessage(Text.of(enderCrystal.toString()))
-        // TODO
+    fun onInteractEntity(
+            event: InteractEntityEvent.Secondary.MainHand,
+            @First player: Player,
+            @Getter("getTargetEntity") enderCrystal: EnderCrystal
+    ) {
+        ChunkLoader(enderCrystal[DataUUID.key].orElse(null) ?: return).open(player)
     }
 }

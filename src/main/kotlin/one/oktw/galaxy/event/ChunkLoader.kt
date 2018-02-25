99cc66a15fa4f1685854af5eec6c75f269d8444a
james58899 <james59988@gmail.com>
2018-02-25 23:18:24 +0800

    cancel explosion event

--- src/main/kotlin/one/oktw/galaxy/event/ChunkLoader.kt
+++ src/main/kotlin/one/oktw/galaxy/event/ChunkLoader.kt
@@ -4,8 +4,10 @@ import kotlinx.coroutines.experimental.launch
 import one.oktw.galaxy.Main.Companion.chunkLoaderManager
 import one.oktw.galaxy.data.DataUUID
 import one.oktw.galaxy.gui.ChunkLoader
+import org.spongepowered.api.block.BlockTypes
 import org.spongepowered.api.entity.EnderCrystal
 import org.spongepowered.api.entity.living.player.Player
+import org.spongepowered.api.entity.projectile.DamagingProjectile
 import org.spongepowered.api.event.Listener
 import org.spongepowered.api.event.entity.AttackEntityEvent
 import org.spongepowered.api.event.entity.CollideEntityEvent
@@ -13,34 +15,30 @@ import org.spongepowered.api.event.entity.InteractEntityEvent
 import org.spongepowered.api.event.entity.SpawnEntityEvent
 import org.spongepowered.api.event.filter.Getter
 import org.spongepowered.api.event.filter.cause.First
+import org.spongepowered.api.event.world.ExplosionEvent
 
 @Suppress("unused")
 class ChunkLoader {
     @Listener
-    fun onSpawnEntity(
-            event: SpawnEntityEvent,
-            @First player: Player
-    ) {
+    fun onSpawnEntity(event: SpawnEntityEvent, @First player: Player) {
         val enderCrystal = event.entities.firstOrNull { it is EnderCrystal } as? EnderCrystal ?: return
 
-        launch {
-            val uuid = chunkLoaderManager.addChunkLoader(enderCrystal.location).uuid
-            enderCrystal.offer(DataUUID(uuid))
+        if (enderCrystal.location.add(0.0, -1.0, 0.0).blockType == BlockTypes.OBSIDIAN) {
+            launch {
+                val uuid = chunkLoaderManager.addChunkLoader(enderCrystal.location).uuid
+                enderCrystal.offer(DataUUID(uuid))
+            }
         }
     }
 
     @Listener
     @Suppress("UNUSED_PARAMETER")
-    fun onInteractEntity(
-            event: InteractEntityEvent.Secondary.MainHand,
-            @First player: Player,
-            @Getter("getTargetEntity") enderCrystal: EnderCrystal
-    ) {
-        ChunkLoader(enderCrystal).open(player)
+    fun onInteractEntity(event: InteractEntityEvent.Secondary.MainHand, @First player: Player, @Getter("getTargetEntity") enderCrystal: EnderCrystal) {
+        if (enderCrystal[DataUUID.key].isPresent) ChunkLoader(enderCrystal).open(player)
     }
 
     @Listener
-    fun onAttackEntity(event: AttackEntityEvent, @First player: Player, @Getter("getTargetEntity") enderCrystal: EnderCrystal) {
+    fun onAttackEntity(event: AttackEntityEvent, @Getter("getTargetEntity") enderCrystal: EnderCrystal) {
         if (enderCrystal[DataUUID.key].isPresent) {
             event.isCancelled = true
         }
@@ -48,6 +46,11 @@ class ChunkLoader {
 
     @Listener
     fun onCollideEntity(event: CollideEntityEvent) {
+        if (event.source is DamagingProjectile) event.filterEntities { !(it is EnderCrystal && it[DataUUID.key].isPresent) }
+    }
+
+    @Listener
+    fun onExplosion(event: ExplosionEvent.Detonate) {
         event.filterEntities { !(it is EnderCrystal && it[DataUUID.key].isPresent) }
     }
 }

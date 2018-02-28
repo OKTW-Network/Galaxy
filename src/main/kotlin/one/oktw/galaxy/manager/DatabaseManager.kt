faf919ef36713eabade224ae18cd5335bfe15484
james58899 <james59988@gmail.com>
2018-02-28 22:49:45 +0800

    support sponge data codec

--- src/main/kotlin/one/oktw/galaxy/manager/DatabaseManager.kt
+++ src/main/kotlin/one/oktw/galaxy/manager/DatabaseManager.kt
@@ -7,10 +7,25 @@ import com.mongodb.ServerAddress
 import com.mongodb.client.MongoDatabase
 import one.oktw.galaxy.Main.Companion.configManager
 import one.oktw.galaxy.Main.Companion.main
+import org.bson.BsonReader
+import org.bson.BsonWriter
+import org.bson.codecs.Codec
+import org.bson.codecs.DecoderContext
+import org.bson.codecs.EncoderContext
+import org.bson.codecs.configuration.CodecProvider
 import org.bson.codecs.configuration.CodecRegistries.fromProviders
 import org.bson.codecs.configuration.CodecRegistries.fromRegistries
+import org.bson.codecs.configuration.CodecRegistry
 import org.bson.codecs.pojo.Conventions.*
 import org.bson.codecs.pojo.PojoCodecProvider
+import org.bson.json.JsonMode
+import org.bson.json.JsonReader
+import org.bson.json.JsonWriter
+import org.bson.json.JsonWriterSettings
+import org.spongepowered.api.Sponge
+import org.spongepowered.api.data.DataSerializable
+import org.spongepowered.api.data.persistence.DataFormats
+import java.io.StringWriter
 import java.util.Arrays.asList
 
 class DatabaseManager {
@@ -40,15 +55,17 @@ class DatabaseManager {
         )
 
         val pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
-                fromProviders(PojoCodecProvider.builder()
-                        .register("one.oktw.galaxy.types")
-                        .automatic(true)
-                        .conventions(asList(
-                                SET_PRIVATE_FIELDS_CONVENTION,
-                                ANNOTATION_CONVENTION,
-                                CLASS_AND_PROPERTY_CONVENTION
-                        ))
-                        .build()
+                fromProviders(
+                        SpongeDataCodecProvider(),
+                        PojoCodecProvider.builder()
+                                .register("one.oktw.galaxy.types", "one.oktw.galaxy.types.item")
+//                                .automatic(true)
+                                .conventions(asList(
+                                        SET_PRIVATE_FIELDS_CONVENTION,
+                                        ANNOTATION_CONVENTION,
+                                        CLASS_AND_PROPERTY_CONVENTION
+                                ))
+                                .build()
                 )
         )
 
@@ -68,6 +85,31 @@ class DatabaseManager {
                     credential,
                     MongoClientOptions.builder().codecRegistry(pojoCodecRegistry).build()
             ).getDatabase(config.getNode("name").string)
+        }
+    }
+
+    class SpongeDataCodecProvider : CodecProvider {
+        override fun <T : Any> get(clazz: Class<T>, registry: CodecRegistry): Codec<T>? {
+            if (DataSerializable::class.java.isAssignableFrom(clazz)) {
+                return object : Codec<T> {
+                    override fun getEncoderClass() = clazz
+
+                    override fun encode(writer: BsonWriter, value: T, encoderContext: EncoderContext) {
+                        writer.pipe(JsonReader(DataFormats.JSON.write((value as DataSerializable).toContainer())))
+                    }
+
+                    override fun decode(reader: BsonReader, decoderContext: DecoderContext): T {
+                        val json = StringWriter()
+                        JsonWriter(json, JsonWriterSettings.builder().outputMode(JsonMode.RELAXED).build()).pipe(reader)
+
+                        main.logger.info(json.toString())
+
+                        @Suppress("UNCHECKED_CAST")
+                        return Sponge.getDataManager().deserialize(clazz as Class<out DataSerializable>, DataFormats.JSON.read(json.toString())).get() as T
+                    }
+                }
+            }
+            return null
         }
     }
 }

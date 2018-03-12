f2ea3de7576edc75ba8fc640610fd38c335a8618
james58899 <james59988@gmail.com>
2018-03-12 13:31:09 +0800

    add effect on offer armor

--- src/main/kotlin/one/oktw/galaxy/helper/ArmorHelper.kt
+++ src/main/kotlin/one/oktw/galaxy/helper/ArmorHelper.kt
@@ -2,9 +2,11 @@ package one.oktw.galaxy.helper
 
 import net.minecraft.entity.SharedMonsterAttributes
 import net.minecraft.entity.ai.attributes.AttributeModifier
+import one.oktw.galaxy.Main.Companion.taskManager
 import one.oktw.galaxy.Main.Companion.travelerManager
 import one.oktw.galaxy.data.DataEnable
 import one.oktw.galaxy.data.DataType
+import one.oktw.galaxy.data.DataUUID
 import one.oktw.galaxy.enums.ItemType.ARMOR
 import one.oktw.galaxy.enums.UpgradeType.*
 import org.spongepowered.api.data.key.Keys
@@ -17,12 +19,14 @@ import org.spongepowered.api.item.enchantment.EnchantmentTypes.FIRE_PROTECTION
 import org.spongepowered.api.item.inventory.ItemStack
 import org.spongepowered.api.text.Text
 import org.spongepowered.api.text.format.TextColors
+import org.spongepowered.api.text.format.TextStyles
 import java.util.Arrays.asList
 
 class ArmorHelper {
     companion object {
         fun offerArmor(player: Player) {
             val upgrade = travelerManager.getTraveler(player).armor
+            val armor = taskManager.armor
 
             val helmet: ItemStack = itemHelper(ItemTypes.DIAMOND_HELMET)
             val chestplate: ItemStack = itemHelper(ItemTypes.DIAMOND_CHESTPLATE)
@@ -45,18 +49,24 @@ class ArmorHelper {
                         null
                     )
                 }
+
+                if (level == 5) armor.protect += player.uniqueId
             }
 
             upgrade.firstOrNull { it.type == ADAPT }?.apply {
                 if (level < 3) helmet.offer(
                     Keys.ITEM_ENCHANTMENTS,
                     asList(Enchantment.of(AQUA_AFFINITY, level))
-                )
+                ) else {
+                    armor.water += player.uniqueId
+                }
 
                 if (level in 2..3) chestplate.offer(
                     Keys.ITEM_ENCHANTMENTS,
                     asList(Enchantment.of(FIRE_PROTECTION, level))
-                )
+                ) else if (level > 3) {
+                    armor.fire += player.uniqueId
+                }
 
                 leggings.apply {
                     offer(DataEnable())
@@ -80,8 +90,8 @@ class ArmorHelper {
             val item = ItemStack.builder()
                 .itemType(itemType)
                 .itemData(DataType.Immutable(ARMOR))
-                .add(Keys.DISPLAY_NAME, Text.of(TextColors.GREEN, "科技裝甲"))
-//                .add(Keys.ITEM_ENCHANTMENTS, asList(Enchantment.of(BINDING_CURSE, 1))) // need testing
+                .itemData(DataUUID.Immutable())
+                .add(Keys.DISPLAY_NAME, Text.of(TextColors.YELLOW, TextStyles.BOLD, "科技裝甲"))
                 .add(Keys.UNBREAKABLE, true)
                 .add(Keys.HIDE_UNBREAKABLE, true)
                 .add(Keys.HIDE_MISCELLANEOUS, true)

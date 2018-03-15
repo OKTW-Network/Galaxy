49519444b5ff40504b843d7165a371cc1e8579b6
james58899 <james59988@gmail.com>
2018-03-15 18:29:46 +0800

    add GalaxyInfo

---
+++ src/main/kotlin/one/oktw/galaxy/gui/GalaxyInfo.kt
@@ -0,0 +1,11 @@
+package one.oktw.galaxy.gui
+
+import org.spongepowered.api.item.inventory.Inventory
+import java.util.*
+
+class GalaxyInfo(uuid: UUID) : GUI() {
+    override val token: String
+        get() = TODO("not implemented")
+    override val inventory: Inventory
+        get() = TODO("not implemented")
+}

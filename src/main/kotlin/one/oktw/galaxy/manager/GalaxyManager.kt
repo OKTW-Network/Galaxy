1750b7b03ec21c29e7673fc6d53e402552b79a81
james58899 <james59988@gmail.com>
2018-03-18 20:39:51 +0800

    clean up

--- src/main/kotlin/one/oktw/galaxy/manager/GalaxyManager.kt
+++ src/main/kotlin/one/oktw/galaxy/manager/GalaxyManager.kt
@@ -18,7 +18,7 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 class GalaxyManager {
-    private val galaxyCollection = databaseManager.database.getCollection("Galaxy", Galaxy::class.java)
+    private val collection = databaseManager.database.getCollection("Galaxy", Galaxy::class.java)
 
     fun createGalaxy(name: String, creator: Player, vararg members: UUID): Galaxy {
         val memberList = ArrayList<Member>(members.size + 1)
@@ -27,43 +27,37 @@ class GalaxyManager {
 
         val galaxy = Galaxy(name = name, members = memberList)
 
-        launch { galaxyCollection.insertOne(galaxy) }
+        launch { collection.insertOne(galaxy) }
         return galaxy
     }
 
-    fun saveGalaxy(galaxy: Galaxy) = launch { galaxyCollection.replaceOne(eq("uuid", galaxy.uuid), galaxy) }
+    fun saveGalaxy(galaxy: Galaxy) = launch { collection.replaceOne(eq("uuid", galaxy.uuid), galaxy) }
 
     suspend fun deleteGalaxy(uuid: UUID) {
         getGalaxy(uuid).await()?.planets?.forEach {
             it.world.let { PlanetHelper.removePlanet(it) }
         }
 
-        launch { galaxyCollection.deleteOne(eq("uuid", uuid)) }
+        launch { collection.deleteOne(eq("uuid", uuid)) }
     }
 
-    fun getGalaxy(uuid: UUID): Deferred<Galaxy?> = async { galaxyCollection.find(eq("uuid", uuid)).first() }
+    fun getGalaxy(uuid: UUID): Deferred<Galaxy?> = async { collection.find(eq("uuid", uuid)).first() }
 
-    fun getGalaxy(planet: Planet) = async { galaxyCollection.find(eq("planets.uuid", planet.uuid)).first() }
+    fun getGalaxy(planet: Planet) = async { collection.find(eq("planets.uuid", planet.uuid)).first() }
 
-    fun listGalaxy() = async { galaxyCollection.find().iterator() }
+    fun listGalaxy() = async { collection.find().asSequence() }
 
-    fun listGalaxy(filter: Bson) = async { galaxyCollection.find(filter).iterator() }
+    fun listGalaxy(filter: Bson) = async { collection.find(filter).asSequence() }
 
-    fun listGalaxy(traveler: Traveler) = async {
-        galaxyCollection.find(eq("members.uuid", traveler.uuid)).iterator()
-    }
+    fun listGalaxy(traveler: Traveler) = async { collection.find(eq("members.uuid", traveler.uuid)).asSequence() }
 
-    fun searchGalaxy(keyword: String) = async {
-        val galaxyList = ArrayList<Galaxy>()
-        galaxyCollection.find(text(keyword)).forEach { galaxyList += it }
-        galaxyList
-    }
+    fun searchGalaxy(keyword: String) = async { collection.find(text(keyword)).asSequence() }
 
     fun getPlanet(uuid: UUID) = async {
-        galaxyCollection.distinct("planets", eq("planets.uuid", uuid), Planet::class.java).firstOrNull()
+        collection.distinct("planets", eq("planets.uuid", uuid), Planet::class.java).firstOrNull()
     }
 
     fun getPlanetFromWorld(uuid: UUID) = async {
-        galaxyCollection.distinct("planets", eq("planets.world", uuid), Planet::class.java).firstOrNull()
+        collection.distinct("planets", eq("planets.world", uuid), Planet::class.java).firstOrNull()
     }
 }

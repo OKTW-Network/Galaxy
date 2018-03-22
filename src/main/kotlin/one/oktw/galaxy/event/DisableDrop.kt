f94984ab6c3a44de3ea48e4d4907b4310c9a3665
james58899 <james59988@gmail.com>
2018-03-22 08:58:42 +0800

    cancel plugin item move

--- src/main/kotlin/one/oktw/galaxy/event/DisableDrop.kt
+++ src/main/kotlin/one/oktw/galaxy/event/DisableDrop.kt
@@ -1,16 +1,24 @@
 package one.oktw.galaxy.event
 
 import one.oktw.galaxy.data.DataUUID
-import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.event.Listener
 import org.spongepowered.api.event.Order
-import org.spongepowered.api.event.filter.cause.First
+import org.spongepowered.api.event.item.inventory.ClickInventoryEvent
 import org.spongepowered.api.event.item.inventory.DropItemEvent
+import org.spongepowered.api.item.inventory.InventoryArchetypes
 
 class DisableDrop {
     @Listener(order = Order.FIRST)
-    fun onDropItem(event: DropItemEvent.Pre, @First player: Player) {
-        if (event.droppedItems.any { it[DataUUID.key].isPresent })
+    fun onDropItem(event: DropItemEvent.Pre) {
+        event.droppedItems.any { it[DataUUID.key].isPresent }.let(event::setCancelled)
+    }
+
+    @Listener
+    fun onChangeInventory(event: ClickInventoryEvent) {
+        if (event.targetInventory.archetype == InventoryArchetypes.PLAYER) return
+
+        if (event.cursorTransaction.default[DataUUID.key].isPresent || event.transactions.any { it.default[DataUUID.key].isPresent }) {
             event.isCancelled = true
+        }
     }
 }

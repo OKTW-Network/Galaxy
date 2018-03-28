0c64c5aa77cb93242f43f78a511e364a5053cdec
james58899 <james59988@gmail.com>
2018-03-28 14:36:23 +0800

    TeleportHelper support world as target

--- src/main/kotlin/one/oktw/galaxy/helper/TeleportHelper.kt
+++ src/main/kotlin/one/oktw/galaxy/helper/TeleportHelper.kt
@@ -9,13 +9,30 @@ import org.spongepowered.api.world.World
 
 class TeleportHelper {
     companion object {
-        suspend fun checkValid(player: Player, location: Location<World>): Boolean {
-            val planet = galaxyManager.getPlanetFromWorld(location.extent.uniqueId).await() ?: return false
+        suspend fun checkValid(player: Player, world: World): Boolean {
+            val planet = galaxyManager.getPlanetFromWorld(world.uniqueId).await() ?: return false
 
             return when (planet.checkPermission(player)) {
                 MODIFY, VIEW -> true
                 DENY -> false
             }
+        }
+
+        suspend fun checkValid(player: Player, location: Location<World>) = checkValid(player, location.extent)
+
+        suspend fun teleport(player: Player, world: World): Boolean {
+            if (!checkValid(player, world)) return false
+            if (!player.transferToWorld(world)) return false
+
+            travelerManager.updateTraveler(player)
+
+            if (galaxyManager.getPlanetFromWorld(world.uniqueId).await()?.checkPermission(player) == VIEW) {
+                ViewerHelper.setViewer(player.uniqueId)
+            } else {
+                ViewerHelper.removeViewer(player.uniqueId)
+            }
+
+            return true
         }
 
         suspend fun teleport(player: Player, location: Location<World>, safety: Boolean = false): Boolean {
@@ -33,6 +50,8 @@ class TeleportHelper {
                 } else {
                     ViewerHelper.removeViewer(player.uniqueId)
                 }
+
+                return result
             }
 
             return false

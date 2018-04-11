3856dee0e8f91aa601f62cfc258ae849a8dc6160
james58899 <james59988@gmail.com>
2018-04-11 15:11:48 +0800

    ActionBar service

--- src/main/kotlin/one/oktw/galaxy/traveler/service/ActionBar.kt
+++ src/main/kotlin/one/oktw/galaxy/traveler/service/ActionBar.kt
@@ -1,49 +1,45 @@
 package one.oktw.galaxy.traveler.service
 
 import one.oktw.galaxy.Main.Companion.main
+import one.oktw.galaxy.traveler.data.ActionBarData
 import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.scheduler.Task
 import org.spongepowered.api.text.Text
 import org.spongepowered.api.text.title.Title
-import java.util.*
-import kotlin.collections.HashMap
 
 class ActionBar {
     companion object {
-        private val actionBar = HashMap<Player, Stack<() -> Text?>>()
-        private val lastText = HashMap<Player, Text>()
+        private val actionBar = HashMap<Player, ActionBarData>()
 
         init {
             Task.builder()
-                .intervalTicks(1)
-                .execute(::doTick)
+                .name("ActionBar")
+                .intervalTicks(0)
+                .execute(::tick)
                 .submit(main)
         }
 
-        fun add(player: Player, title: () -> Text?) {
-            actionBar.getOrPut(player) { Stack() } += title
+        fun setActionBar(player: Player, data: ActionBarData) {
+            if (player in actionBar && actionBar[player]!!.priority > data.priority) return
+
+            // show one time
+            player.sendTitle(buildTitle(data.text))
+
+            if (data.time > 0) actionBar[player] = data
         }
 
-        private fun doTick() {
-            actionBar.forEach {
-                val player = it.key
-                val title = it.value
-                while (true) {
-                    if (title.empty()) {
-                        actionBar -= player
-                        break
-                    }
-
-                    val text = title.peek()()
-                    if (text == null) {
-                        title.pop()
-                        continue
-                    } else {
-                        player.sendTitle(Title.builder().actionBar(text).build())
-                        break
-                    }
+        private fun tick() {
+            actionBar.forEach { player, data ->
+                if (data.time == 0) {
+                    actionBar -= player
+                    return@forEach
                 }
+
+                data.time--
+                player.sendTitle(buildTitle(data.text))
             }
         }
+
+        private fun buildTitle(text: Text) = Title.builder().actionBar(text).build()
     }
 }

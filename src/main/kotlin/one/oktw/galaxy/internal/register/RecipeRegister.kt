edb1f093c54214aa707095073b83fc64782f1a08
james58899 <james59988@gmail.com>
2018-05-31 18:58:18 +0800

    add some recipe

--- src/main/kotlin/one/oktw/galaxy/internal/register/RecipeRegister.kt
+++ src/main/kotlin/one/oktw/galaxy/internal/register/RecipeRegister.kt
@@ -1,35 +1,65 @@
 package one.oktw.galaxy.internal.register
 
-import one.oktw.galaxy.Main
+import one.oktw.galaxy.Main.Companion.main
+import one.oktw.galaxy.item.enums.ToolType.WRENCH
+import one.oktw.galaxy.item.enums.UpgradeType
+import one.oktw.galaxy.item.type.Tool
+import one.oktw.galaxy.item.type.Upgrade
 import org.spongepowered.api.Sponge
 import org.spongepowered.api.data.key.Keys
-import org.spongepowered.api.item.ItemTypes
+import org.spongepowered.api.data.type.DyeColors
+import org.spongepowered.api.item.ItemTypes.*
 import org.spongepowered.api.item.inventory.ItemStack
-import org.spongepowered.api.item.recipe.crafting.Ingredient
+import org.spongepowered.api.item.inventory.ItemStackSnapshot
 import org.spongepowered.api.item.recipe.crafting.Ingredient.NONE
 import org.spongepowered.api.item.recipe.crafting.Ingredient.of
 import org.spongepowered.api.item.recipe.crafting.ShapedCraftingRecipe
-import org.spongepowered.api.text.Text
-import org.spongepowered.api.text.format.TextColors
 
 class RecipeRegister {
     init {
-        val ironIngot: Ingredient = of(ItemTypes.IRON_INGOT)
-
-        val wrench = ShapedCraftingRecipe.builder().rows()
-            .row(ironIngot, NONE, ironIngot)
-            .row(NONE, of(ItemStack.of(ItemTypes.STICK, 1)), NONE)
-            .row(NONE, ironIngot, NONE)
-            .result(ItemStack.of(ItemTypes.STICK, 1).apply {
-                offer(
-                    Keys.DISPLAY_NAME,
-                    Text.of(TextColors.GOLD, "Wrench")
-                )
-            }) // TODO
-            .build("wrench", Main.main)
+        val stick = of(STICK)
+        val ironIngot = of(IRON_INGOT)
+        val gold = of(GOLD_INGOT)
+        val diamond = of(DIAMOND)
+        val redstone = of(REDSTONE)
+        val obsidian = of(OBSIDIAN)
+        val quartz = of(QUARTZ)
+        val craftingTable = of(CRAFTING_TABLE)
+        val glassPane = of(GLASS_PANE)
+        val lapis = of(ItemStack.of(DYE, 1).apply { offer(Keys.DYE_COLOR, DyeColors.BLUE) })
 
         Sponge.getRegistry().craftingRecipeRegistry.apply {
-            register(wrench)
+            // Wrench
+            register(
+                ShapedCraftingRecipe.builder().rows()
+                    .row(ironIngot, NONE, ironIngot)
+                    .row(NONE, stick, NONE)
+                    .row(NONE, ironIngot, NONE)
+                    .result(Tool(WRENCH).createItemStack())
+                    .build("wrench", main)
+            )
+
+            // Advance Crafting System
+            register(
+                ShapedCraftingRecipe.builder().rows()
+                    .row(redstone, diamond, lapis)
+                    .row(ironIngot, craftingTable, ironIngot)
+                    .row(lapis, obsidian, redstone)
+                    .result(ItemStackSnapshot.NONE) // TODO custom block
+                    .build("advance_crafting_system", main)
+            )
+
+            // Upgrade base
+            register(
+                ShapedCraftingRecipe.builder().rows()
+                    .row(NONE, gold, NONE)
+                    .row(redstone, glassPane, redstone)
+                    .row(NONE, quartz, NONE)
+                    .result(Upgrade(UpgradeType.BASE).createItemStack())
+                    .build("upgrade_base", main)
+            )
+
+            // TODO upgrade
         }
     }
 }

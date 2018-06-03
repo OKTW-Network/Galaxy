83f7f06a6c1255915ef97451fb93443e5173bdcf
james58899 <james59988@gmail.com>
2018-06-03 19:53:55 +0800

    add name for fake block item

--- src/main/kotlin/one/oktw/galaxy/block/FakeBlockItem.kt
+++ src/main/kotlin/one/oktw/galaxy/block/FakeBlockItem.kt
@@ -2,12 +2,17 @@ package one.oktw.galaxy.block
 
 import one.oktw.galaxy.data.DataBlockType
 import one.oktw.galaxy.data.DataItemType
+import one.oktw.galaxy.internal.LanguageService
+import one.oktw.galaxy.item.ItemUtil.Companion.removeCoolDown
+import one.oktw.galaxy.item.ItemUtil.Companion.removeDamage
 import one.oktw.galaxy.item.enums.ItemType.BLOCK
 import one.oktw.galaxy.item.type.Item
-import org.spongepowered.api.data.key.Keys.ITEM_DURABILITY
-import org.spongepowered.api.data.key.Keys.UNBREAKABLE
+import org.spongepowered.api.data.key.Keys.*
 import org.spongepowered.api.item.ItemTypes.WOODEN_SWORD
 import org.spongepowered.api.item.inventory.ItemStack
+import org.spongepowered.api.text.Text
+import org.spongepowered.api.text.format.TextColors.BLUE
+import org.spongepowered.api.text.format.TextStyles.BOLD
 
 data class FakeBlockItem(private val block: FakeBlocks) : Item {
     override val itemType = BLOCK
@@ -16,9 +21,16 @@ data class FakeBlockItem(private val block: FakeBlocks) : Item {
         .itemType(WOODEN_SWORD)
         .itemData(DataItemType(BLOCK))
         .itemData(DataBlockType(block))
+        .add(DISPLAY_NAME, Text.of(BOLD, BLUE, LanguageService().getString("block.${block.name}")))
         .add(UNBREAKABLE, true)
+        .add(HIDE_UNBREAKABLE, true)
+        .add(HIDE_MISCELLANEOUS, true)
+        .add(HIDE_ATTRIBUTES, true)
+        .add(HIDE_ENCHANTMENTS, true)
         .add(ITEM_DURABILITY, block.id)
         .build()
+        .let(::removeDamage)
+        .let(::removeCoolDown)
 
     override fun displayedItems() = listOfNotNull(createItemStack().createSnapshot())
 

d6cdf30aa5669546fa52fadaed8a1bbdcc5a4e0f
james58899 <james59988@gmail.com>
2018-06-03 13:11:07 +0800

    update Item

--- src/main/kotlin/one/oktw/galaxy/block/FakeBlockItem.kt
+++ src/main/kotlin/one/oktw/galaxy/block/FakeBlockItem.kt
@@ -1,8 +1,10 @@
 package one.oktw.galaxy.block
 
+import one.oktw.galaxy.data.DataType
 import one.oktw.galaxy.item.enums.ItemType.BLOCK
 import one.oktw.galaxy.item.type.Item
-import org.spongepowered.api.data.key.Keys
+import org.spongepowered.api.data.key.Keys.ITEM_DURABILITY
+import org.spongepowered.api.data.key.Keys.UNBREAKABLE
 import org.spongepowered.api.item.ItemTypes.WOODEN_SWORD
 import org.spongepowered.api.item.inventory.ItemStack
 
@@ -11,7 +13,13 @@ data class FakeBlockItem(private val block: FakeBlocks) : Item {
 
     override fun createItemStack(): ItemStack = ItemStack.builder()
         .itemType(WOODEN_SWORD)
-        .add(Keys.UNBREAKABLE, true)
-        .add(Keys.ITEM_DURABILITY, block.id)
+        .add(UNBREAKABLE, true)
+        .add(ITEM_DURABILITY, block.id)
         .build()
+
+    override fun displayedItems() = listOfNotNull(createItemStack().createSnapshot())
+
+    override fun test(itemStack: ItemStack): Boolean {
+        return itemStack[DataType.key].orElse(null) == BLOCK && itemStack[ITEM_DURABILITY].orElse(null) == block.id
+    }
 }

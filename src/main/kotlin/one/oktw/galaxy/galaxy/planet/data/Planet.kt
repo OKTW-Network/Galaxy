978e0a92ee46c3950cba727ac74d5db832d4ebe6
james58899 <james59988@gmail.com>
2018-06-14 10:24:39 +0800

    add effect to planet

--- src/main/kotlin/one/oktw/galaxy/galaxy/planet/data/Planet.kt
+++ src/main/kotlin/one/oktw/galaxy/galaxy/planet/data/Planet.kt
@@ -1,13 +1,18 @@
 package one.oktw.galaxy.galaxy.planet.data
 
 import one.oktw.galaxy.Main.Companion.dummyUUID
+import one.oktw.galaxy.enums.SecurityLevel
+import one.oktw.galaxy.enums.SecurityLevel.VISIT
+import org.spongepowered.api.effect.potion.PotionEffect
 import java.util.*
+import kotlin.collections.ArrayList
 
 data class Planet(
     val uuid: UUID = UUID.randomUUID(),
     var world: UUID = dummyUUID,
     var name: String = "",
     var size: Int = 32,
+    var effect: List<PotionEffect> = ArrayList(),
     var visitable: Boolean = true,
     var lastTime: Date = Date()
 )

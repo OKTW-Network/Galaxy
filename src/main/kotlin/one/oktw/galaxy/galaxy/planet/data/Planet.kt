9c78b1ed4cc268389947aef91f699c701ad3cee8
james58899 <james59988@gmail.com>
2018-06-15 17:07:47 +0800

    add level to planet

--- src/main/kotlin/one/oktw/galaxy/galaxy/planet/data/Planet.kt
+++ src/main/kotlin/one/oktw/galaxy/galaxy/planet/data/Planet.kt
@@ -12,6 +12,7 @@ data class Planet(
     var world: UUID = dummyUUID,
     var name: String = "",
     var size: Int = 32,
+    var level: Short = 1,
     var effect: List<PotionEffect> = ArrayList(),
     var visitable: Boolean = true,
     var lastTime: Date = Date()

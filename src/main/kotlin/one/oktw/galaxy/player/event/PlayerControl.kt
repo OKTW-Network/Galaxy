09a135808ee4a99bf60d3d88f4a25bd3fc3ccd26
james58899 <james59988@gmail.com>
2018-07-02 16:36:18 +0800

    save player data every 1 min and delay 10 sec for every player

--- src/main/kotlin/one/oktw/galaxy/player/event/PlayerControl.kt
+++ src/main/kotlin/one/oktw/galaxy/player/event/PlayerControl.kt
@@ -1,5 +1,6 @@
 package one.oktw.galaxy.player.event
 
+import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
 import kotlinx.coroutines.experimental.withContext
@@ -29,6 +30,7 @@ import org.spongepowered.api.resourcepack.ResourcePack
 import org.spongepowered.api.resourcepack.ResourcePacks
 import org.spongepowered.api.service.user.UserStorageService
 import java.net.URI
+import java.util.concurrent.TimeUnit
 
 class PlayerControl {
     private val lobbyResourcePack: ResourcePack?
@@ -45,6 +47,30 @@ class PlayerControl {
 
         lobbyResourcePack = config.getNode("lobby").string?.let { ResourcePacks.fromUri(URI(it)) }
         planetResourcePack = config.getNode("planet").string?.let { ResourcePacks.fromUri(URI(it)) }
+
+        // Auto save player data every 10 sec
+        launch {
+            val server = Sponge.getServer()
+            var players = server.onlinePlayers.iterator()
+
+            while (true) {
+                if (!players.hasNext()) {
+                    players = server.onlinePlayers.iterator()
+                    delay(1, TimeUnit.MINUTES)
+                    continue
+                }
+
+                val player = players.next()
+
+                galaxyManager.get(player.world).await()?.run {
+                    getMember(player.uniqueId)?.also {
+                        withContext(serverThread) { saveMember(saveTraveler(it, player)) }
+
+                        delay(10, TimeUnit.SECONDS)
+                    }
+                }
+            }
+        }
     }
 
     @Listener

6e1525b7f34ca13782c26aa5aae4f71031d737c3
james58899 <james59988@gmail.com>
2018-07-05 18:46:09 +0800

    fix cocoa

--- src/main/kotlin/one/oktw/galaxy/player/event/Harvest.kt
+++ src/main/kotlin/one/oktw/galaxy/player/event/Harvest.kt
@@ -2,8 +2,8 @@ package one.oktw.galaxy.player.event
 
 import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraft.util.math.BlockPos
+import org.spongepowered.api.block.BlockTypes
 import org.spongepowered.api.data.key.Keys
-import org.spongepowered.api.data.manipulator.immutable.block.ImmutableGrowthData
 import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.event.Listener
 import org.spongepowered.api.event.block.InteractBlockEvent
@@ -13,9 +13,15 @@ class Harvest {
     @Listener
     fun onClick(event: InteractBlockEvent.Secondary.MainHand, @First player: Player) {
         val block = event.targetBlock
-        val grow = block[ImmutableGrowthData::class.java].orElse(null)?.growthStage() ?: return
+        val age = block[Keys.GROWTH_STAGE].orElse(null) ?: return
+        val max = when (block.state.type) {
+            BlockTypes.COCOA -> 2
+            BlockTypes.MELON_STEM -> 8
+            BlockTypes.PUMPKIN_STEM -> 8
+            else -> 7
+        }
 
-        if (grow.get() != grow.maxValue) return
+        if (age != max) return
 
         if ((player as EntityPlayerMP).interactionManager.tryHarvestBlock(block.position.run { BlockPos(x, y, z) })) {
             block.location.get().block = block.state.with(Keys.GROWTH_STAGE, 0).get()

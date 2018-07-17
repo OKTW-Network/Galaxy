3ae08089f462973156c9af37cfd2e95ce6b4ee49
Mmis1000 <mmis10002@gmail.com>
2018-07-17 15:16:13 +0800

    implement auto harvest for melon and pumpkin

--- src/main/kotlin/one/oktw/galaxy/player/event/Harvest.kt
+++ src/main/kotlin/one/oktw/galaxy/player/event/Harvest.kt
@@ -8,24 +8,75 @@ import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.event.Listener
 import org.spongepowered.api.event.block.InteractBlockEvent
 import org.spongepowered.api.event.filter.cause.First
+import org.spongepowered.api.util.Direction
+import org.spongepowered.api.world.gen.populator.Pumpkin
 
 class Harvest {
     @Listener
     fun onClick(event: InteractBlockEvent.Secondary.MainHand, @First player: Player) {
         val block = event.targetBlock
-        val age = block[Keys.GROWTH_STAGE].orElse(null) ?: return
-        val max = when (block.state.type) {
-            COCOA -> 2
-            BEETROOTS -> 3
-            MELON_STEM -> 8
-            PUMPKIN_STEM -> 8
-            else -> 7
-        }
 
-        if (age != max) return
+        when (block.state.type) {
+            PUMPKIN -> {
+                val location = block.location.get();
 
-        if ((player as EntityPlayerMP).interactionManager.tryHarvestBlock(block.position.run { BlockPos(x, y, z) })) {
-            block.location.get().block = block.state.with(Keys.GROWTH_STAGE, 0).get()
+                if (
+                    location.add(0.0, -1.0, 0.0).blockType != GRASS &&
+                    location.add(0.0, -1.0, 0.0).blockType != DIRT &&
+                    location.add(0.0, -1.0, 0.0).blockType != FARMLAND
+                ) {
+                    return
+                }
+
+                if (
+                    location.add(1.0, 0.0, 0.0).blockType != PUMPKIN_STEM &&
+                    location.add(0.0, 0.0, 1.0).blockType != PUMPKIN_STEM &&
+                    location.add(-1.0, 0.0, 0.0).blockType != PUMPKIN_STEM &&
+                    location.add(0.0, 0.0, -1.0).blockType != PUMPKIN_STEM
+                ) {
+                    return
+                }
+
+                (player as EntityPlayerMP).interactionManager.tryHarvestBlock(block.position.run { BlockPos(x, y, z) })
+            }
+            MELON_BLOCK -> {
+                val location = block.location.get();
+
+                if (
+                        location.add(0.0, -1.0, 0.0).blockType != GRASS &&
+                        location.add(0.0, -1.0, 0.0).blockType != DIRT &&
+                        location.add(0.0, -1.0, 0.0).blockType != FARMLAND
+                ) {
+                    return
+                }
+
+                if (
+                        location.add(1.0, 0.0, 0.0).blockType != MELON_STEM &&
+                        location.add(0.0, 0.0, 1.0).blockType != MELON_STEM &&
+                        location.add(-1.0, 0.0, 0.0).blockType != MELON_STEM &&
+                        location.add(0.0, 0.0, -1.0).blockType != MELON_STEM
+                ) {
+                    return
+                }
+
+                (player as EntityPlayerMP).interactionManager.tryHarvestBlock(block.position.run { BlockPos(x, y, z) })
+            }
+            else -> {
+                val age = block[Keys.GROWTH_STAGE].orElse(null) ?: return
+                val max = when (block.state.type) {
+                    COCOA -> 2
+                    BEETROOTS -> 3
+                    PUMPKIN_STEM -> 8 // don't harvest it, we harvest block instead
+                    MELON_STEM -> 8 // don't harvest it, we harvest block instead
+                    else -> 7
+                }
+
+                if (age != max) return
+
+                if ((player as EntityPlayerMP).interactionManager.tryHarvestBlock(block.position.run { BlockPos(x, y, z) })) {
+                    block.location.get().block = block.state.with(Keys.GROWTH_STAGE, 0).get()
+                }
+            }
         }
     }
 }

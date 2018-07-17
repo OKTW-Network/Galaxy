e43266239ba5d6e3d6650ecbf6b993d668c08879
james58899 <james59988@gmail.com>
2018-07-17 18:02:53 +0800

    clean up

--- src/main/kotlin/one/oktw/galaxy/player/event/Harvest.kt
+++ src/main/kotlin/one/oktw/galaxy/player/event/Harvest.kt
@@ -14,7 +14,7 @@ import org.spongepowered.api.world.World
 import java.util.Arrays.asList
 
 class Harvest {
-    fun isNextTo(location: Location<World>, type: BlockType): Boolean {
+    private fun isNextTo(location: Location<World>, type: BlockType): Boolean {
         return location.add(1.0, 0.0, 0.0).blockType == type ||
                 location.add(0.0, 0.0, 1.0).blockType == type ||
                 location.add(-1.0, 0.0, 0.0).blockType == type ||
@@ -27,52 +27,36 @@ class Harvest {
 
         when (block.state.type) {
             PUMPKIN -> {
-                val location = block.location.get();
+                val location = block.location.get()
 
-                if (location.add(0.0, -1.0, 0.0).blockType !in asList(GRASS, DIRT, FARMLAND)) {
-                    return
-                }
-
-                if (!isNextTo(location, PUMPKIN_STEM)) {
-                    return
-                }
+                if (location.add(0.0, -1.0, 0.0).blockType !in asList(GRASS, DIRT, FARMLAND)) return
+                if (!isNextTo(location, PUMPKIN_STEM)) return
 
                 (player as EntityPlayerMP).interactionManager.tryHarvestBlock(block.position.run { BlockPos(x, y, z) })
             }
             MELON_BLOCK -> {
-                val location = block.location.get();
+                val location = block.location.get()
 
-                if (location.add(0.0, -1.0, 0.0).blockType !in asList(GRASS, DIRT, FARMLAND)) {
-                    return
-                }
-
-                if (!isNextTo(location, MELON_STEM)) {
-                    return
-                }
+                if (location.add(0.0, -1.0, 0.0).blockType !in asList(GRASS, DIRT, FARMLAND)) return
+                if (!isNextTo(location, MELON_STEM)) return
 
                 (player as EntityPlayerMP).interactionManager.tryHarvestBlock(block.position.run { BlockPos(x, y, z) })
             }
-            else -> {
-                val age = block[Keys.GROWTH_STAGE].orElse(null) ?: return
-                val max = when (block.state.type) {
-                    COCOA -> 2
-                    BEETROOTS -> 3
-                    PUMPKIN_STEM -> 8 // don't harvest it, we harvest block instead
-                    MELON_STEM -> 8 // don't harvest it, we harvest block instead
-                    else -> 7
-                }
+        }
 
-                if (age != max) return
+        val age = block[Keys.GROWTH_STAGE].orElse(null) ?: return
+        val max = when (block.state.type) {
+            COCOA -> 2
+            BEETROOTS -> 3
+            PUMPKIN_STEM -> 8 // don't harvest it, we harvest block instead
+            MELON_STEM -> 8 // don't harvest it, we harvest block instead
+            else -> 7
+        }
 
-                if (
-                    (player as EntityPlayerMP).interactionManager.tryHarvestBlock(
-                        block.position.run { BlockPos(x, y, z) }
-                    )
-                ) {
+        if (age != max) return
 
-                    block.location.get().block = block.state.with(Keys.GROWTH_STAGE, 0).get()
-                }
-            }
+        if ((player as EntityPlayerMP).interactionManager.tryHarvestBlock(block.position.run { BlockPos(x, y, z) })) {
+            block.location.get().block = block.state.with(Keys.GROWTH_STAGE, 0).get()
         }
     }
 }

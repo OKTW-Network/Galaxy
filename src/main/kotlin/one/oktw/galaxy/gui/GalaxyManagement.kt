54ea85abc9fe079cc08d88b133b4bd46a940a8d9
james58899 <james59988@gmail.com>
2018-07-24 21:17:58 +0800

    using new input method

--- src/main/kotlin/one/oktw/galaxy/gui/GalaxyManagement.kt
+++ src/main/kotlin/one/oktw/galaxy/gui/GalaxyManagement.kt
@@ -8,9 +8,11 @@ import one.oktw.galaxy.galaxy.data.Galaxy
 import one.oktw.galaxy.galaxy.data.extensions.createPlanet
 import one.oktw.galaxy.galaxy.data.extensions.refresh
 import one.oktw.galaxy.galaxy.planet.TeleportHelper
+import one.oktw.galaxy.galaxy.planet.data.extensions.loadWorld
 import one.oktw.galaxy.item.enums.ButtonType.*
 import one.oktw.galaxy.item.type.Button
-import org.spongepowered.api.Sponge
+import one.oktw.galaxy.util.Chat.Companion.confirm
+import one.oktw.galaxy.util.Chat.Companion.input
 import org.spongepowered.api.data.key.Keys
 import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.event.EventListener
@@ -23,9 +25,9 @@ import org.spongepowered.api.item.inventory.property.InventoryTitle
 import org.spongepowered.api.item.inventory.query.QueryOperationTypes
 import org.spongepowered.api.item.inventory.type.GridInventory
 import org.spongepowered.api.text.Text
-import org.spongepowered.api.text.action.TextActions
-import org.spongepowered.api.text.format.TextColors
+import org.spongepowered.api.text.format.TextColors.*
 import org.spongepowered.api.text.format.TextStyles
+import org.spongepowered.api.text.format.TextStyles.BOLD
 import java.util.*
 
 class GalaxyManagement(private val galaxy: Galaxy) : GUI() {
@@ -47,49 +49,49 @@ class GalaxyManagement(private val galaxy: Galaxy) : GUI() {
         Button(PLUS).createItemStack()
             .apply {
                 offer(DataUUID(buttonID[0]))
-                offer(Keys.DISPLAY_NAME, Text.of(TextColors.GREEN, lang["UI.Button.CreateNewPlanet"]))
+                offer(Keys.DISPLAY_NAME, Text.of(GREEN, lang["UI.Button.CreateNewPlanet"]))
             }
             .let { inventory.set(1, 1, it) }
 
         Button(LIST).createItemStack()
             .apply {
                 offer(DataUUID(buttonID[1]))
-                offer(Keys.DISPLAY_NAME, Text.of(TextColors.GREEN, lang["UI.Button.ManageMember"]))
+                offer(Keys.DISPLAY_NAME, Text.of(GREEN, lang["UI.Button.ManageMember"]))
             }
             .let { inventory.set(2, 1, it) }
 
         Button(MEMBER_ADD).createItemStack()
             .apply {
                 offer(DataUUID(buttonID[2]))
-                offer(Keys.DISPLAY_NAME, Text.of(TextColors.GREEN, lang["UI.Button.AddMember"]))
+                offer(Keys.DISPLAY_NAME, Text.of(GREEN, lang["UI.Button.AddMember"]))
             }
             .let { inventory.set(3, 1, it) }
 
         Button(MEMBER_ASK).createItemStack()
             .apply {
                 offer(DataUUID(buttonID[3]))
-                offer(Keys.DISPLAY_NAME, Text.of(TextColors.GREEN, lang["UI.Button.JoinRequestList"]))
+                offer(Keys.DISPLAY_NAME, Text.of(GREEN, lang["UI.Button.JoinRequestList"]))
             }
             .let { inventory.set(4, 1, it) }
 
         Button(WRITE).createItemStack()
             .apply {
                 offer(DataUUID(buttonID[4]))
-                offer(Keys.DISPLAY_NAME, Text.of(TextColors.GREEN, lang["UI.Button.Rename"]))
+                offer(Keys.DISPLAY_NAME, Text.of(GREEN, lang["UI.Button.Rename"]))
             }
             .let { inventory.set(5, 1, it) }
 
         Button(WRITE).createItemStack()
             .apply {
                 offer(DataUUID(buttonID[4]))
-                offer(Keys.DISPLAY_NAME, Text.of(TextColors.GREEN, lang["UI.Button.ChangeGalaxyInfo"]))
+                offer(Keys.DISPLAY_NAME, Text.of(GREEN, lang["UI.Button.ChangeGalaxyInfo"]))
             }
             .let { inventory.set(6, 1, it) }
 
         Button(WRITE).createItemStack()
             .apply {
                 offer(DataUUID(buttonID[4]))
-                offer(Keys.DISPLAY_NAME, Text.of(TextColors.GREEN, lang["UI.Button.ChangeGalaxyNotification"]))
+                offer(Keys.DISPLAY_NAME, Text.of(GREEN, lang["UI.Button.ChangeGalaxyNotification"]))
             }
             .let { inventory.set(7, 1, it) }
 
@@ -105,62 +107,38 @@ class GalaxyManagement(private val galaxy: Galaxy) : GUI() {
         val player = event.source as Player
 
         when (event.cursorTransaction.default[DataUUID.key].orElse(null) ?: return) {
-            buttonID[0] -> {
+            buttonID[0] -> launch {
                 GUIHelper.closeAll(player)
 
                 if (galaxy.planets.size >= 1) {
-                    player.sendMessage(Text.of(TextColors.RED, "目前僅能創建一個星球！請等待日後開放"))
-                    return
+                    player.sendMessage(Text.of(RED, "目前僅能創建一個星球！請等待日後開放"))
+                    return@launch
                 }
 
-                player.sendMessage(Text.of(TextColors.AQUA, "請輸入一個名稱來創建星球："))
-                chatListener = EventListener {
-                    if (it.source == player) {
-                        Sponge.getEventManager().unregisterListeners(chatListener)
-                        it.isCancelled = true
+                val name = input(player, Text.of(AQUA, "請輸入一個名稱來創建星球："))?.toPlain()
 
-                        var lock = false
-                        val name = it.rawMessage.toPlain()
-                        val confirmCallback = TextActions.executeCallback {
-                            if (lock) return@executeCallback
-                            lock = true
-                            launch {
-                                try {
-                                    val planet = galaxy.createPlanet(name)
+                if (name == null) {
+                    player.sendMessage(Text.of(RED, "已取消創建星球"))
+                    return@launch
+                }
 
-                                    player.sendMessage(Text.of(TextColors.YELLOW, "星球創建成功！"))
-                                    TeleportHelper.teleport(player, planet)
-                                    // TODO try time
-                                } catch (e: IllegalArgumentException) {
-                                    player.sendMessage(Text.of(TextColors.RED, "星球創建失敗：", e.message))
-                                }
-                            }
-                        }
-                        val cancelCallback = TextActions.executeCallback {
-                            if (lock) return@executeCallback
-                            player.sendMessage(Text.of(TextColors.RED, "已取消創建星球"))
-                            lock = true
-                        }
+                if (confirm(player, Text.of(AQUA, "確定要創建名為「", TextStyles.RESET, BOLD, name, TextStyles.RESET, AQUA, "」的星球嗎？")) == true) {
+                    val galaxy = galaxy.refresh()
 
-                        player.sendMessage(
-                            Text.of(
-                                TextColors.AQUA, "確定要創建名為「",
-                                TextColors.RESET,
-                                TextStyles.BOLD, name,
-                                TextStyles.RESET,
-                                TextColors.AQUA, "」的星球嗎？\n",
-                                TextColors.GREEN,
-                                TextStyles.UNDERLINE,
-                                TextActions.showText(Text.of(TextColors.GREEN, "確定")), confirmCallback, "確定",
-                                TextStyles.RESET, " ",
-                                TextColors.RED,
-                                TextStyles.UNDERLINE,
-                                TextActions.showText(Text.of(TextColors.RED, "取消")), cancelCallback, "取消"
-                            )
-                        )
+                    if (galaxy.planets.size >= 1) {
+                        player.sendMessage(Text.of(RED, "目前僅能創建一個星球！請等待日後開放"))
+                        return@launch
+                    }
+
+                    try {
+                        val planet = galaxy.createPlanet(name).apply { loadWorld() }
+
+                        player.sendMessage(Text.of(YELLOW, "星球創建成功！"))
+                        TeleportHelper.teleport(player, planet)
+                    } catch (e: IllegalArgumentException) {
+                        player.sendMessage(Text.of(RED, "星球創建失敗：", e.message))
                     }
                 }
-                Sponge.getEventManager().registerListener(main, MessageChannelEvent.Chat::class.java, chatListener)
             }
             buttonID[1] -> GUIHelper.openAsync(player) { BrowserMember(galaxy.refresh(), true) }
             buttonID[2] -> Unit // TODO GUIHelper.open(player) { AddMember() }

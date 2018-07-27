7534ccee768a186bae60476e1fb0046cb6c3d007
james58899 <james59988@gmail.com>
2018-07-27 17:17:03 +0800

    split spawn position fix

--- src/main/kotlin/one/oktw/galaxy/galaxy/planet/gen/populator/Spawn.kt
+++ src/main/kotlin/one/oktw/galaxy/galaxy/planet/gen/populator/Spawn.kt
@@ -12,7 +12,6 @@ import org.spongepowered.api.data.key.Keys
 import org.spongepowered.api.data.type.PortionTypes
 import org.spongepowered.api.text.translation.Translation
 import org.spongepowered.api.util.Direction
-import org.spongepowered.api.world.DimensionTypes.NETHER
 import org.spongepowered.api.world.World
 import org.spongepowered.api.world.extent.Extent
 import org.spongepowered.api.world.gen.Populator
@@ -36,53 +35,31 @@ class Spawn : Populator {
 
     override fun populate(world: World, volume: Extent, random: Random) {
         val spawn = world.spawnLocation.blockPosition
-        val min = volume.blockMin
-        val max = volume.blockMax
-
-        if (spawn.x !in min.x..max.x || spawn.z !in min.z..max.z) return
-
-        var start = spawn
-        val top = if (world.dimension.type == NETHER) 54 else 192
-        for (y in 0..top) {
-            if (volume.getBlockType(start.add(0, y, 0)) == BlockTypes.AIR) {
-                start = start.add(0, y, 0)
-                break
-            }
-        }
-
-        // fix spawn pos
-        world.properties.spawnPosition = start
-
-        // fix world border center
-        world.spawnLocation.run {
-            world.worldBorder.setCenter(x, z)
-            world.properties.setWorldBorderCenter(x, z)
-        }
 
         // clean
-        for (x in start.x - 2..start.x + 2) {
-            for (y in start.y - 1..start.y + 3) {
-                for (z in start.z - 2..start.z + 2) {
+        for (x in spawn.x - 2..spawn.x + 2) {
+            for (y in spawn.y - 1..spawn.y + 3) {
+                for (z in spawn.z - 2..spawn.z + 2) {
                     volume.setBlockType(x, y, z, BlockTypes.AIR)
                 }
             }
         }
 
         // 3x3 iron block
-        for (x in start.x - 1..start.x + 1) {
-            for (z in start.z - 1..start.z + 1) {
-                volume.setBlockType(x, start.y - 1, z, BlockTypes.IRON_BLOCK)
+        for (x in spawn.x - 1..spawn.x + 1) {
+            for (z in spawn.z - 1..spawn.z + 1) {
+                volume.setBlockType(x, spawn.y - 1, z, BlockTypes.IRON_BLOCK)
             }
         }
 
         // pillar
-        for (x in intArrayOf(start.x - 2, start.x + 2)) {
-            for (z in Arrays.asList(start.z - 2, start.z + 2)) {
-                for (y in start.y..start.y + 2) {
+        for (x in intArrayOf(spawn.x - 2, spawn.x + 2)) {
+            for (z in Arrays.asList(spawn.z - 2, spawn.z + 2)) {
+                for (y in spawn.y..spawn.y + 2) {
                     volume.setBlockType(x, y, z, BlockTypes.IRON_BLOCK)
                 }
-                volume.setBlockType(x, start.y - 1, z, BlockTypes.GOLD_BLOCK)
-                volume.setBlockType(x, start.y + 3, z, BlockTypes.DIAMOND_BLOCK)
+                volume.setBlockType(x, spawn.y - 1, z, BlockTypes.GOLD_BLOCK)
+                volume.setBlockType(x, spawn.y + 3, z, BlockTypes.DIAMOND_BLOCK)
             }
         }
 
@@ -94,29 +71,29 @@ class Spawn : Populator {
 
         for (i in -1..1) {
             for (y in intArrayOf(-1, 3)) {
-                volume.setBlock(start.add(i, y, 2), south)
-                volume.setBlock(start.add(i, y, -2), north)
-                volume.setBlock(start.add(2, y, i), east)
-                volume.setBlock(start.add(-2, y, i), west)
+                volume.setBlock(spawn.add(i, y, 2), south)
+                volume.setBlock(spawn.add(i, y, -2), north)
+                volume.setBlock(spawn.add(2, y, i), east)
+                volume.setBlock(spawn.add(-2, y, i), west)
             }
         }
 
         // top
         for (i in 0..1) {
-            volume.setBlock(start.add(-i, 3, 1), north.with(Keys.PORTION_TYPE, PortionTypes.TOP).get()) // 0,1 -1,1
-            volume.setBlock(start.add(i, 3, -1), south.with(Keys.PORTION_TYPE, PortionTypes.TOP).get()) // 0,-1 1,-1
-            volume.setBlock(start.add(1, 3, i), west.with(Keys.PORTION_TYPE, PortionTypes.TOP).get()) // 1,0 1,1
-            volume.setBlock(start.add(-1, 3, -i), east.with(Keys.PORTION_TYPE, PortionTypes.TOP).get()) // -1,0, -1,-1
+            volume.setBlock(spawn.add(-i, 3, 1), north.with(Keys.PORTION_TYPE, PortionTypes.TOP).get()) // 0,1 -1,1
+            volume.setBlock(spawn.add(i, 3, -1), south.with(Keys.PORTION_TYPE, PortionTypes.TOP).get()) // 0,-1 1,-1
+            volume.setBlock(spawn.add(1, 3, i), west.with(Keys.PORTION_TYPE, PortionTypes.TOP).get()) // 1,0 1,1
+            volume.setBlock(spawn.add(-1, 3, -i), east.with(Keys.PORTION_TYPE, PortionTypes.TOP).get()) // -1,0, -1,-1
         }
-        volume.setBlockType(start.add(0, 3, 0), BlockTypes.GLASS)
+        volume.setBlockType(spawn.add(0, 3, 0), BlockTypes.GLASS)
 
         // beacon
-        volume.setBlockType(start, BlockTypes.BEACON)
+        volume.setBlockType(spawn, BlockTypes.BEACON)
         launch(serverThread) {
             galaxyManager.get(world)?.getPlanet(world)?.uuid?.apply {
-                volume.offer(start, DataBlockType(PLANET_TERMINAL))
-                volume.offer(start, DataUUID(this))
-                volume.addScheduledUpdate(start, 0, 0)
+                volume.offer(spawn, DataBlockType(PLANET_TERMINAL))
+                volume.offer(spawn, DataUUID(this))
+                volume.addScheduledUpdate(spawn, 0, 0)
             }
         }
     }
---
+++ src/main/kotlin/one/oktw/galaxy/galaxy/planet/gen/populator/SpawnPosFix.kt
@@ -0,0 +1,51 @@
+package one.oktw.galaxy.galaxy.planet.gen.populator
+
+import com.flowpowered.math.vector.Vector3i
+import org.spongepowered.api.block.BlockTypes
+import org.spongepowered.api.text.translation.Translation
+import org.spongepowered.api.world.World
+import org.spongepowered.api.world.extent.Extent
+import org.spongepowered.api.world.gen.Populator
+import org.spongepowered.api.world.gen.PopulatorType
+import java.util.*
+
+class SpawnPosFix(private val minY: Int = 64, private val maxY: Int = 256) : Populator {
+    override fun getType() = object : PopulatorType {
+        override fun getTranslation() = object : Translation {
+            override fun getId() = "spawn_position_fix"
+
+            override fun get(locale: Locale) = "Spawn position fix"
+
+            override fun get(locale: Locale, vararg args: Any) = "Spawn position fix"
+        }
+
+        override fun getName() = "Spawn position fix"
+
+        override fun getId() = "spawn_position_fix"
+    }
+
+    override fun populate(world: World, volume: Extent, random: Random) {
+        val spawn = world.spawnLocation.blockPosition
+        val min = volume.blockMin
+        val max = volume.blockMax
+
+        if (spawn.x !in min.x..max.x || spawn.z !in min.z..max.z) return
+
+        var pos = Vector3i(spawn.x, 0, spawn.z)
+        for (y in minY..maxY) {
+            if (volume.getBlockType(pos.add(0, y, 0)) == BlockTypes.AIR) {
+                pos = pos.add(0, y, 0)
+                break
+            }
+        }
+
+        // fix spawn pos
+        world.properties.spawnPosition = pos
+
+        // fix world border center
+        world.spawnLocation.run {
+            world.worldBorder.setCenter(x, z)
+            world.properties.setWorldBorderCenter(x, z)
+        }
+    }
+}

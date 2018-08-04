b031a12269e7f103cb1e0f60e5c48c773a232ad3
Mmis1000 <mmis10002@gmail.com>
2018-08-05 00:28:09 +0800

    add `all` catalog

--- src/main/kotlin/one/oktw/galaxy/recipe/Recipes.kt
+++ src/main/kotlin/one/oktw/galaxy/recipe/Recipes.kt
@@ -3,9 +3,12 @@ package one.oktw.galaxy.recipe
 import one.oktw.galaxy.Main
 import one.oktw.galaxy.block.data.FakeBlockItem
 import one.oktw.galaxy.block.enums.CustomBlocks
+import one.oktw.galaxy.item.enums.ButtonType
 import one.oktw.galaxy.item.enums.MaterialType
 import one.oktw.galaxy.item.enums.ToolType
+import one.oktw.galaxy.item.type.Button
 import one.oktw.galaxy.item.type.Material
+import one.oktw.galaxy.item.type.Gun
 import one.oktw.galaxy.item.type.Tool
 import org.spongepowered.api.item.ItemTypes
 import org.spongepowered.api.item.inventory.ItemStackSnapshot
@@ -17,9 +20,11 @@ class Recipes {
         private val lang = Main.languageService.getDefaultLanguage()
 
         enum class Type {
+            ALL,
             TOOL,
             MATERIAL,
-            MACHINE
+            MACHINE,
+            WEAPON
         }
 
         private val materials: List<HiTechCraftingRecipe> = asList(
@@ -58,24 +63,34 @@ class Recipes {
                 .build()
         )
 
+        private val weapons: List<HiTechCraftingRecipe> = asList()
+
+        private val all: List<HiTechCraftingRecipe> = ArrayList<HiTechCraftingRecipe>().plus(tools).plus(materials).plus(machines).plus(weapons)
+
         val catalog: Map<Type, List<HiTechCraftingRecipe>> = mapOf(
+            Type.ALL to all,
             Type.TOOL to tools,
             Type.MATERIAL to materials,
-            Type.MACHINE to machines
+            Type.MACHINE to machines,
+            Type.WEAPON to weapons
         )
 
         val icons: Map<Type, ItemStackSnapshot> = mapOf(
+            Type.ALL to Button(ButtonType.PLUS).createItemStack().createSnapshot(),
             Type.TOOL to Tool(ToolType.WRENCH).createItemStack().createSnapshot(),
             Type.MATERIAL to Material(MaterialType.PART_RAW_BASE).createItemStack().createSnapshot(),
-            Type.MACHINE to FakeBlockItem(CustomBlocks.ELEVATOR).createItemStack().createSnapshot()
+            Type.MACHINE to FakeBlockItem(CustomBlocks.ELEVATOR).createItemStack().createSnapshot(),
+            Type.WEAPON to Gun().createItemStack().createSnapshot()
         )
 
         val names: Map<Type, String> = mapOf(
+            Type.ALL to lang["recipe.catalog.ALL"],
             Type.TOOL to lang["recipe.catalog.TOOL"],
             Type.MATERIAL to lang["recipe.catalog.MATERIAL"],
-            Type.MACHINE to lang["recipe.catalog.MACHINE"]
+            Type.MACHINE to lang["recipe.catalog.MACHINE"],
+            Type.WEAPON to lang["recipe.catalog.WEAPON"]
         )
 
-        val types: List<Type> = asList(Type.MACHINE, Type.TOOL, Type.MATERIAL)
+        val types: List<Type> = asList(Type.ALL, Type.MACHINE, Type.TOOL, Type.WEAPON, Type.MATERIAL)
     }
 }

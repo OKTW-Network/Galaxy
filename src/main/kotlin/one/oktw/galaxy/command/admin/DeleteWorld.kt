9da2eb20f41897cab378c3a5f48b990aada439e6
jimchen5209 <jimchen5209@gmail.com>
2018-08-31 20:34:19 +0800

    Tab completing world

--- src/main/kotlin/one/oktw/galaxy/command/admin/DeleteWorld.kt
+++ src/main/kotlin/one/oktw/galaxy/command/admin/DeleteWorld.kt
@@ -7,7 +7,6 @@ import one.oktw.galaxy.command.CommandBase
 import one.oktw.galaxy.galaxy.data.extensions.getPlanet
 import one.oktw.galaxy.galaxy.data.extensions.removePlanet
 import one.oktw.galaxy.galaxy.planet.PlanetHelper
-import org.spongepowered.api.Sponge
 import org.spongepowered.api.command.CommandResult
 import org.spongepowered.api.command.CommandSource
 import org.spongepowered.api.command.args.CommandContext
@@ -15,37 +14,34 @@ import org.spongepowered.api.command.args.GenericArguments
 import org.spongepowered.api.command.spec.CommandSpec
 import org.spongepowered.api.text.Text
 import org.spongepowered.api.text.format.TextColors
-import java.util.*
+import org.spongepowered.api.world.storage.WorldProperties
 
 class DeleteWorld : CommandBase {
     override val spec: CommandSpec = CommandSpec.builder()
         .permission("oktw.command.admin.deleteWorld")
-        .arguments(GenericArguments.uuid(Text.of("world")))
+        .arguments(GenericArguments.world(Text.of("World")))
         .executor(this)
         .build()
 
     override fun execute(src: CommandSource, args: CommandContext): CommandResult {
-        val server = Sponge.getServer()
         launch {
-            val uuid = args.getOne<UUID>("world").get()
-            if (server.getWorldProperties(uuid).isPresent) {
-                val properties = server.getWorldProperties(uuid).get()
-                val galaxy = galaxyManager.get(properties)
-                if (galaxy != null) {
-                    src.sendMessage(Text.of(TextColors.GREEN, "Planet found!Removing planet instead."))
-                    val planet = galaxy.getPlanet(properties)?.uuid
-                    if (planet != null) {
-                        galaxy.removePlanet(planet)
-                        src.sendMessage(Text.of(TextColors.GREEN, "Planet deleted!"))
-                        return@launch
-                    }
-                }
-                if (PlanetHelper.removePlanet(uuid).await()) {
-                    src.sendMessage(Text.of(TextColors.RED, "World deleted!"))
-                } else {
-                    src.sendMessage(Text.of(TextColors.RED, "Failed!"))
+            val world = args.getOne<WorldProperties>("World").get()
+            val galaxy = galaxyManager.get(world)
+            if (galaxy != null) {
+                src.sendMessage(Text.of(TextColors.GREEN, "Planet found!Removing planet instead."))
+                val planet = galaxy.getPlanet(world)?.uuid
+                if (planet != null) {
+                    galaxy.removePlanet(planet)
+                    src.sendMessage(Text.of(TextColors.GREEN, "Planet deleted!"))
+                    return@launch
                 }
             }
+            if (PlanetHelper.removePlanet(world.uniqueId).await()) {
+                src.sendMessage(Text.of(TextColors.RED, "World deleted!"))
+            } else {
+                src.sendMessage(Text.of(TextColors.RED, "Failed!"))
+            }
+
         }
         return CommandResult.success()
     }
--- src/main/kotlin/one/oktw/galaxy/command/admin/UnloadWorld.kt
+++ src/main/kotlin/one/oktw/galaxy/command/admin/UnloadWorld.kt
@@ -9,18 +9,18 @@ import org.spongepowered.api.command.args.GenericArguments
 import org.spongepowered.api.command.spec.CommandSpec
 import org.spongepowered.api.text.Text
 import org.spongepowered.api.text.format.TextColors
-import java.util.*
+import org.spongepowered.api.world.storage.WorldProperties
 
 class UnloadWorld : CommandBase {
     override val spec: CommandSpec = CommandSpec.builder()
         .permission("oktw.command.admin.unloadWorld")
-        .arguments(GenericArguments.uuid(Text.of("world")))
+        .arguments(GenericArguments.world(Text.of("World")))
         .executor(this)
         .build()
 
     override fun execute(src: CommandSource, args: CommandContext): CommandResult {
         val server = Sponge.getServer()
-        server.getWorld(args.getOne<UUID>("world").get()).ifPresent {
+        server.getWorld(args.getOne<WorldProperties>("World").get().uniqueId).ifPresent {
             if (server.unloadWorld(it)) {
                 src.sendMessage(Text.of(TextColors.GREEN, "World unloaded!"))
             } else {

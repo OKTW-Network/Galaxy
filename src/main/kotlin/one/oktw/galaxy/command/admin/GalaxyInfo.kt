34c832574302d765f3e2729c2a905d4c73f5fb86
jimchen5209 <jimchen5209@gmail.com>
2018-09-08 21:26:46 +0800

    Missing exception handler

--- src/main/kotlin/one/oktw/galaxy/command/admin/GalaxyInfo.kt
+++ src/main/kotlin/one/oktw/galaxy/command/admin/GalaxyInfo.kt
@@ -3,6 +3,7 @@ package one.oktw.galaxy.command.admin
 import kotlinx.coroutines.experimental.launch
 import one.oktw.galaxy.command.CommandBase
 import one.oktw.galaxy.command.CommandHelper
+import org.spongepowered.api.Sponge
 import org.spongepowered.api.command.CommandResult
 import org.spongepowered.api.command.CommandSource
 import org.spongepowered.api.command.args.CommandContext
@@ -10,6 +11,7 @@ import org.spongepowered.api.command.args.GenericArguments
 import org.spongepowered.api.command.spec.CommandSpec
 import org.spongepowered.api.service.pagination.PaginationList
 import org.spongepowered.api.text.Text
+import org.spongepowered.api.text.format.TextColors
 import java.util.*
 
 class GalaxyInfo : CommandBase {
@@ -21,21 +23,28 @@ class GalaxyInfo : CommandBase {
 
     override fun execute(src: CommandSource, args: CommandContext): CommandResult {
         val uuid = args.getOne<UUID>("galaxy").orElse(null)
-        launch {
-            val galaxy = CommandHelper.getGalaxy(uuid, src)
+        try {
+            launch {
+                val galaxy = CommandHelper.getGalaxy(uuid, src)
 
-            PaginationList.builder()
-                .contents(
-                    Text.of("Name: ", galaxy.name),
-                    Text.of("UUID: ", galaxy.uuid),
-                    Text.of("Info: ", galaxy.info),
-                    Text.of("Notice: ", galaxy.notice),
-                    Text.of("Star dust: ", galaxy.starDust),
-                    Text.of("Planet: ", galaxy.planets),
-                    Text.of("Member: ", galaxy.members)
-                )
-                .title(Text.of("Galaxy Info"))
-                .sendTo(src)
+                PaginationList.builder()
+                    .contents(
+                        Text.of("Name: ", galaxy.name),
+                        Text.of("UUID: ", galaxy.uuid),
+                        Text.of("Info: ", galaxy.info),
+                        Text.of("Notice: ", galaxy.notice),
+                        Text.of("Star dust: ", galaxy.starDust),
+                        Text.of("Planet: ", galaxy.planets),
+                        Text.of("Member: ", galaxy.members)
+                    )
+                    .title(Text.of("Galaxy Info"))
+                    .sendTo(src)
+            }
+        } catch (e: IllegalArgumentException) {
+            src.sendMessage(Text.of(TextColors.RED, "Error: ", e.message))
+            if (e.message == "Not enough arguments!") {
+                src.sendMessage(Text.of(TextColors.RED, Sponge.getCommandManager().getUsage(src)))
+            }
         }
         return CommandResult.success()
     }

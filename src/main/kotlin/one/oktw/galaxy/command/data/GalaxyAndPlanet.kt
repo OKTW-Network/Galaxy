99cd0994724b63f5cb57c4c221b2498b6e46313f
jimchen5209 <jimchen5209@gmail.com>
2018-09-10 20:36:51 +0800

    A file is not deleted in last commit

--- src/main/kotlin/one/oktw/galaxy/command/data/GalaxyAndPlanet.kt
+++
@@ -1,7 +0,0 @@
-package one.oktw.galaxy.command.data
-
-import one.oktw.galaxy.galaxy.data.Galaxy
-import one.oktw.galaxy.galaxy.planet.data.Planet
-import java.util.*
-
-data class GalaxyAndPlanet(val galaxy: Galaxy, val planet: Planet, val planetUUID: UUID)

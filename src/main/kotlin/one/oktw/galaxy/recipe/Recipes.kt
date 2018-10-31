8517f340abedd93e8293e79a20a5fac8ad859e73
mmis1000 <mmis10002@gmail.com>
2018-10-31 13:32:00 +0800

    Add 2018 halloween event item

--- src/main/kotlin/one/oktw/galaxy/recipe/Recipes.kt
+++ src/main/kotlin/one/oktw/galaxy/recipe/Recipes.kt
@@ -1,5 +1,6 @@
 package one.oktw.galaxy.recipe
 
+import net.minecraft.entity.ai.attributes.AttributeModifier
 import one.oktw.galaxy.Main
 import one.oktw.galaxy.block.data.FakeBlockItem
 import one.oktw.galaxy.block.enums.CustomBlocks
@@ -13,9 +14,14 @@ import one.oktw.galaxy.item.type.Tool
 import org.spongepowered.api.data.key.Keys
 import org.spongepowered.api.data.type.DyeColors
 import org.spongepowered.api.item.ItemTypes
+import org.spongepowered.api.item.enchantment.Enchantment
+import org.spongepowered.api.item.enchantment.EnchantmentTypes
 import org.spongepowered.api.item.inventory.ItemStack
 import org.spongepowered.api.item.inventory.ItemStackSnapshot
 import org.spongepowered.api.item.recipe.crafting.Ingredient.of
+import org.spongepowered.api.text.Text
+import org.spongepowered.api.text.format.TextColors
+import org.spongepowered.api.text.format.TextStyles
 import org.spongepowered.api.text.translation.Translation
 import java.util.Arrays.asList
 
@@ -80,7 +86,55 @@ class Recipes {
 
         private val creativeMachines = machines
 
-        private val weapons: List<HiTechCraftingRecipe> = ArrayList()
+        private val weapons: List<HiTechCraftingRecipe> = asList(
+            // TODO, remove this after event
+            HiTechCraftingRecipe.builder()
+                .add(of(ItemTypes.DIAMOND_SWORD), 1)
+                .add(of(ItemTypes.PUMPKIN), 6)
+                .cost(666)
+                .result(ItemStack.of(ItemTypes.DIAMOND_SWORD).apply {
+                    offer(Keys.DISPLAY_NAME, lang.ofPlaceHolder(
+                        TextStyles.BOLD,
+
+                        TextColors.YELLOW,
+                        lang.of("item.event.halloween2018.undeadKiller"))
+                    )
+
+                    offer(Keys.ITEM_LORE, asList<Text>(
+                        lang.ofPlaceHolder(TextColors.DARK_GRAY, lang.of("item.event.halloween2018.undeadKillerLore"))
+                    ))
+
+                    offer(Keys.UNBREAKABLE, true)
+                    offer(Keys.HIDE_UNBREAKABLE, true)
+                    offer(Keys.HIDE_MISCELLANEOUS, true)
+                    offer(Keys.HIDE_ATTRIBUTES, true)
+
+                    // id == 127
+                    offer(Keys.ITEM_DURABILITY, 127)
+                    offer(Keys.ITEM_ENCHANTMENTS, asList(
+                        Enchantment.of(EnchantmentTypes.SMITE, 5)
+                    ))
+
+                    // TODO, vanilla, change to sponge api if possible
+                    @Suppress("CAST_NEVER_SUCCEEDS")
+                    val vanillaItem = this as net.minecraft.item.ItemStack
+
+                    vanillaItem.repairCost = 40
+
+                    vanillaItem.addAttributeModifier(
+                        "generic.attackDamage",
+                        AttributeModifier("generic.attackDamage", 4.0, 0),
+                        null
+                    )
+
+                    vanillaItem.addAttributeModifier(
+                        "generic.attackSpeed",
+                        AttributeModifier("generic.attackSpeed", -2.2, 0),
+                        null
+                    )
+                })
+                .build()
+        )
 
         private val creativeWeapons = weapons
 

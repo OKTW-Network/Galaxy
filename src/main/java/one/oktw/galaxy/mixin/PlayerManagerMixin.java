de018aa131a8cc0603c1932c4ce563a6b975051e
Mmis1000 <mmis10002@gmail.com>
2019-05-28 00:18:14 +0800

    Add chat hook mixin

---
+++ src/main/java/one/oktw/galaxy/mixin/PlayerManagerMixin.java
@@ -0,0 +1,38 @@
+/*
+ * OKTW Galaxy Project
+ * Copyright (C) 2018-2019
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.oktw.galaxy.mixin;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.PlayerManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerManager.class)
+public class PlayerManagerMixin {
+    @Inject(
+        method = "Lnet/minecraft/server/PlayerManager;broadcastChatMessage(Lnet/minecraft/network/chat/Component;Z)V",
+        at = @At("HEAD")
+    )
+    public void onBroadcastChatMessage(Component component_1, boolean isSystemMessage, CallbackInfo ci) {
+        if (isSystemMessage) return;
+        ci.cancel();
+    }
+}

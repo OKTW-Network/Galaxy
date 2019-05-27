a07e0bfa69b3782bc1b23da6b1cacfaab1207270
james58899 <james59988@gmail.com>
2019-05-27 22:08:24 +0800

    Fabric init

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories {
+        jcenter()
+        maven(url = "https://maven.fabricmc.net") {
+            name = "Fabric"
+        }
+        gradlePluginPortal()
+    }
+}
---
+++ src/main/kotlin/one/oktw/galaxy/Main.kt
@@ -0,0 +1,28 @@
+/*
+ * OKTW Galaxy Project
+ * Copyright (C) 2018-2019
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.oktw.galaxy
+
+import net.fabricmc.api.ModInitializer
+
+@Suppress("unused")
+class Main : ModInitializer {
+    override fun onInitialize() {
+        TODO("not implemented")
+    }
+}

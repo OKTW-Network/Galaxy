c8a73e067ffc640c059319396ced586209ef0d0d
james58899 <james59988@gmail.com>
2019-06-08 12:23:05 +0800

    Set kotlin JVM target version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     //    "maven-publish"
     kotlin("jvm") version "1.3.31"
@@ -25,6 +27,10 @@ base {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 minecraft {

14873cefc671dd3f1e0a6357ee76fb6157546c81
Mmis1000 <mmis10002@gmail.com>
2019-06-13 23:43:25 +0800

    Send the packet to proxy in json format via plugin channel

--- src/main/java/one/oktw/galaxy/mixin/PlayerManagerMixin.java
+++ src/main/java/one/oktw/galaxy/mixin/PlayerManagerMixin.java
@@ -18,21 +18,51 @@
 
 package one.oktw.galaxy.mixin;
 
+import com.google.common.collect.Lists;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import net.minecraft.client.network.packet.CustomPayloadS2CPacket;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.PacketByteBuf;
+import one.oktw.galaxy.proxy.api.ProxyAPI;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @Mixin(PlayerManager.class)
-public class PlayerManagerMixin {
+abstract class PlayerManagerMixin {
+    @Final
+    @Shadow()
+    private final List<ServerPlayerEntity> players = Lists.newArrayList();
+
+    private static final Identifier GALAXY_CHAT = new Identifier("galaxy/chat");
     @Inject(
-        method = "Lnet/minecraft/server/PlayerManager;broadcastChatMessage(Lnet/minecraft/network/chat/Component;Z)V",
+        method = "broadcastChatMessage(Lnet/minecraft/network/chat/Component;Z)V",
         at = @At("HEAD")
     )
     public void onBroadcastChatMessage(Component component_1, boolean isSystemMessage, CallbackInfo ci) {
         if (isSystemMessage) return;
         ci.cancel();
+
+        ByteBuf content = Unpooled.buffer();
+        PacketByteBuf buff = new PacketByteBuf(content);
+        buff.writeBytes(ProxyAPI.INSTANCE.encode(TextComponent.Serializer.toJson(component_1)));
+
+        CustomPayloadS2CPacket packet = new CustomPayloadS2CPacket(GALAXY_CHAT, buff);
+
+        if (players.size() > 0) {
+            ServerPlayerEntity player = players.get(0);
+
+            player.networkHandler.sendPacket(packet);
+        }
     }
 }

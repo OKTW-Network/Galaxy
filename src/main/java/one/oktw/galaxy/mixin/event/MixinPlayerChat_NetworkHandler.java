1086b8246d72ff8e3ecf00dfc0db0d000287ac5f
jimchen5209 <jimchen5209@gmail.com>
2019-07-15 23:14:14 +0800

    add Custom Payload Mixin

--- src/main/java/one/oktw/galaxy/mixin/event/MixinPlayerChat_NetworkHandler.java
+++ src/main/java/one/oktw/galaxy/mixin/event/MixinPlayerChat_NetworkHandler.java
@@ -21,13 +21,18 @@ package one.oktw.galaxy.mixin.event;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.network.packet.CustomPayloadC2SPacket;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import one.oktw.galaxy.Main;
 import one.oktw.galaxy.event.type.PlayerChatEvent;
+import one.oktw.galaxy.network.CustomPayloadC2SPacketAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class MixinPlayerChat_NetworkHandler {
@@ -41,5 +46,11 @@ public class MixinPlayerChat_NetworkHandler {
         if (main == null || !main.getEventManager().emit(new PlayerChatEvent(player, message)).getCancel()) {
             playerManager.broadcastChatMessage(message, isSystem);
         }
+    }
+
+    @Inject(method = "onCustomPayload", at = @At("HEAD"), cancellable = true)
+    public void onCustomPayload(CustomPayloadC2SPacket packet, CallbackInfo info) {
+        Identifier channel = ((CustomPayloadC2SPacketAccessor) packet).getChannel();
+
     }
 }

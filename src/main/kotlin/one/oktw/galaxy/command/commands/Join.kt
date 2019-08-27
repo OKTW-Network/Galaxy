0483e80e1d9571236ff45fb783621962dcb6f5f0
jimchen5209 <jimchen5209@gmail.com>
2019-08-27 21:26:56 +0800

    Change message text to galaxy theme and add tips

--- src/main/kotlin/one/oktw/galaxy/command/commands/Join.kt
+++ src/main/kotlin/one/oktw/galaxy/command/commands/Join.kt
@@ -78,15 +78,16 @@ class Join : Command, CoroutineScope by CoroutineScope(Dispatchers.Default + Sup
     private fun execute(source: ServerCommandSource, collection: Collection<GameProfile>): Int {
         val sourcePlayer = source.player
         if (!lock.getOrPut(sourcePlayer, { Mutex() }).tryLock()) {
-            source.sendFeedback(LiteralText("請稍後...").styled { style -> style.color = Formatting.YELLOW }, false)
+            val starting = starting[sourcePlayer] ?: false
+            source.sendFeedback(LiteralText(if (starting) "飛船正在飛向星系請稍後..." else "請稍後...").styled { style -> style.color = Formatting.AQUA }, false)
             return com.mojang.brigadier.Command.SINGLE_SUCCESS
         }
 
         val targetPlayer = collection.first()
 
         sourcePlayer.networkHandler.sendPacket(CustomPayloadS2CPacket(PROXY_IDENTIFIER, PacketByteBuf(wrappedBuffer(encode(CreateGalaxy(targetPlayer.id))))))
-        val text = LiteralText(if (sourcePlayer.gameProfile == targetPlayer) "正在加入您的星系" else "正在加入 ${targetPlayer.name} 的星系").styled { style ->
-            style.color = Formatting.YELLOW
+        val text = LiteralText(if (sourcePlayer.gameProfile == targetPlayer) "已將飛船目的地設為您的星系" else "已將飛船目的地設為 ${targetPlayer.name} 的星系").styled { style ->
+            style.color = Formatting.AQUA
         }
         source.sendFeedback(text, false)
         sourcePlayer.networkHandler.sendPacket(TitleS2CPacket(TitleS2CPacket.Action.TITLE, text))
@@ -101,16 +102,24 @@ class Join : Command, CoroutineScope by CoroutineScope(Dispatchers.Default + Sup
 
                 when (data.stage) {
                     Queue -> {
-                        val subText = LiteralText("正在等待星系載入").styled { style -> style.color = Formatting.YELLOW }
+                        val subText = LiteralText("飛船正在準備起飛...").styled { style -> style.color = Formatting.AQUA }
                         updateVisualStatus(source, text, subText, 0)
+                        val tipText = LiteralText("星系已經排入飛船目的地，請等待系統開始載入星系").styled { style ->
+                            style.color = Formatting.YELLOW
+                        }
+                        source.sendFeedback(tipText, false)
                     }
                     Creating -> {
-                        val subText = LiteralText("星系載入中...").styled { style -> style.color = Formatting.YELLOW }
+                        val subText = LiteralText("星系載入中...").styled { style -> style.color = Formatting.AQUA }
                         updateVisualStatus(source, text, subText, 20)
                     }
                     Starting -> {
-                        val subText = LiteralText("星系正在啟動請稍後...").styled { style -> style.color = Formatting.YELLOW }
+                        val subText = LiteralText("飛船正在飛向星系請稍後...").styled { style -> style.color = Formatting.AQUA }
                         updateVisualStatus(source, text, subText, 40)
+                        val tipText = LiteralText("飛船正在飛向星系，約兩分鐘後抵達，請耐心等候").styled { style ->
+                            style.color = Formatting.YELLOW
+                        }
+                        source.sendFeedback(tipText, false)
                         starting[sourcePlayer] = true
                         launch {
                             val bossBar = getOrCreateProcessBossBar(source)
@@ -130,15 +139,19 @@ class Join : Command, CoroutineScope by CoroutineScope(Dispatchers.Default + Sup
                                 delay(Duration.ofMillis(500))
                                 seconds += 0.5
                                 if (seconds >= 1) {
-                                    LiteralText("星系正在啟動請稍後... 已經過 ${seconds.toInt()} 秒").styled { style ->
-                                        style.color = Formatting.YELLOW
-                                    }.let(bossBar::setName)
+                                    LiteralText("飛船正在飛向星系請稍後...").styled { style ->
+                                        style.color = Formatting.AQUA
+                                    }.append(
+                                        LiteralText(" 航行時間： ${seconds.toInt()} 秒").styled { style ->
+                                            style.color = Formatting.YELLOW
+                                        }
+                                    ).let(bossBar::setName)
                                 }
                             }
                         }
                     }
                     Started -> {
-                        val subText = LiteralText("星系已載入！").styled { style -> style.color = Formatting.GREEN }
+                        val subText = LiteralText("成功抵達目的地！").styled { style -> style.color = Formatting.GREEN }
                         sourcePlayer.sendMessage(subText)
                         updateVisualStatus(source, text, subText, 200)
                         starting[sourcePlayer] = false
@@ -151,7 +164,7 @@ class Join : Command, CoroutineScope by CoroutineScope(Dispatchers.Default + Sup
                         }
                     }
                     Failed -> {
-                        sourcePlayer.sendMessage(LiteralText("星系載入失敗，請聯絡開發團隊！").styled { style -> style.color = Formatting.RED })
+                        sourcePlayer.sendMessage(LiteralText("您的飛船在飛行途中炸毀了，請聯絡開發團隊！").styled { style -> style.color = Formatting.RED })
                         starting[sourcePlayer] = false
                         starting.remove(sourcePlayer)
                         removeProcessBossBar(source)

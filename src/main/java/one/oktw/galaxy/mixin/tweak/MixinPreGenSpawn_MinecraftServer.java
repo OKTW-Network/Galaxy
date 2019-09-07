f2d0d482292ba6447029268cd72b0d9095a42683
james58899 <james59988@gmail.com>
2019-09-08 03:30:11 +0800

    Skip waiting spawn gen

---
+++ src/main/java/one/oktw/galaxy/mixin/tweak/MixinPreGenSpawn_MinecraftServer.java
@@ -0,0 +1,33 @@
+/*
+ * OKTW Galaxy Project
+ * Copyright (C) 2018-2019
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.oktw.galaxy.mixin.tweak;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerChunkManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(MinecraftServer.class)
+public class MixinPreGenSpawn_MinecraftServer {
+    @Redirect(method = "prepareStartRegion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;getTotalChunksLoadedCount()I"))
+    private <T> int skipLoadSpawn(ServerChunkManager serverChunkManager) {
+        return 441;
+    }
+}

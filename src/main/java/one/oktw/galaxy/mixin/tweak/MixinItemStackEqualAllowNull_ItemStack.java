94289a2b61df8007583ff84c034636da82c67758
james58899 <james59988@gmail.com>
2019-10-12 17:33:20 +0800

    Allow ItemStack nullable

---
+++ src/main/java/one/oktw/galaxy/mixin/tweak/MixinItemStackEqualAllowNull_ItemStack.java
@@ -0,0 +1,36 @@
+/*
+ * OKTW Galaxy Project
+ * Copyright (C) 2018-2019
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.oktw.galaxy.mixin.tweak;
+
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemStack.class)
+public class MixinItemStackEqualAllowNull_ItemStack {
+    @Inject(method = "areEqual", at = @At("HEAD"), cancellable = true)
+    private static void allowNull(ItemStack itemStack_1, ItemStack itemStack_2, CallbackInfoReturnable<Boolean> cir) {
+        if (itemStack_1 == null || itemStack_2 == null) {
+            cir.setReturnValue(false);
+            cir.cancel();
+        }
+    }
+}

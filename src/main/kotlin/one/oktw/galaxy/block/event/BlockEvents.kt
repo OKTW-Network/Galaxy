30e3f28e4a2d7787667f6d1115cad46a5925fa63
jimchen5209 <jimchen5209@gmail.com>
2019-10-20 23:15:02 +0800

    Event(triggers place/break/openGUI)

--- src/main/kotlin/one/oktw/galaxy/block/event/BlockEvents.kt
+++ src/main/kotlin/one/oktw/galaxy/block/event/BlockEvents.kt
@@ -18,15 +18,30 @@
 
 package one.oktw.galaxy.block.event
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.time.delay
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.server.network.packet.PlayerInteractBlockC2SPacket
+import net.minecraft.text.LiteralText
+import net.minecraft.util.Hand
+import net.minecraft.util.math.Box
 import one.oktw.galaxy.block.Block
 import one.oktw.galaxy.block.type.BlockType
 import one.oktw.galaxy.event.annotation.EventListener
 import one.oktw.galaxy.event.type.PlayerInteractBlockEvent
+import one.oktw.galaxy.item.Tool
 import one.oktw.galaxy.item.type.ItemType
+import one.oktw.galaxy.item.type.ToolType
+import java.time.Duration
 
 class BlockEvents {
+    private val eventLock: MutableList<PlayerInteractBlockC2SPacket> = mutableListOf()
     @EventListener(true)
     fun onPlayerInteractBlock(event: PlayerInteractBlockEvent) {
+        if (eventLock.contains(event.packet)) return
+        eventLock.add(event.packet)
+        val world = event.player.serverWorld
         val position = event.packet.hitY.blockPos
         val placePosition = position.offset(event.packet.hitY.side)
 
@@ -34,13 +49,52 @@ class BlockEvents {
 
         val itemStack = event.player.getStackInHand(hand)
 
+        if (event.player.isSneaking && itemStack.isItemEqual(Tool(ToolType.WRENCH).createItemStack())) {
+            event.player.sendMessage(LiteralText("Break block"))
+            waitAndUnlock(event.packet)
+            return
+        }
+
+        if (!event.player.isSneaking) {
+            val entities = world.getEntities(null, Box(position))
+            val entity = entities.firstOrNull { entity -> entity.scoreboardTags.contains("BLOCK") }
+            if (entity != null) {
+                val tag = entity.scoreboardTags.firstOrNull { string -> BlockType.values().map { it.name }.contains(string) }
+                if (tag != null) {
+                    val blockType = BlockType.valueOf(tag)
+                    if (blockType.hasGUI && hand == Hand.MAIN_HAND) openGUI(blockType, event.player)
+                    waitAndUnlock(event.packet)
+                }
+            }
+        }
+
         val tag = itemStack.tag ?: return
         val itemType = tag.getString("customItemType") ?: return
         if (itemType != ItemType.BLOCK.name) return
 
         val itemBlock = Block(BlockType.valueOf(tag.getString("customBlockType") ?: return))
 
-        //TODO Check for GUI, hand, sneak
-        itemBlock.activate(placePosition)
+        GlobalScope.launch {
+            itemBlock.activate(world, placePosition)
+            waitAndUnlock(event.packet)
+        }
+    }
+
+    private fun waitAndUnlock(packet: PlayerInteractBlockC2SPacket) {
+        GlobalScope.launch {
+            delay(Duration.ofSeconds(1))
+            eventLock.remove(packet)
+        }
+    }
+
+    private fun openGUI(blockType: BlockType, player: ServerPlayerEntity) {
+        when (blockType) {
+            BlockType.CONTROL_PANEL -> player.sendMessage(LiteralText("Control Panel"))
+            BlockType.PLANET_TERMINAL -> player.sendMessage(LiteralText("Planet Terminal"))
+            BlockType.HT_CRAFTING_TABLE -> player.sendMessage(LiteralText("HTCT"))
+            BlockType.TELEPORTER_CORE_BASIC -> player.sendMessage(LiteralText("Basic Teleporter"))
+            BlockType.TELEPORTER_CORE_ADVANCE -> player.sendMessage(LiteralText("Advanced Teleporter"))
+            else -> Unit
+        }
     }
 }

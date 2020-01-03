d3cfb9af5c284bf3349401afd5c2d931f20eb8b8
james58899 <james59988@gmail.com>
2020-01-04 03:20:47 +0800

    Fix Mixin compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,15 +28,15 @@ base {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         apiVersion = "1.3"
         languageVersion = "1.3"
-        jvmTarget = "12"
+        jvmTarget = "1.8"
     }
 }
 

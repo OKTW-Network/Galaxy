dee9d7beb81da2b54a9e0dd14ccdc64b8a5d991b
james58899 <james59988@gmail.com>
2020-04-18 01:22:03 +0800

    Re-sync on event canceled

--- src/main/java/one/oktw/galaxy/mixin/event/MixinPlayerInteractBlock_NetworkHandler.java
+++ src/main/java/one/oktw/galaxy/mixin/event/MixinPlayerInteractBlock_NetworkHandler.java
@@ -18,11 +18,16 @@
 
 package one.oktw.galaxy.mixin.event;
 
+import net.minecraft.client.network.packet.BlockUpdateS2CPacket;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.packet.PlayerInteractBlockC2SPacket;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import one.oktw.galaxy.Main;
 import one.oktw.galaxy.event.type.PlayerInteractBlockEvent;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -34,12 +39,25 @@ public class MixinPlayerInteractBlock_NetworkHandler {
     @Shadow
     public ServerPlayerEntity player;
 
-    @Inject(method = "onPlayerInteractBlock", at = @At("HEAD"), cancellable = true)
+    @Shadow
+    @Final
+    private MinecraftServer server;
+
+    @Inject(method = "onPlayerInteractBlock", at = @At(
+        value = "INVOKE",
+        target = "Lnet/minecraft/server/network/ServerPlayerInteractionManager;interactBlock(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/Hand;Lnet/minecraft/util/hit/BlockHitResult;)Lnet/minecraft/util/ActionResult;"
+    ), cancellable = true)
     private void onPlayerInteractBlock(PlayerInteractBlockC2SPacket packet, CallbackInfo info) {
         Main main = Main.Companion.getMain();
         if (main == null) return;
         if (main.getEventManager().emit(new PlayerInteractBlockEvent(packet, player)).getCancel()) {
             info.cancel();
+            // Re-sync block & inventory
+            ServerWorld world = server.getWorld(this.player.dimension);
+            BlockPos blockPos = packet.getHitY().getBlockPos();
+            player.networkHandler.sendPacket(new BlockUpdateS2CPacket(world, blockPos));
+            player.networkHandler.sendPacket(new BlockUpdateS2CPacket(world, blockPos.offset(packet.getHitY().getSide())));
+            player.onContainerRegistered(player.container, player.container.getStacks());
         }
     }
 }

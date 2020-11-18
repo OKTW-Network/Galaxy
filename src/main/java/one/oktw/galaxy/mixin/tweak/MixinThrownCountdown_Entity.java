0c076e434b6dfbba862c11ae5857567919d7d783
james58899 <james59988@gmail.com>
2020-11-18 23:03:36 +0800

    Remove entity if it stuck on water

---
+++ src/main/java/one/oktw/galaxy/mixin/tweak/MixinThrownCountdown_Entity.java
@@ -0,0 +1,49 @@
+/*
+ * OKTW Galaxy Project
+ * Copyright (C) 2018-2020
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.oktw.galaxy.mixin.tweak;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.entity.projectile.SpectralArrowEntity;
+import net.minecraft.entity.projectile.thrown.ThrownEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Entity.class)
+public abstract class MixinThrownCountdown_Entity {
+    private int intoWater = 0;
+
+    @Shadow
+    public abstract void remove();
+
+    @Inject(method = "checkWaterState",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;onSwimmingStart()V"))
+    private void countIntoWater(CallbackInfo ci) {
+        //noinspection ConstantConditions
+        if (((Object) this) instanceof ThrownEntity || ((Object) this) instanceof ArrowEntity || ((Object) this) instanceof SpectralArrowEntity) {
+            if (intoWater > 10) {
+                remove();
+            }
+            intoWater++;
+        }
+    }
+}

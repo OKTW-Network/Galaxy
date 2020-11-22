16c01d654c693ded54e96d205d90fc82099fda23
Jim Chen <jimchen5209@gmail.com>
2020-11-22 22:45:05 +0800

    Custom Tags

---
+++ src/main/kotlin/one/oktw/galaxy/recipe/tags/StainedGlass.kt
@@ -0,0 +1,42 @@
+/*
+ * OKTW Galaxy Project
+ * Copyright (C) 2018-2020
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.oktw.galaxy.recipe.tags
+
+import net.minecraft.item.Items
+
+class StainedGlass: CustomTag {
+    override val itemList = listOf(
+        Items.WHITE_STAINED_GLASS,
+        Items.ORANGE_STAINED_GLASS,
+        Items.MAGENTA_STAINED_GLASS,
+        Items.LIGHT_BLUE_STAINED_GLASS,
+        Items.YELLOW_STAINED_GLASS,
+        Items.LIME_STAINED_GLASS,
+        Items.PINK_STAINED_GLASS,
+        Items.GRAY_STAINED_GLASS,
+        Items.LIGHT_GRAY_STAINED_GLASS,
+        Items.CYAN_STAINED_GLASS,
+        Items.PURPLE_STAINED_GLASS,
+        Items.BLUE_STAINED_GLASS,
+        Items.BROWN_STAINED_GLASS,
+        Items.GREEN_STAINED_GLASS,
+        Items.RED_STAINED_GLASS,
+        Items.BLACK_STAINED_GLASS
+    )
+}
--- src/main/kotlin/one/oktw/galaxy/recipe/utils/Ingredient.kt
+++ src/main/kotlin/one/oktw/galaxy/recipe/utils/Ingredient.kt
@@ -21,19 +21,24 @@ package one.oktw.galaxy.recipe.utils
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.tag.Tag
+import one.oktw.galaxy.recipe.tags.CustomTag
 import java.security.InvalidParameterException
 
 class Ingredient(
     private val item: Item? = null,
     private val tag: Tag.Identified<Item>? = null,
-    private val itemStack: ItemStack? = null
+    private val itemStack: ItemStack? = null,
+    private val customTag: CustomTag? = null
 ) {
     init {
-        if (item == null && tag == null && itemStack == null) throw InvalidParameterException("No input provided.")
+        if (item == null && tag == null && itemStack == null && customTag == null) throw InvalidParameterException("No input provided.")
         if (
             (item != null && tag != null) ||
+            (item != null && itemStack != null) ||
+            (item != null &&customTag != null) ||
             (tag != null && itemStack != null) ||
-            (itemStack != null && item != null)
+            (tag != null && customTag != null) ||
+            (itemStack != null && customTag != null)
         ) throw InvalidParameterException("Only one input is allowed.")
     }
 
@@ -42,7 +47,7 @@ class Ingredient(
             item != null -> input.item == item
             tag != null -> tag.contains(input.item)
             itemStack != null -> itemStack.isItemEqual(input)
-
+            customTag != null -> customTag.contains(input.item)
             else -> false
         }
     }

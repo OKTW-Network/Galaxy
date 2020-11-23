4f528108a3bcd7980eec264e958a7aef6edff267
WoodMan <woodmanpublic@gmail.com>
2020-11-23 21:31:02 +0800

    Complete glass block recipe

--- src/main/kotlin/one/oktw/galaxy/recipe/easyRecipe/Glass.kt
+++ src/main/kotlin/one/oktw/galaxy/recipe/easyRecipe/Glass.kt
@@ -21,9 +21,11 @@ package one.oktw.galaxy.recipe.easyRecipe
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.minecraft.inventory.CraftingInventory
+import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
 import net.minecraft.recipe.CraftingRecipe
 import net.minecraft.recipe.RecipeSerializer
+import net.minecraft.tag.ItemTags
 import net.minecraft.util.Identifier
 import net.minecraft.world.World
 import one.oktw.galaxy.recipe.tags.CustomTags
@@ -32,23 +34,64 @@ import one.oktw.galaxy.recipe.utils.Ingredient
 import one.oktw.galaxy.recipe.utils.RecipeUtils
 
 class Glass : CraftingRecipe {
-    private val item = Items.GLASS.defaultStack
-    private val list = listOf(
-        Ingredient(customTag = CustomTags.StainedGlass), Ingredient(customTag = CustomTags.StainedGlass), Ingredient(customTag = CustomTags.StainedGlass),
-        Ingredient(customTag = CustomTags.StainedGlass), Ingredient(item = Items.WATER_BUCKET), Ingredient(customTag = CustomTags.StainedGlass),
-        Ingredient(customTag = CustomTags.StainedGlass), Ingredient(customTag = CustomTags.StainedGlass), Ingredient(customTag = CustomTags.StainedGlass)
+    private val slabs = hashMapOf(
+        Items.WATER_BUCKET to Items.GLASS,
+        Items.RED_DYE to Items.RED_STAINED_GLASS,
+        Items.GREEN_DYE to Items.GREEN_STAINED_GLASS,
+        Items.PURPLE_DYE to Items.PURPLE_STAINED_GLASS,
+        Items.CYAN_DYE to Items.CYAN_STAINED_GLASS,
+        Items.LIGHT_GRAY_DYE to Items.LIGHT_GRAY_STAINED_GLASS,
+        Items.GRAY_DYE to Items.GRAY_STAINED_GLASS,
+        Items.PINK_DYE to Items.PINK_STAINED_GLASS,
+        Items.LIME_DYE to Items.LIME_STAINED_GLASS,
+        Items.YELLOW_DYE to Items.YELLOW_STAINED_GLASS,
+        Items.LIGHT_BLUE_DYE to Items.LIGHT_BLUE_STAINED_GLASS,
+        Items.MAGENTA_DYE to Items.MAGENTA_STAINED_GLASS,
+        Items.ORANGE_DYE to Items.ORANGE_STAINED_GLASS,
+        Items.BLACK_DYE to Items.BLACK_STAINED_GLASS,
+        Items.BROWN_DYE to Items.BROWN_STAINED_GLASS,
+        Items.BLUE_DYE to Items.BLUE_STAINED_GLASS,
+        Items.WHITE_DYE to Items.WHITE_STAINED_GLASS
     )
 
-    override fun matches(inv: CraftingInventory, world: World): Boolean = RecipeUtils.isItemShapedMatches(inv, 3, 3, list)
+    override fun matches(inv: CraftingInventory, world: World): Boolean {
+        var match = false
+        slabs.forEach { (items, _) ->
+            val list = listOf(
+                Ingredient(customTag = StainedGlass()), Ingredient(customTag = StainedGlass()), Ingredient(customTag = StainedGlass()),
+                Ingredient(customTag = StainedGlass()), Ingredient(item = items), Ingredient(customTag = StainedGlass()),
+                Ingredient(customTag = StainedGlass()), Ingredient(customTag = StainedGlass()), Ingredient(customTag = StainedGlass()),
+            )
+            if (RecipeUtils.isItemShapedMatches(inv, 3, 3, list)) {
+                match = true
+                return@forEach
+            }
+        }
+        return match
+    }
 
-    override fun craft(inv: CraftingInventory) = item.copy()
+    override fun craft(inv: CraftingInventory): ItemStack {
+        var item = ItemStack.EMPTY
+        slabs.forEach { (items, result) ->
+            val list = listOf(
+                Ingredient(customTag = StainedGlass()), Ingredient(customTag = StainedGlass()), Ingredient(customTag = StainedGlass()),
+                Ingredient(customTag = StainedGlass()), Ingredient(item = items), Ingredient(customTag = StainedGlass()),
+                Ingredient(customTag = StainedGlass()), Ingredient(customTag = StainedGlass()), Ingredient(customTag = StainedGlass()),
+            )
+            if (RecipeUtils.isItemShapedMatches(inv, 3, 3, list)) {
+                item = result.defaultStack.apply { this.count = 8 }
+                return@forEach
+            }
+        }
+        return item
+    }
 
     @Environment(EnvType.CLIENT)
     override fun fits(width: Int, height: Int): Boolean {
         throw NotImplementedError()
     }
 
-    override fun getOutput() = item
+    override fun getOutput() = Items.OAK_SLAB.defaultStack.apply { this.count = 24 }
 
     override fun getId() = Identifier("galaxy", "easy_recipe/glass")
 

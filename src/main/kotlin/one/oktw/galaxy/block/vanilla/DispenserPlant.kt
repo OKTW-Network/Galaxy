f90823e5f33715883cb629463674896c1b7f7f31
WoodMan <woodmanpublic@gmail.com>
2020-12-07 17:25:33 +0800

    Add Planting sound and Rename function

--- src/main/kotlin/one/oktw/galaxy/block/vanilla/DispenserPlant.kt
+++ src/main/kotlin/one/oktw/galaxy/block/vanilla/DispenserPlant.kt
@@ -24,13 +24,16 @@ import net.minecraft.block.DispenserBlock
 import net.minecraft.block.dispenser.ItemDispenserBehavior
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvent
+import net.minecraft.sound.SoundEvents
 import net.minecraft.util.math.BlockPointer
 import net.minecraft.util.math.Direction
 import net.minecraft.world.World
 
 
 object DispenserPlant {
-    private fun plant(blockPointer: BlockPointer, itemStack: ItemStack, block: Block, validBlocksToPlantOn: List<Block>): ItemStack {
+    private fun plant(blockPointer: BlockPointer, itemStack: ItemStack, block: Block, validBlocksToPlantOn: List<Block>, soundEvent: SoundEvent): ItemStack {
         val world: World = blockPointer.world
         val dispenserFacing = blockPointer.blockState.get(DispenserBlock.FACING)
 
@@ -44,12 +47,14 @@ object DispenserPlant {
             if (dispenserFacing == Direction.UP) {
                 if (plantBlockState.block == Blocks.AIR && world.getBaseLightLevel(plantBlockPos, 0) < 13 && currentBlockState.isOpaqueFullCube(world, currentBlockPos)) {
                     world.setBlockState(plantBlockPos, block.defaultState)
+                    world.playSound(null, plantBlockPos, soundEvent, SoundCategory.BLOCKS, 1.0F, 1.0F)
                     itemStack.decrement(1)
                     return itemStack
                 }
             } else {
                 if (currentBlockState.block == Blocks.AIR && world.getBaseLightLevel(currentBlockPos, 0) < 13 && plantBlockState.isOpaqueFullCube(world, plantBlockPos)) {
                     world.setBlockState(currentBlockPos, block.defaultState)
+                    world.playSound(null, currentBlockPos, soundEvent, SoundCategory.BLOCKS, 1.0F, 1.0F)
                     itemStack.decrement(1)
                     return itemStack
                 }
@@ -58,12 +63,14 @@ object DispenserPlant {
 
         if  (validBlocksToPlantOn.contains(currentBlockState.block) && plantBlockState.block == Blocks.AIR) {
             world.setBlockState(plantBlockPos, block.defaultState)
+            world.playSound(null, plantBlockPos, soundEvent, SoundCategory.BLOCKS, 1.0F, 1.0F)
             itemStack.decrement(1)
             return itemStack
         }
 
         if (validBlocksToPlantOn.contains(plantBlockState.block) && currentBlockState.block == Blocks.AIR) {
             world.setBlockState(currentBlockPos, block.defaultState)
+            world.playSound(null, currentBlockPos, soundEvent, SoundCategory.BLOCKS, 1.0F, 1.0F)
             itemStack.decrement(1)
             return itemStack
         }
@@ -71,33 +78,33 @@ object DispenserPlant {
         return ItemDispenserBehavior().dispense(blockPointer, itemStack)
     }
 
-    private fun seedsDispenserBehavior(block: Block, dependent: List<Block>): ItemDispenserBehavior = object : ItemDispenserBehavior() {
+    private fun plantDispenserBehavior(block: Block, dependent: List<Block>, soundEvent: SoundEvent): ItemDispenserBehavior = object : ItemDispenserBehavior() {
         override fun dispenseSilently(blockPointer: BlockPointer, itemStack: ItemStack): ItemStack {
-            return plant(blockPointer, itemStack, block, dependent)
+            return plant(blockPointer, itemStack, block, dependent, soundEvent)
         }
     }
 
     fun register() {
-        DispenserBlock.registerBehavior(Items.WHEAT_SEEDS, seedsDispenserBehavior(Blocks.WHEAT, listOf(Blocks.FARMLAND)))
-        DispenserBlock.registerBehavior(Items.POTATO, seedsDispenserBehavior(Blocks.POTATOES, listOf(Blocks.FARMLAND)))
-        DispenserBlock.registerBehavior(Items.CARROT, seedsDispenserBehavior(Blocks.CARROTS, listOf(Blocks.FARMLAND)))
-        DispenserBlock.registerBehavior(Items.MELON_SEEDS, seedsDispenserBehavior(Blocks.MELON_STEM, listOf(Blocks.FARMLAND)))
-        DispenserBlock.registerBehavior(Items.PUMPKIN_SEEDS, seedsDispenserBehavior(Blocks.PUMPKIN_STEM, listOf(Blocks.FARMLAND)))
-        DispenserBlock.registerBehavior(Items.BEETROOT_SEEDS, seedsDispenserBehavior(Blocks.BEETROOTS, listOf(Blocks.FARMLAND)))
-        DispenserBlock.registerBehavior(Items.NETHER_WART, seedsDispenserBehavior(Blocks.NETHER_WART, listOf(Blocks.SOUL_SAND)))
-        DispenserBlock.registerBehavior(Items.SWEET_BERRIES, seedsDispenserBehavior(Blocks.SWEET_BERRY_BUSH, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.COARSE_DIRT, Blocks.FARMLAND)))
-        DispenserBlock.registerBehavior(Items.OAK_SAPLING, seedsDispenserBehavior(Blocks.OAK_SAPLING, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.FARMLAND)))
-        DispenserBlock.registerBehavior(Items.BIRCH_SAPLING, seedsDispenserBehavior(Blocks.BIRCH_SAPLING, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.FARMLAND)))
-        DispenserBlock.registerBehavior(Items.SPRUCE_SAPLING, seedsDispenserBehavior(Blocks.SPRUCE_SAPLING, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.FARMLAND)))
-        DispenserBlock.registerBehavior(Items.JUNGLE_SAPLING, seedsDispenserBehavior(Blocks.JUNGLE_SAPLING, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.FARMLAND)))
-        DispenserBlock.registerBehavior(Items.ACACIA_SAPLING, seedsDispenserBehavior(Blocks.ACACIA_SAPLING, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.FARMLAND)))
-        DispenserBlock.registerBehavior(Items.DARK_OAK_SAPLING, seedsDispenserBehavior(Blocks.DARK_OAK_SAPLING, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.FARMLAND)))
-        DispenserBlock.registerBehavior(Items.SUGAR_CANE, seedsDispenserBehavior(Blocks.SUGAR_CANE, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.SAND, Blocks.RED_SAND, Blocks.COARSE_DIRT)))
-        DispenserBlock.registerBehavior(Items.CHORUS_FLOWER, seedsDispenserBehavior(Blocks.CHORUS_FLOWER, listOf(Blocks.END_STONE)))
-        DispenserBlock.registerBehavior(Items.BAMBOO, seedsDispenserBehavior(Blocks.BAMBOO, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.SAND, Blocks.RED_SAND, Blocks.COARSE_DIRT, Blocks.GRAVEL, Blocks.MYCELIUM)))
-        DispenserBlock.registerBehavior(Items.RED_MUSHROOM, seedsDispenserBehavior(Blocks.RED_MUSHROOM, listOf()))
-        DispenserBlock.registerBehavior(Items.BROWN_MUSHROOM, seedsDispenserBehavior(Blocks.BROWN_MUSHROOM, listOf()))
-        DispenserBlock.registerBehavior(Items.CRIMSON_FUNGUS, seedsDispenserBehavior(Blocks.CRIMSON_FUNGUS, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.COARSE_DIRT, Blocks.FARMLAND, Blocks.CRIMSON_NYLIUM, Blocks.WARPED_NYLIUM, Blocks.SOUL_SOIL, Blocks.MYCELIUM)))
-        DispenserBlock.registerBehavior(Items.WARPED_FUNGUS, seedsDispenserBehavior(Blocks.WARPED_FUNGUS, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.COARSE_DIRT, Blocks.FARMLAND, Blocks.CRIMSON_NYLIUM, Blocks.WARPED_NYLIUM, Blocks.SOUL_SOIL, Blocks.MYCELIUM)))
+        DispenserBlock.registerBehavior(Items.WHEAT_SEEDS, plantDispenserBehavior(Blocks.WHEAT, listOf(Blocks.FARMLAND), SoundEvents.ITEM_CROP_PLANT))
+        DispenserBlock.registerBehavior(Items.POTATO, plantDispenserBehavior(Blocks.POTATOES, listOf(Blocks.FARMLAND), SoundEvents.ITEM_CROP_PLANT))
+        DispenserBlock.registerBehavior(Items.CARROT, plantDispenserBehavior(Blocks.CARROTS, listOf(Blocks.FARMLAND), SoundEvents.ITEM_CROP_PLANT))
+        DispenserBlock.registerBehavior(Items.MELON_SEEDS, plantDispenserBehavior(Blocks.MELON_STEM, listOf(Blocks.FARMLAND), SoundEvents.ITEM_CROP_PLANT))
+        DispenserBlock.registerBehavior(Items.PUMPKIN_SEEDS, plantDispenserBehavior(Blocks.PUMPKIN_STEM, listOf(Blocks.FARMLAND), SoundEvents.ITEM_CROP_PLANT))
+        DispenserBlock.registerBehavior(Items.BEETROOT_SEEDS, plantDispenserBehavior(Blocks.BEETROOTS, listOf(Blocks.FARMLAND), SoundEvents.ITEM_CROP_PLANT))
+        DispenserBlock.registerBehavior(Items.NETHER_WART, plantDispenserBehavior(Blocks.NETHER_WART, listOf(Blocks.SOUL_SAND), SoundEvents.ITEM_NETHER_WART_PLANT))
+        DispenserBlock.registerBehavior(Items.SWEET_BERRIES, plantDispenserBehavior(Blocks.SWEET_BERRY_BUSH, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.COARSE_DIRT, Blocks.FARMLAND), SoundEvents.BLOCK_SWEET_BERRY_BUSH_PLACE))
+        DispenserBlock.registerBehavior(Items.OAK_SAPLING, plantDispenserBehavior(Blocks.OAK_SAPLING, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.FARMLAND), SoundEvents.BLOCK_GRASS_PLACE))
+        DispenserBlock.registerBehavior(Items.BIRCH_SAPLING, plantDispenserBehavior(Blocks.BIRCH_SAPLING, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.FARMLAND), SoundEvents.BLOCK_GRASS_PLACE))
+        DispenserBlock.registerBehavior(Items.SPRUCE_SAPLING, plantDispenserBehavior(Blocks.SPRUCE_SAPLING, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.FARMLAND), SoundEvents.BLOCK_GRASS_PLACE))
+        DispenserBlock.registerBehavior(Items.JUNGLE_SAPLING, plantDispenserBehavior(Blocks.JUNGLE_SAPLING, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.FARMLAND), SoundEvents.BLOCK_GRASS_PLACE))
+        DispenserBlock.registerBehavior(Items.ACACIA_SAPLING, plantDispenserBehavior(Blocks.ACACIA_SAPLING, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.FARMLAND), SoundEvents.BLOCK_GRASS_PLACE))
+        DispenserBlock.registerBehavior(Items.DARK_OAK_SAPLING, plantDispenserBehavior(Blocks.DARK_OAK_SAPLING, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.FARMLAND), SoundEvents.BLOCK_GRASS_PLACE))
+        DispenserBlock.registerBehavior(Items.SUGAR_CANE, plantDispenserBehavior(Blocks.SUGAR_CANE, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.SAND, Blocks.RED_SAND, Blocks.COARSE_DIRT), SoundEvents.BLOCK_GRASS_PLACE))
+        DispenserBlock.registerBehavior(Items.CHORUS_FLOWER, plantDispenserBehavior(Blocks.CHORUS_FLOWER, listOf(Blocks.END_STONE), SoundEvents.BLOCK_WOOD_PLACE))
+        DispenserBlock.registerBehavior(Items.BAMBOO, plantDispenserBehavior(Blocks.BAMBOO_SAPLING, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.SAND, Blocks.RED_SAND, Blocks.COARSE_DIRT, Blocks.GRAVEL, Blocks.MYCELIUM), SoundEvents.BLOCK_BAMBOO_SAPLING_PLACE))
+        DispenserBlock.registerBehavior(Items.RED_MUSHROOM, plantDispenserBehavior(Blocks.RED_MUSHROOM, listOf(), SoundEvents.BLOCK_GRASS_PLACE))
+        DispenserBlock.registerBehavior(Items.BROWN_MUSHROOM, plantDispenserBehavior(Blocks.BROWN_MUSHROOM, listOf(), SoundEvents.BLOCK_GRASS_PLACE))
+        DispenserBlock.registerBehavior(Items.CRIMSON_FUNGUS, plantDispenserBehavior(Blocks.CRIMSON_FUNGUS, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.COARSE_DIRT, Blocks.FARMLAND, Blocks.CRIMSON_NYLIUM, Blocks.WARPED_NYLIUM, Blocks.SOUL_SOIL, Blocks.MYCELIUM), SoundEvents.BLOCK_FUNGUS_PLACE))
+        DispenserBlock.registerBehavior(Items.WARPED_FUNGUS, plantDispenserBehavior(Blocks.WARPED_FUNGUS, listOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.PODZOL, Blocks.COARSE_DIRT, Blocks.FARMLAND, Blocks.CRIMSON_NYLIUM, Blocks.WARPED_NYLIUM, Blocks.SOUL_SOIL, Blocks.MYCELIUM), SoundEvents.BLOCK_FUNGUS_PLACE))
     }
 }

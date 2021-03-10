0e123fe9815112f356c6085bd63b6671c8e221a1
James58899 <james59988@gmail.com>
2021-03-10 21:17:39 +0800

    Ignore invulnerable ItemFrame

---
+++ src/main/java/one/oktw/galaxy/mixin/tweak/MixinPipe_ComparatorBlock.java
@@ -0,0 +1,38 @@
+/*
+ * OKTW Galaxy Project
+ * Copyright (C) 2018-2021
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.oktw.galaxy.mixin.tweak;
+
+import net.minecraft.block.ComparatorBlock;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ComparatorBlock.class)
+public class MixinPipe_ComparatorBlock {
+    @Inject(method = "getAttachedItemFrame", at = @At("RETURN"), cancellable = true)
+    private void ignoreInvulnerableItemFrame(World world, Direction facing, BlockPos pos, CallbackInfoReturnable<ItemFrameEntity> cir) {
+        ItemFrameEntity entity = cir.getReturnValue();
+        if (entity != null && entity.isInvulnerable()) cir.setReturnValue(null);
+    }
+}

e91af34d0994800b80da1f7241f4c5256a137885
James58899 <james59988@gmail.com>
2022-06-28 18:06:17 +0800

    Refactor GUI container

--- src/main/kotlin/one/oktw/galaxy/gui/GUI.kt
+++ src/main/kotlin/one/oktw/galaxy/gui/GUI.kt
@@ -1,6 +1,6 @@
 /*
  * OKTW Galaxy Project
- * Copyright (C) 2018-2021
+ * Copyright (C) 2018-2022
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
@@ -28,13 +28,13 @@ import net.minecraft.screen.NamedScreenHandlerFactory
 import net.minecraft.screen.ScreenHandler
 import net.minecraft.screen.ScreenHandlerType
 import net.minecraft.screen.ScreenHandlerType.*
+import net.minecraft.screen.slot.Slot
 import net.minecraft.screen.slot.SlotActionType
 import net.minecraft.screen.slot.SlotActionType.*
 import net.minecraft.text.Text
 import one.oktw.galaxy.Main.Companion.main
 import one.oktw.galaxy.gui.utils.InventoryEditor
 import one.oktw.galaxy.gui.utils.InventoryUtils
-import one.oktw.galaxy.gui.utils.InventoryUtils.Companion.genericScreenHandlerType
 import java.util.concurrent.ConcurrentHashMap
 
 @Suppress("unused", "MemberVisibilityCanBePrivate")
@@ -61,13 +61,8 @@ class GUI(private val type: ScreenHandlerType<out ScreenHandler>, private val ti
 
     override fun getDisplayName() = title
 
-    override fun createMenu(syncId: Int, playerInventory: PlayerInventory, player: PlayerEntity): ScreenHandler? {
-        return when (type) {
-            in genericScreenHandlerType -> GenericContainer(syncId, playerInventory)
-            GENERIC_3X3 -> Generic3x3Container(syncId, playerInventory)
-            HOPPER -> HopperContainer(syncId, playerInventory)
-            else -> null
-        }
+    override fun createMenu(syncId: Int, playerInventory: PlayerInventory, player: PlayerEntity): ScreenHandler {
+        return GuiContainer(syncId, playerInventory)
     }
 
     fun getItem(x: Int, y: Int) = getItem(inventoryUtils.xyToIndex(x, y))
@@ -137,10 +132,27 @@ class GUI(private val type: ScreenHandlerType<out ScreenHandler>, private val ti
 
     private fun checkRange(x: Int, y: Int) = inventoryUtils.xyToIndex(x, y) in 0 until inventory.size()
 
-    // Vanilla container hack
-    private inner class GenericContainer(syncId: Int, playerInventory: PlayerInventory) :
-        net.minecraft.screen.GenericContainerScreenHandler(type, syncId, playerInventory, inventory, inventory.size() / 9) {
+    private inner class GuiContainer(syncId: Int, playerInventory: PlayerInventory) : ScreenHandler(type, syncId) {
         init {
+            inventory.onOpen(playerInventory.player)
+
+            // Add slot
+            // TODO custom slot
+            // GUI Inventory slot
+            for (i in 0 until inventory.size()) {
+                addSlot(Slot(inventory, i, 0, 0)) // xy only use on client side, ignore it.
+            }
+            // Player inventory
+            // Index 0..8 is HotBar, 9..35 is Inventory
+            for (y in 0 until 3) {
+                for (x in 0 until 9) {
+                    addSlot(Slot(playerInventory, x + y * 9 + 9, 0, 0))
+                }
+            }
+            for (j in 0 until 9) {
+                addSlot(Slot(playerInventory, j, 0, 0))
+            }
+
             openListener.forEach { it.invoke(playerInventory.player) }
         }
 
@@ -231,195 +243,15 @@ class GUI(private val type: ScreenHandlerType<out ScreenHandler>, private val ti
                 }
             }
         }
-    }
 
-    private inner class Generic3x3Container(syncId: Int, playerInventory: PlayerInventory) :
-        net.minecraft.screen.Generic3x3ContainerScreenHandler(syncId, playerInventory, inventory) {
-        init {
-            openListener.forEach { it.invoke(playerInventory.player) }
+        override fun transferSlot(player: PlayerEntity, index: Int): ItemStack {
+            // Not in used, logic override in onSlotClick
+            return ItemStack.EMPTY
         }
 
-        override fun close(player: PlayerEntity) {
-            super.close(player)
-            closeListener.forEach { it.invoke(player) }
-        }
-
-        override fun onSlotClick(slot: Int, button: Int, action: SlotActionType, player: PlayerEntity) {
-            // Trigger binding TODO allow binding cancel player change
-            if (slot in 0 until inventory.size()) {
-                inventoryUtils.indexToXY(slot).let { (x, y) ->
-                    bindings[slot]?.invoke(GUIClickEvent(x, y, action, inventory.getStack(slot)))
-                    rangeBindings.filterKeys { (xRange, yRange) -> x in xRange && y in yRange }.values
-                        .forEach { it.invoke(GUIClickEvent(x, y, action, inventory.getStack(slot))) }
-                }
-            }
-
-            // Cancel player change inventory
-            if (slot < inventory.size() && slot != -999 && slot !in allowUseSlot) {
-                if (action == QUICK_CRAFT) endQuickCraft()
-                return
-            }
-
-            return when (action) {
-                PICKUP, SWAP, CLONE, THROW, QUICK_CRAFT -> super.onSlotClick(slot, button, action, player)
-                QUICK_MOVE -> {
-                    if (slot in 0 until inventory.size() && slot !in allowUseSlot) return
-
-                    val inventorySlot = slots[slot]
-
-                    if (inventorySlot.hasStack()) {
-                        val slotItemStack = inventorySlot.stack
-
-                        // TODO move item to canUse slot
-                        if (slot in playerInventoryRange) {
-                            if (!insertItem(slotItemStack, playerHotBarRange.first, playerHotBarRange.last, false)) return
-                        } else if (slot in playerHotBarRange) {
-                            if (!insertItem(slotItemStack, playerInventoryRange.first, playerInventoryRange.last, false)) return
-                        }
-
-                        // clean up empty slot
-                        if (slotItemStack.isEmpty) {
-                            inventorySlot.stack = ItemStack.EMPTY
-                        } else {
-                            inventorySlot.markDirty()
-                        }
-                    }
-
-                    return
-                }
-                PICKUP_ALL -> { // Rewrite PICKUP_ALL only take from allow use slot & player inventory.
-                    if (slot < 0) return
-
-                    val cursorItemStack = player.currentScreenHandler.cursorStack
-                    val clickSlot = slots[slot]
-                    if (!cursorItemStack.isEmpty && (!clickSlot.hasStack() || !clickSlot.canTakeItems(player))) {
-                        var takeFullStack = false
-
-                        loop@ while (!takeFullStack) { // First time only take not full stack items
-                            takeFullStack = true
-                            for (index in allowUseSlot + (inventory.size() until slots.size)) {
-                                if (cursorItemStack.count >= cursorItemStack.maxCount) break@loop
-
-                                val scanSlot = slots[index]
-                                if (scanSlot.hasStack()
-                                    && canInsertItemIntoSlot(scanSlot, cursorItemStack, true)
-                                    && scanSlot.canTakeItems(player)
-                                    && canInsertIntoSlot(cursorItemStack, scanSlot)
-                                ) {
-                                    val selectItemStack = scanSlot.stack
-                                    if (takeFullStack || selectItemStack.count != selectItemStack.maxCount) {
-                                        val takeCount = (cursorItemStack.maxCount - cursorItemStack.count).coerceAtMost(selectItemStack.count)
-                                        val selectItemStack2 = scanSlot.takeStack(takeCount)
-                                        cursorItemStack.increment(takeCount)
-                                        if (selectItemStack2.isEmpty) {
-                                            scanSlot.stack = ItemStack.EMPTY
-                                        }
-
-                                        scanSlot.onTakeItem(player, selectItemStack2)
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    this.sendContentUpdates()
-                }
-            }
-        }
-    }
-
-    private inner class HopperContainer(syncId: Int, playerInventory: PlayerInventory) :
-        net.minecraft.screen.HopperScreenHandler(syncId, playerInventory, inventory) {
-        init {
-            openListener.forEach { it.invoke(playerInventory.player) }
-        }
-
-        override fun close(player: PlayerEntity) {
-            super.close(player)
-            closeListener.forEach { it.invoke(player) }
-        }
-
-        override fun onSlotClick(slot: Int, button: Int, action: SlotActionType, player: PlayerEntity) {
-            // Trigger binding TODO allow binding cancel player change
-            if (slot in 0 until inventory.size()) {
-                inventoryUtils.indexToXY(slot).let { (x, y) ->
-                    bindings[slot]?.invoke(GUIClickEvent(x, y, action, inventory.getStack(slot)))
-                    rangeBindings.filterKeys { (xRange, yRange) -> x in xRange && y in yRange }.values
-                        .forEach { it.invoke(GUIClickEvent(x, y, action, inventory.getStack(slot))) }
-                }
-            }
-
-            // Cancel player change inventory
-            if (slot < inventory.size() && slot != -999 && slot !in allowUseSlot) {
-                if (action == QUICK_CRAFT) endQuickCraft()
-                return
-            }
-
-            return when (action) {
-                PICKUP, SWAP, CLONE, THROW, QUICK_CRAFT -> super.onSlotClick(slot, button, action, player)
-                QUICK_MOVE -> {
-                    if (slot in 0 until inventory.size() && slot !in allowUseSlot) return
-
-                    val inventorySlot = slots[slot]
-
-                    if (inventorySlot.hasStack()) {
-                        val slotItemStack = inventorySlot.stack
-
-                        // TODO move item to canUse slot
-                        if (slot in playerInventoryRange) {
-                            if (!insertItem(slotItemStack, playerHotBarRange.first, playerHotBarRange.last, false)) return
-                        } else if (slot in playerHotBarRange) {
-                            if (!insertItem(slotItemStack, playerInventoryRange.first, playerInventoryRange.last, false)) return
-                        }
-
-                        // clean up empty slot
-                        if (slotItemStack.isEmpty) {
-                            inventorySlot.stack = ItemStack.EMPTY
-                        } else {
-                            inventorySlot.markDirty()
-                        }
-                    }
-
-                    return
-                }
-                PICKUP_ALL -> { // Rewrite PICKUP_ALL only take from allow use slot & player inventory.
-                    if (slot < 0) return
-
-                    val cursorItemStack = player.currentScreenHandler.cursorStack
-                    val clickSlot = slots[slot]
-                    if (!cursorItemStack.isEmpty && (!clickSlot.hasStack() || !clickSlot.canTakeItems(player))) {
-                        var takeFullStack = false
-
-                        loop@ while (!takeFullStack) { // First time only take not full stack items
-                            takeFullStack = true
-                            for (index in allowUseSlot + (inventory.size() until slots.size)) {
-                                if (cursorItemStack.count >= cursorItemStack.maxCount) break@loop
-
-                                val scanSlot = slots[index]
-                                if (scanSlot.hasStack()
-                                    && canInsertItemIntoSlot(scanSlot, cursorItemStack, true)
-                                    && scanSlot.canTakeItems(player)
-                                    && canInsertIntoSlot(cursorItemStack, scanSlot)
-                                ) {
-                                    val selectItemStack = scanSlot.stack
-                                    if (takeFullStack || selectItemStack.count != selectItemStack.maxCount) {
-                                        val takeCount = (cursorItemStack.maxCount - cursorItemStack.count).coerceAtMost(selectItemStack.count)
-                                        val selectItemStack2 = scanSlot.takeStack(takeCount)
-                                        cursorItemStack.increment(takeCount)
-                                        if (selectItemStack2.isEmpty) {
-                                            scanSlot.stack = ItemStack.EMPTY
-                                        }
-
-                                        scanSlot.onTakeItem(player, selectItemStack2)
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    this.sendContentUpdates()
-                }
-            }
+        override fun canUse(player: PlayerEntity): Boolean {
+            // TODO close GUI
+            return true
         }
     }
 }

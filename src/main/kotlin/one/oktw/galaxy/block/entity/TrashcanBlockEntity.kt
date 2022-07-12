613507dd735408a1e4697f5dd447ba68f03fd45b
Cute_Panda <asdf3601a@gmail.com>
2022-07-12 16:32:26 +0800

    可以反悔的垃圾桶

--- src/main/kotlin/one/oktw/galaxy/block/entity/TrashcanBlockEntity.kt
+++ src/main/kotlin/one/oktw/galaxy/block/entity/TrashcanBlockEntity.kt
@@ -20,6 +20,7 @@ package one.oktw.galaxy.block.entity
 
 import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.inventory.Inventories
 import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
 import net.minecraft.screen.ScreenHandlerType
@@ -28,6 +29,7 @@ import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.Text
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
+import net.minecraft.util.collection.DefaultedList
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
 import one.oktw.galaxy.block.listener.CustomBlockClickListener
@@ -36,13 +38,6 @@ import one.oktw.galaxy.gui.GUISBackStackManager
 
 class TrashcanBlockEntity(type: BlockEntityType<*>, pos: BlockPos, modelItem: ItemStack) : Inventory, ModelCustomBlockEntity(type, pos, modelItem),
     CustomBlockClickListener {
-    private val gui = GUI.Builder(ScreenHandlerType.GENERIC_9X4)
-        .setTitle(Text.of("Trashcan"))
-        .apply {
-            for (y in 0 until 4) for (x in 0 until 9) addSlot(x, 0, Slot(this@TrashcanBlockEntity, x, 0, 0))
-        }
-        .build()
-
     override fun clear() {}
 
     override fun size(): Int = 1
@@ -64,7 +59,37 @@ class TrashcanBlockEntity(type: BlockEntityType<*>, pos: BlockPos, modelItem: It
     }
 
     override fun onClick(player: PlayerEntity, hand: Hand, hit: BlockHitResult): ActionResult {
+        val gui = GUI.Builder(ScreenHandlerType.GENERIC_9X4)
+            .setTitle(Text.of("Trashcan"))
+            .apply {
+                for (y in 0 until 4) for (x in 0 until 9) addSlot(x, y, Slot(FakeInventory(9 * 4), x, 0, 0))
+            }
+            .build()
+
         GUISBackStackManager.openGUI(player as ServerPlayerEntity, gui)
         return ActionResult.SUCCESS
     }
+}
+
+class FakeInventory(invSize: Int) : Inventory {
+    var inventory: DefaultedList<ItemStack> = DefaultedList.ofSize(invSize, ItemStack.EMPTY)
+    override fun clear() = inventory.clear()
+
+    override fun size(): Int = inventory.size
+
+    override fun isEmpty(): Boolean = true
+
+    override fun getStack(slot: Int): ItemStack = inventory[slot]
+
+    override fun removeStack(slot: Int, amount: Int): ItemStack = Inventories.splitStack(inventory, slot, amount)
+
+    override fun removeStack(slot: Int): ItemStack = Inventories.removeStack(inventory, slot)
+
+    override fun setStack(slot: Int, stack: ItemStack?) {
+        inventory[slot] = stack
+    }
+
+    override fun markDirty() {}
+
+    override fun canPlayerUse(player: PlayerEntity?): Boolean = false
 }

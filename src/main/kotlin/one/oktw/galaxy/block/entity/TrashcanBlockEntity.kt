239b1947beca0dfde4a73252cd7d7a8e05371a03
Cute_Panda <asdf3601a@gmail.com>
2022-07-15 17:42:34 +0800

    removed fakeInventory

--- src/main/kotlin/one/oktw/galaxy/block/entity/TrashcanBlockEntity.kt
+++ src/main/kotlin/one/oktw/galaxy/block/entity/TrashcanBlockEntity.kt
@@ -20,8 +20,8 @@ package one.oktw.galaxy.block.entity
 
 import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.inventory.Inventories
 import net.minecraft.inventory.Inventory
+import net.minecraft.inventory.SimpleInventory
 import net.minecraft.item.ItemStack
 import net.minecraft.screen.ScreenHandlerType
 import net.minecraft.screen.slot.Slot
@@ -29,7 +29,6 @@ import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.Text
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
-import net.minecraft.util.collection.DefaultedList
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
 import one.oktw.galaxy.block.listener.CustomBlockClickListener
@@ -52,46 +51,37 @@ class TrashcanBlockEntity(type: BlockEntityType<*>, pos: BlockPos, modelItem: It
 
     override fun setStack(slot: Int, stack: ItemStack?) {}
 
-    override fun canPlayerUse(player: PlayerEntity): Boolean {
-        return if (world!!.getBlockEntity(pos) !== this) {
-            false
-        } else player.squaredDistanceTo(pos.x.toDouble() + 0.5, pos.y.toDouble() + 0.5, pos.z.toDouble() + 0.5) <= 64.0
-    }
+    override fun canPlayerUse(player: PlayerEntity): Boolean = false
 
     override fun onClick(player: PlayerEntity, hand: Hand, hit: BlockHitResult): ActionResult {
-        val gui = GUI.Builder(ScreenHandlerType.GENERIC_9X4)
-            .setTitle(Text.of("Trashcan"))
-            .apply {
-                var i = 0
-                val inv = FakeInventory(9 * 4)
-                for (y in 0 until 4) for (x in 0 until 9) addSlot(x, y, Slot(inv, i++, 0, 0))
-            }
-            .build()
+        if (!player.isSpectator) {
+            val gui = GUI
+                .Builder(ScreenHandlerType.GENERIC_9X4)
+                .apply {
+                    setTitle(Text.of("Trashcan"))
 
-        GUISBackStackManager.openGUI(player as ServerPlayerEntity, gui)
-        return ActionResult.SUCCESS
+                    var i = 0
+//                    val inv = object : SimpleInventory(9 * 4) {
+//                        override fun canPlayerUse(player: PlayerEntity): Boolean {
+//                            val trashcanBlock = this@TrashcanBlockEntity
+//                            if (trashcanBlock.world!!.getBlockEntity(trashcanBlock.pos) != trashcanBlock) {
+//                                return false
+//                            }
+//
+//                            return player.squaredDistanceTo(trashcanBlock.pos.x + 0.5, trashcanBlock.pos.y + 0.5, trashcanBlock.pos.z + 0.5) <= 64
+//                        }
+//                    }
+                    val inv = SimpleInventory(9 * 4)
+
+                    for (y in 0 until 4) for (x in 0 until 9) addSlot(x, y, Slot(inv, i++, 0, 0))
+                }
+                .build()
+
+            GUISBackStackManager.openGUI(player as ServerPlayerEntity, gui)
+
+            return ActionResult.SUCCESS
+        }
+
+        return ActionResult.CONSUME
     }
-}
-
-class FakeInventory(invSize: Int) : Inventory {
-    var inventory: DefaultedList<ItemStack> = DefaultedList.ofSize(invSize, ItemStack.EMPTY)
-    override fun clear() = inventory.clear()
-
-    override fun size(): Int = inventory.size
-
-    override fun isEmpty(): Boolean = true
-
-    override fun getStack(slot: Int): ItemStack = inventory[slot]
-
-    override fun removeStack(slot: Int, amount: Int): ItemStack = Inventories.splitStack(inventory, slot, amount)
-
-    override fun removeStack(slot: Int): ItemStack = Inventories.removeStack(inventory, slot)
-
-    override fun setStack(slot: Int, stack: ItemStack?) {
-        inventory[slot] = stack
-    }
-
-    override fun markDirty() {}
-
-    override fun canPlayerUse(player: PlayerEntity?): Boolean = false
 }

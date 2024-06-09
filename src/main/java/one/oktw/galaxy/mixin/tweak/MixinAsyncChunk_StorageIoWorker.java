308549db455dea7879af50a4fa4de1291a02497d
jimchen5209 <jimchen5209@gmail.com>
2024-06-09 13:29:24 +0800

    fix: Update mixin types - MixinAsyncChunk_StorageIoWorker - MixinOptimizeContainer_HopperBlockEntity

--- src/main/java/one/oktw/galaxy/mixin/tweak/MixinAsyncChunk_StorageIoWorker.java
+++ src/main/java/one/oktw/galaxy/mixin/tweak/MixinAsyncChunk_StorageIoWorker.java
@@ -25,6 +25,7 @@ import net.minecraft.util.thread.TaskExecutor;
 import net.minecraft.util.thread.TaskQueue;
 import net.minecraft.world.storage.StorageIoWorker;
 import net.minecraft.world.storage.StorageIoWorker.Priority;
+import net.minecraft.world.storage.StorageKey;
 import one.oktw.galaxy.util.KotlinCoroutineTaskExecutor;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.*;
@@ -60,9 +61,9 @@ public abstract class MixinAsyncChunk_StorageIoWorker {
     protected abstract void write(ChunkPos pos, StorageIoWorker.Result result);
 
     @Inject(method = "<init>", at = @At("RETURN"))
-    private void parallelExecutor(Path directory, boolean dsync, String name, CallbackInfo ci) {
+    private void parallelExecutor(StorageKey storageKey, Path directory, boolean dsync, CallbackInfo ci) {
         results = new ConcurrentHashMap<>();
-        executor = new KotlinCoroutineTaskExecutor<>(new TaskQueue.Prioritized(4 /* FOREGROUND,BACKGROUND,WRITE_DONE,SHUTDOWN */), "IOWorker-" + name);
+        executor = new KotlinCoroutineTaskExecutor<>(new TaskQueue.Prioritized(4 /* FOREGROUND,BACKGROUND,WRITE_DONE,SHUTDOWN */), "IOWorker-" + storageKey.type());
     }
 
     /**
--- src/main/java/one/oktw/galaxy/mixin/tweak/MixinOptimizeContainer_HopperBlockEntity.java
+++ src/main/java/one/oktw/galaxy/mixin/tweak/MixinOptimizeContainer_HopperBlockEntity.java
@@ -61,19 +61,6 @@ public abstract class MixinOptimizeContainer_HopperBlockEntity extends LootableC
      * @reason No stream forEach
      */
     @Overwrite
-    private static boolean isInventoryEmpty(Inventory inv, Direction facing) {
-        for (int i : getAvailableSlots_NoStream(inv, facing)) {
-            if (!inv.getStack(i).isEmpty()) return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * @author James58899
-     * @reason No stream forEach
-     */
-    @Overwrite
     private static boolean isInventoryFull(Inventory inv, Direction direction) {
         for (int i : getAvailableSlots_NoStream(inv, direction)) {
             ItemStack itemStack = inv.getStack(i);
@@ -83,7 +70,7 @@ public abstract class MixinOptimizeContainer_HopperBlockEntity extends LootableC
     }
 
     @Inject(method = "extract(Lnet/minecraft/block/entity/Hopper;Lnet/minecraft/inventory/Inventory;ILnet/minecraft/util/math/Direction;)Z",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;copy()Lnet/minecraft/item/ItemStack;", shift = At.Shift.BEFORE),
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getCount()I", shift = At.Shift.BEFORE),
         locals = LocalCapture.CAPTURE_FAILHARD,
         cancellable = true)
     private static void extract_EarlyCheck(Hopper hopper, Inventory inventory, int slot, Direction side, CallbackInfoReturnable<Boolean> cir, ItemStack itemStack) {
@@ -99,10 +86,9 @@ public abstract class MixinOptimizeContainer_HopperBlockEntity extends LootableC
         at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isEmpty()Z", shift = At.Shift.BEFORE, ordinal = 0),
         locals = LocalCapture.CAPTURE_FAILHARD,
         cancellable = true)
-    private static void insert_EarlyCheck(World world, BlockPos pos, BlockState state, Inventory inventory, CallbackInfoReturnable<Boolean> cir, Inventory inventory2, Direction direction, int slot) {
-        ItemStack itemStack = inventory.getStack(slot);
-        for (int i : getAvailableSlots_NoStream(inventory2, direction)) {
-            ItemStack slotItem = inventory2.getStack(i);
+    private static void insert_EarlyCheck(World world, BlockPos pos, HopperBlockEntity blockEntity, CallbackInfoReturnable<Boolean> cir, Inventory inventory, Direction direction, int slot, ItemStack itemStack) {
+        for (int i : getAvailableSlots_NoStream(inventory, direction)) {
+            ItemStack slotItem = inventory.getStack(i);
             if (slotItem.isEmpty() || canMergeItems(slotItem, itemStack)) return;
         }
 
@@ -110,15 +96,13 @@ public abstract class MixinOptimizeContainer_HopperBlockEntity extends LootableC
     }
 
     @Inject(method = "extract(Lnet/minecraft/world/World;Lnet/minecraft/block/entity/Hopper;)Z",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/block/entity/HopperBlockEntity;isInventoryEmpty(Lnet/minecraft/inventory/Inventory;Lnet/minecraft/util/math/Direction;)Z"),
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/block/entity/HopperBlockEntity;getAvailableSlots(Lnet/minecraft/inventory/Inventory;Lnet/minecraft/util/math/Direction;)[I"),
         cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
-    private static void extract_NoStream(World world, Hopper hopper, CallbackInfoReturnable<Boolean> cir, Inventory inventory, Direction direction) {
-        if (!isInventoryEmpty(inventory, direction)) {
-            for (int i : getAvailableSlots_NoStream(inventory, direction)) {
-                if (extract(hopper, inventory, i, direction)) {
-                    cir.setReturnValue(true);
-                    return;
-                }
+    private static void extract_NoStream(World world, Hopper hopper, CallbackInfoReturnable<Boolean> cir, BlockPos blockPos, BlockState blockState, Inventory inventory, Direction direction) {
+        for (int i : getAvailableSlots_NoStream(inventory, direction)) {
+            if (extract(hopper, inventory, i, direction)) {
+                cir.setReturnValue(true);
+                return;
             }
         }
         cir.setReturnValue(false);

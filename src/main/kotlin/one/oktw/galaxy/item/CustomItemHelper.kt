c3fc180e68b20dfeb24cb7adfcc7b3ff794ae71e
jimchen5209 <jimchen5209@gmail.com>
2024-06-12 14:24:53 +0800

    refactor: change nbt naming style to snake to match minecraft style

--- src/main/kotlin/one/oktw/galaxy/item/CustomItemHelper.kt
+++ src/main/kotlin/one/oktw/galaxy/item/CustomItemHelper.kt
@@ -27,13 +27,13 @@ import net.minecraft.util.Identifier
 object CustomItemHelper {
     fun getNbt(itemStack: ItemStack): NbtCompound {
         val galaxyData = (itemStack.get(DataComponentTypes.CUSTOM_DATA) ?: NbtComponent.DEFAULT).copyNbt()
-        return galaxyData.getCompound("GalaxyData")
+        return galaxyData.getCompound("galaxy_data")
     }
 
     fun getItem(itemStack: ItemStack): CustomItem? {
         val customNbt = getNbt(itemStack)
 
-        return customNbt.getString("CustomItemIdentifier")?.let(Identifier::tryParse)
+        return customNbt.getString("custom_item_identifier")?.let(Identifier::tryParse)
             ?.let(CustomItem.registry::get)
             ?.run { readCustomNbt(customNbt) }
     }

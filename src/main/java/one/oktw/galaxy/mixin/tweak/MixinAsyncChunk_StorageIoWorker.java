07a0dc566182e3946038329d5ec334e64a5a7a64
jimchen5209 <jimchen5209@gmail.com>
2024-06-12 15:12:25 +0800

    chore: update copyright

--- src/main/java/one/oktw/galaxy/mixin/tweak/MixinAsyncChunk_StorageIoWorker.java
+++ src/main/java/one/oktw/galaxy/mixin/tweak/MixinAsyncChunk_StorageIoWorker.java
@@ -1,6 +1,6 @@
 /*
  * OKTW Galaxy Project
- * Copyright (C) 2018-2023
+ * Copyright (C) 2018-2024
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
--- src/main/java/one/oktw/galaxy/mixin/tweak/MixinOptimizeContainer_HopperBlockEntity.java
+++ src/main/java/one/oktw/galaxy/mixin/tweak/MixinOptimizeContainer_HopperBlockEntity.java
@@ -1,6 +1,6 @@
 /*
  * OKTW Galaxy Project
- * Copyright (C) 2018-2021
+ * Copyright (C) 2018-2024
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published

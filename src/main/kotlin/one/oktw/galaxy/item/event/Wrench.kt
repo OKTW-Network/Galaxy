eabf00de142a3b27dd29c66753363b9d273ee314
jimchen5209 <jimchen5209@gmail.com>
2024-08-17 22:36:54 +0800

    fix: hand not swing when spinning custom block

--- src/main/kotlin/one/oktw/galaxy/item/event/Wrench.kt
+++ src/main/kotlin/one/oktw/galaxy/item/event/Wrench.kt
@@ -1,6 +1,6 @@
 /*
  * OKTW Galaxy Project
- * Copyright (C) 2018-2023
+ * Copyright (C) 2018-2024
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
@@ -79,6 +79,7 @@ class Wrench {
             startDirection[player] = blockPos to direction
 
             blockEntity.facing = if (next == -1 || next > allowedFacing.lastIndex) allowedFacing.first() else allowedFacing[next]
+            return true
         }
 
         // Check destructible

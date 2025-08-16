c0b23a8b8cd33052e7a703b35453eab86e1c20a1
jimchen5209 <jimchen5209@gmail.com>
2025-08-16 16:16:23 +0800

    build: bump kotlin to 2.2.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     //    "maven-publish"
-    kotlin("jvm") version "2.2.0"
+    kotlin("jvm") version "2.2.10"
     id("fabric-loom") version "1.11-SNAPSHOT"
 }
 

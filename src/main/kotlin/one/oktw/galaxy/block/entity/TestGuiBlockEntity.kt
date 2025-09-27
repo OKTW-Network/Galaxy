27212619478788c1386b31a7608db45249b228bb
James58899 <james59988@gmail.com>
2025-09-28 03:04:48 +0800

    Add Test GUI background

--- src/main/kotlin/one/oktw/galaxy/block/entity/TestGuiBlockEntity.kt
+++ src/main/kotlin/one/oktw/galaxy/block/entity/TestGuiBlockEntity.kt
@@ -32,6 +32,7 @@ import net.minecraft.storage.WriteView
 import net.minecraft.text.Text
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
+import net.minecraft.util.Identifier
 import net.minecraft.util.collection.DefaultedList
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
@@ -45,34 +46,40 @@ class TestGuiBlockEntity(type: BlockEntityType<*>, pos: BlockPos, modelItem: Ite
     CustomBlockClickListener, Inventory {
     private val inventory = DefaultedList.ofSize(3 * 9, ItemStack.EMPTY)
 
-    // FIXME: GUI Background in title
-    private val gui = GUI.Builder(ScreenHandlerType.GENERIC_9X6).setTitle(Text.of("Test GUI")).blockEntity(this).apply {
-        var i = 0
-        for (x in 0 until 9) addSlot(x, 0, Slot(this@TestGuiBlockEntity, i++, 0, 0))
-        for (y in 4 until 6) for (x in 0 until 9) addSlot(x, y, Slot(this@TestGuiBlockEntity, i++, 0, 0))
-    }.build().apply {
-        editInventory {
-            fill(0 until 9, 1 until 4, Misc.PLACEHOLDER.createItemStack())
-            set(4, 2, Gui.CHECK_MARK.createItemStack())
+    private val gui = GUI.Builder(ScreenHandlerType.GENERIC_9X6)
+        .setTitle(Text.of("Test GUI"))
+        .setBackground("A", Identifier.of("galaxy", "gui_font/container_layout/test_gui"))
+        .blockEntity(this)
+        .apply {
+            var i = 0
+            for (x in 0 until 9) addSlot(x, 0, Slot(this@TestGuiBlockEntity, i++, 0, 0))
+            for (y in 4 until 6) for (x in 0 until 9) addSlot(x, y, Slot(this@TestGuiBlockEntity, i++, 0, 0))
+        }.build().apply {
+            editInventory {
+                fill(0 until 9, 1 until 4, Misc.PLACEHOLDER.createItemStack())
+                set(4, 2, Gui.CHECK_MARK.createItemStack())
+            }
+            addBinding(4, 2) {
+                GUISBackStackManager.openGUI(player, gui2)
+            }
         }
-        addBinding(4, 2) {
-            GUISBackStackManager.openGUI(player, gui2)
-        }
-    }
 
-    // FIXME: GUI Background in title
-    private val gui2 = GUI.Builder(ScreenHandlerType.GENERIC_9X4).setTitle(Text.of("Test GUI2")).blockEntity(this).apply {
-        var i = 0
-        for (y in 0 until 3) for (x in 0 until 9) addSlot(x, y, Slot(this@TestGuiBlockEntity, i++, 0, 0))
-    }.build().apply {
-        editInventory {
-            fill(0 until 9, 3..3, Misc.PLACEHOLDER.createItemStack())
-            set(4, 3, Gui.CROSS_MARK.createItemStack().apply { this.set(DataComponentTypes.ITEM_NAME, Text.of("CLOSE ALL")) })
+    private val gui2 = GUI.Builder(ScreenHandlerType.GENERIC_9X4)
+        .setTitle(Text.of("Test GUI2"))
+        .setBackground("B", Identifier.of("galaxy", "gui_font/container_layout/test_gui"))
+        .blockEntity(this)
+        .apply {
+            var i = 0
+            for (y in 0 until 3) for (x in 0 until 9) addSlot(x, y, Slot(this@TestGuiBlockEntity, i++, 0, 0))
+        }.build().apply {
+            editInventory {
+                fill(0 until 9, 3..3, Misc.PLACEHOLDER.createItemStack())
+                set(4, 3, Gui.CROSS_MARK.createItemStack().apply { this.set(DataComponentTypes.ITEM_NAME, Text.of("CLOSE ALL")) })
+            }
+            addBinding(4, 3) {
+                GUISBackStackManager.closeAll(player)
+            }
         }
-        addBinding(4, 3) {
-            GUISBackStackManager.closeAll(player)
-        }
-    }
 
     override fun readCopyableData(view: ReadView) {
         Inventories.readData(view, inventory)

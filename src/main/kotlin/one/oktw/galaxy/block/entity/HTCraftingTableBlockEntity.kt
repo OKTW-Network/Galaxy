0858eaa423ac82b48b1e11cc31397f2f4e981622
James58899 <james59988@gmail.com>
2025-10-01 12:31:47 +0800

    Impl HT Crafting

--- src/main/kotlin/one/oktw/galaxy/block/entity/HTCraftingTableBlockEntity.kt
+++ src/main/kotlin/one/oktw/galaxy/block/entity/HTCraftingTableBlockEntity.kt
@@ -20,8 +20,11 @@ package one.oktw.galaxy.block.entity
 
 import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.inventory.SimpleInventory
 import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import net.minecraft.screen.ScreenHandlerType
+import net.minecraft.screen.slot.Slot
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.Text
 import net.minecraft.util.ActionResult
@@ -33,7 +36,7 @@ import one.oktw.galaxy.block.listener.CustomBlockClickListener
 import one.oktw.galaxy.gui.GUI
 import one.oktw.galaxy.gui.GUISBackStackManager
 import one.oktw.galaxy.item.CustomBlockItem
-import one.oktw.galaxy.player.PlayerHelper
+import one.oktw.galaxy.item.Misc
 
 class HTCraftingTableBlockEntity(type: BlockEntityType<*>, pos: BlockPos, modelItem: ItemStack) : ModelCustomBlockEntity(type, pos, modelItem),
     CustomBlockClickListener {
@@ -46,6 +49,7 @@ class HTCraftingTableBlockEntity(type: BlockEntityType<*>, pos: BlockPos, modelI
         .build()
         .apply {
             editInventory {
+                fillAll(Misc.PLACEHOLDER.createItemStack())
                 set(0, 1, CustomBlockItem.HT_CRAFTING_TABLE.createItemStack())
                 set(2, 0, CustomBlockItem.HT_CRAFTING_TABLE.createItemStack())
                 set(3, 0, CustomBlockItem.ELEVATOR.createItemStack())
@@ -54,35 +58,93 @@ class HTCraftingTableBlockEntity(type: BlockEntityType<*>, pos: BlockPos, modelI
                 set(6, 0, CustomBlockItem.TRASHCAN.createItemStack())
             }
             addBinding(2, 0) {
-                GUISBackStackManager.openGUI(player, getRecipeGui(CustomBlockItem.HT_CRAFTING_TABLE.createItemStack()))
+                cancel = true
+                val recipe = listOf(
+                    ItemStack(Items.REDSTONE, 2),
+                    ItemStack(Items.IRON_INGOT, 2),
+                    ItemStack(Items.LAPIS_LAZULI, 2),
+                    ItemStack(Items.DIAMOND, 1),
+                    ItemStack(Items.CRAFTING_TABLE, 1),
+                    ItemStack(Items.OBSIDIAN, 1)
+                )
+                GUISBackStackManager.openGUI(player, getRecipeGui(recipe, CustomBlockItem.HT_CRAFTING_TABLE.createItemStack()))
             }
             addBinding(3, 0) {
-                GUISBackStackManager.openGUI(player, getRecipeGui(CustomBlockItem.ELEVATOR.createItemStack()))
+                cancel = true
+                val recipe = listOf(
+                    ItemStack(Items.IRON_BLOCK, 1),
+                    ItemStack(Items.ENDER_PEARL, 1)
+                )
+                GUISBackStackManager.openGUI(player, getRecipeGui(recipe, CustomBlockItem.ELEVATOR.createItemStack()))
             }
             addBinding(4, 0) {
-                GUISBackStackManager.openGUI(player, getRecipeGui(CustomBlockItem.ANGEL_BLOCK.createItemStack()))
+                cancel = true
+                val recipe = listOf(CustomBlockItem.ANGEL_BLOCK.createItemStack()) // TODO
+                GUISBackStackManager.openGUI(player, getRecipeGui(recipe, CustomBlockItem.ANGEL_BLOCK.createItemStack()))
             }
             addBinding(5, 0) {
-                GUISBackStackManager.openGUI(player, getRecipeGui(CustomBlockItem.HARVEST.createItemStack()))
+                cancel = true
+                val recipe = listOf(
+                    ItemStack(Items.COPPER_INGOT, 7),
+                    ItemStack(Items.DISPENSER, 1),
+                    ItemStack(Items.OBSERVER, 1)
+                )
+                GUISBackStackManager.openGUI(player, getRecipeGui(recipe, CustomBlockItem.HARVEST.createItemStack()))
             }
             addBinding(6, 0) {
-                GUISBackStackManager.openGUI(player, getRecipeGui(CustomBlockItem.TRASHCAN.createItemStack()))
+                cancel = true
+                val recipe = listOf(
+                    ItemStack(Items.GLASS, 4),
+                    ItemStack(Items.CACTUS, 1),
+                    ItemStack(Items.TERRACOTTA, 2),
+                    ItemStack(Items.SAND, 1)
+                )
+                GUISBackStackManager.openGUI(player, getRecipeGui(recipe, CustomBlockItem.TRASHCAN.createItemStack()))
             }
         }
 
-    private fun getRecipeGui(itemStack: ItemStack): GUI {
+    private fun getRecipeGui(recipe: List<ItemStack>, itemStack: ItemStack): GUI {
+        val output = SimpleInventory(itemStack.copy())
         return GUI.Builder(ScreenHandlerType.GENERIC_9X3)
             .setTitle(Text.translatable("UI.Title.HiTechCraftingTableRecipe", itemStack.itemName))
             .setBackground("B", Identifier.of("galaxy", "gui_font/container_layout/ht_crafting_table"))
             .blockEntity(this)
-            .build()
-            .apply {
-                editInventory {
-                    set(7, 1, itemStack)
+            .addSlot(7, 1, object : Slot(output, 0, 0, 0) {
+                override fun canInsert(stack: ItemStack) = false
+
+                override fun canTakeItems(player: PlayerEntity): Boolean {
+                    return recipe.all { recipe ->
+                        var count = recipe.count
+                        for (item in player.inventory.mainStacks) {
+                            if (ItemStack.areItemsAndComponentsEqual(item, recipe)) count -= item.count
+                            if (count <= 0) break
+                        }
+                        count <= 0
+                    }
                 }
-                addBinding(7, 1) {
-                    PlayerHelper.giveItemToPlayer(player, itemStack)
-                    player.closeHandledScreen()
+
+                override fun onTakeItem(player: PlayerEntity, stack: ItemStack) {
+                    recipe.forEach {
+                        var count = it.count
+                        while (count > 0) {
+                            val slot = player.inventory.getSlotWithStack(it)
+                            if (slot == -1) break // not found
+                            count -= player.inventory.removeStack(slot, count).count
+                        }
+                    }
+                    setStackNoCallbacks(itemStack.copy()) // refill output
+                    super.onTakeItem(player, stack)
+                }
+            })
+            .setSkipPick(7, 1)
+            .build().apply {
+                editInventory {
+                    fillAll(Misc.PLACEHOLDER.createItemStack())
+                    var i = 0
+                    for (y in 0..2) for (x in 1..4) {
+                        val item = recipe.getOrNull(i++) ?: break
+                        set(x, y, item)
+                    }
                 }
             }
     }

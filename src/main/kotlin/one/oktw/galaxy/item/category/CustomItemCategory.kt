28e44df5b7808aa49a38b9aa6d9c1a0c75c32e2e
jimchen5209 <jimchen5209@gmail.com>
2025-10-03 00:11:15 +0800

    feat: add custom item category

---
+++ src/main/kotlin/one/oktw/galaxy/item/category/CustomItemCategory.kt
@@ -0,0 +1,40 @@
+/*
+ * OKTW Galaxy Project
+ * Copyright (C) 2018-2025
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.oktw.galaxy.item.category
+
+import net.minecraft.text.Text
+import one.oktw.galaxy.item.CustomItem
+
+abstract class CustomItemCategory {
+    companion object {
+        val categories = listOf(
+            Block(),
+            Tool(),
+            Weapon(),
+            Material(),
+            Upgrade()
+        )
+
+        val creativeCategories = categories + Gui()
+    }
+
+    abstract val displayName: Text
+    abstract val displayItem: CustomItem
+    abstract val items: List<CustomItem>
+}
